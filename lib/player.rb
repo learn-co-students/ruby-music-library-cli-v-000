45d1eda10cf2f3e8c1a20580b344d6576a56e94e
aviflombaum <avi@flombaum.com>
2015-10-29 00:09:37 -0400

    Initial Master

--- lib/player.rb
+++
@@ -1,7 +0,0 @@
-class Player
-  attr_reader :token
-
-  def initialize(token)
-    @token = token
-  end
-end
--- lib/players/human.rb
+++
@@ -1,8 +0,0 @@
-class Player::Human < Player
-
-  def move(board)
-    puts "Please enter 1-9:"
-    gets.strip
-  end
-
-end
