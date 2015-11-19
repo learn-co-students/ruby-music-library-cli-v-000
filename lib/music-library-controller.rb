d62597dd916abac6c26abb845e416eb51eedd3d6
github username <github email address>
2015-11-18 23:18:11 -0500

    updated cli

--- lib/music-library-controller.rb
+++ lib/music-library-controller.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class MusicLibraryController
-  attr_accessor :path 
+  attr_accessor :path
   
   def initialize(path="./db/mp3s")
     MusicImporter.new(path).import
@@ -9,11 +9,34 @@ class MusicLibraryController
 
   def call
     answer = ""
+
     while answer != "exit"
-      answer = gets.strip.downcase 
-      list_songs
-      list_artist
-      play_song
+      puts "\nChoose one of the following actions or enter 'q' to exit\n\n"
+      puts "Play - Play a song"
+      puts "Artist - Search by artist to view a list of thier songs"
+      puts "Genre - Seach songs by genre"
+      puts "Songs - View a list of songs"
+      puts "List Artists - View a list of Artists"
+      puts "List Genres - View of a list of genres\n\n"
+
+      answer = gets.strip.downcase
+      puts
+      exit if answer == "q" 
+      
+      case answer
+      when "play"
+        play_song
+      when "artist"
+        songs_by_artist
+      when "genre"
+        songs_by_genre
+      when "songs"
+        list_songs
+      when "list artists"
+        list_artists 
+      when "list genres"
+        list_genres
+      end
     end
   end
 
@@ -23,15 +46,45 @@ class MusicLibraryController
     end
   end
 
-  def list_artist
-    Artist.all.each_with_index { |artist, i| puts "#{i+1}. #{artist}"}
+  def list_artists
+    Artist.all.each_with_index { |artist, i| puts "#{i+1}. #{artist.name}"}
   end
 
-  def list_genre
-    Genre.all.each_with_index { |genre, i| puts "#{i+1}. #{genre}"}
+  def list_genres
+    Genre.all.each_with_index { |genre, i| puts "#{i+1}. #{genre.name.capitalize}"}
   end
 
   def play_song
-    puts "Playing #{Song.all[0].artist.name} - #{Song.all[0].name} - #{Song.all[0].genre.name}"
+    list_songs 
+    puts "\nEnter the song number you would like to play:"
+    answer = gets.strip.to_i-1
+    puts "\nPlaying: #{Song.all[answer].artist.name} - #{Song.all[answer].name} - #{Song.all[answer].genre.name}"
+  end
+
+  def songs_by_artist
+    list_artists
+    puts "\nEnter the artists name to see a list of their songs:"
+    artist_name = gets.strip.downcase
+
+    puts "\n#{artist_name.split.map(&:capitalize).join(' ')}'s Songs:\n\n"
+    Song.all.each do |song|
+      if song.artist.name.downcase == artist_name
+        puts "  #{song.name}" 
+      end
+    end
+  end
+
+  def songs_by_genre
+    list_genres
+    puts "\nEnter a genre to list all of their songs:"
+
+    genre_name = gets.strip.downcase
+    puts "\n#{genre_name.capitalize} Songs:\n\n"
+
+    Song.all.each do |song|
+      if song.genre.name.downcase == genre_name 
+        puts "  #{song.artist.name} - #{song.name}"
+      end
+    end
   end
 end
