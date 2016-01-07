fe595afecf14b925488efe032ffbd3c87188e29f
Chris Aporta <christopher.aporta@gmail.com>
2016-01-06 19:14:29 -0500

    complete spec/005

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,10 @@
 class Genre
-  attr_accessor :name
+  attr_accessor :name, :songs
   @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def self.all
--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,23 @@
 require 'pry'
 
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, :genre
   @@all = []
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist = artist if !artist.nil?
+    self.genre = genre if !genre.nil?
   end
 
   def artist=(artist)
     @artist = artist
     artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
   end
 
   def self.all
