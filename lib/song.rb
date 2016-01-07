5fbc0f73db1ab28cf072485379cbc7945b6549d1
Chris Aporta <christopher.aporta@gmail.com>
2016-01-06 20:54:54 -0500

    complete spec/008

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 require 'pry'
+require_relative '../concerns/findable.rb'
 
 class Song
+  extend Concerns::Findable
   attr_accessor :name, :artist, :genre
   @@all = []
 
@@ -18,14 +20,6 @@ class Song
   def genre=(genre)
     @genre = genre
     genre.songs << self unless genre.songs.include?(self)
-  end
-
-  def self.find_by_name(name)
-    all.find { |song| song.name == name  }
-  end
-
-  def self.find_or_create_by_name(name)
-    find_by_name(name) || create(name)
   end
 
   def self.all
