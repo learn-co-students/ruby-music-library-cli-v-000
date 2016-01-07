6abbd5bb4bbf9b6af8a308d61549fd289b728f38
Chris Aporta <christopher.aporta@gmail.com>
2016-01-06 20:16:33 -0500

    complete spec/007

--- lib/song.rb
+++ lib/song.rb
@@ -20,6 +20,14 @@ class Song
     genre.songs << self unless genre.songs.include?(self)
   end
 
+  def self.find_by_name(name)
+    all.find { |song| song.name == name  }
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
   def self.all
     @@all
   end
