21a8d1a5ea9a26af6b51041d3c029514cbf674de
marcotsui2003 <marcotsui2003@yahoo.com>
2016-01-12 16:46:17 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,16 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+  module Findable
+    def find_by_name(name)
+      self.all.detect {|item| item.name == name}
+    end
+
+    def find_or_create_by_name(name)
+      result = self.find_by_name(name)
+      result ||= self.create(name)
+    end
+  end
 end
 
 require_all 'lib'
