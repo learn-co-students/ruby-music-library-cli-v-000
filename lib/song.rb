0c38ca0d39fafaf1287a13f92c14b1e33c559691
shinyrachel <rachel.kory@gmail.com>
2016-02-17 18:14:44 -0500

    007 findable songs spec passing

--- lib/song.rb
+++ lib/song.rb
@@ -38,6 +38,18 @@ class Song
     genre.add_song(self)
   end
 
+  def self.find_by_name(name)
+    self.all.find do |song|
+      song.name
+    end
+  end
 
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create(name)
+    end
+  end
 
 end
