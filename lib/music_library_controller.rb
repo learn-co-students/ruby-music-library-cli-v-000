2eb3bb59f7683f0629426befb9ea94371be0a8db
fiveinfinity <jlachance1@gmail.com>
2016-02-22 09:37:41 -0600

    minor change to CLI

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -11,7 +11,14 @@ class MusicLibraryController
 
   def call
     song_counter = 1
-    welcome = 'Hi there'
+    puts 'WELCOME TO THE MUSIC LIBRARY. PLEASE CHOOSE FROM THE FOLLOWING:'
+    # puts '1. list songs'
+    # puts '2. list artists'
+    # puts '3. list genres'
+    # puts '4. play song'
+    # puts '5. list artist'
+    # puts '6. list genre'
+    # puts '7. exit'
     answer = gets.chomp
 
     if answer == 'list songs'
@@ -19,16 +26,21 @@ class MusicLibraryController
         puts "#{song_counter}. #{i.artist.name} - #{i.name} - #{i.genre.name}"
         song_counter += 1
       end
+
       answer = gets.chomp
     elsif answer == 'list artists'
       Song.all.each do |i|
         puts "#{song_counter}. #{i.artist.name}"
+        song_counter += 1
       end
+
       answer = gets.chomp
     elsif answer == 'list genres'
       Song.all.each do |i|
         puts "#{song_counter}. #{i.genre.name}"
+        song_counter += 1
       end
+
       answer = gets.chomp
       #regardless of what you type in after play song, this will always print out every artist in the library.
     elsif answer == 'play song'
@@ -36,6 +48,7 @@ class MusicLibraryController
         Song.all.each do |i|
           puts "Playing #{i.artist.name} - #{i.name} - #{i.genre.name}"
         end
+
       answer = gets.chomp
     elsif answer == 'list artist'
       answer = gets.chomp #gets artist name here
@@ -44,6 +57,7 @@ class MusicLibraryController
             puts "Playing #{i.artist.name} - #{i.name} - #{i.genre.name}"
           end
         end
+
       answer = gets.chomp
     elsif answer == 'list genre'
       answer = gets.chomp #gets genre name here
@@ -52,6 +66,7 @@ class MusicLibraryController
             puts "Playing #{i.artist.name} - #{i.name} - #{i.genre.name}"
           end
         end
+
       answer = gets.chomp
     else
       puts 'exit'
