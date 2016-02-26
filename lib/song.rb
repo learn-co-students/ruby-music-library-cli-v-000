f24372fe0c9d85e5f34c25dfd8f9c439c3d3d2e3
wsaxe <geosaxe@gmail.com>
2016-02-25 19:49:10 -0600

    passed all up to 010

--- lib/song.rb
+++ lib/song.rb
@@ -35,4 +35,19 @@ class Song
     @genre = genre
     genre.songs << self if genre != "" && !genre.songs.include?(self)
   end
+  def self.new_from_filename(filename)
+    file_artist = filename.split(" - ")[0]
+    file_song_name = filename.split(" - ")[1]
+    file_genre_mp3 = filename.split(" - ")[2]
+    file_genre = file_genre_mp3.split(".").first
+    song = Song.new(file_song_name)
+    song.artist = Artist.find_or_create_by_name(file_artist)
+    song.genre = Genre.find_or_create_by_name(file_genre)
+    song
+  end
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+    song
+  end
 end
