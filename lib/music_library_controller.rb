0bfcf6bb1476d32c3c21f7fcc2b7f71c34f1b4c3
abadfish <kirsten.ofarrell@gmail.com>
2016-02-28 19:44:02 -0500

    DONE

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -11,7 +11,7 @@ class MusicLibraryController
     
     puts "Welcome to your music Library"
     input = ""
-    unless input == "exit"  
+    while input != "exit"  
 
       puts "Choose one of the following options:
         list songs, list genres, play song, list artist, list genre, or exit"
@@ -56,20 +56,15 @@ class MusicLibraryController
   def play_song
     puts "What song number would you like to play?"
     song_input = gets.strip
-    puts "Playing #{Song.all[song_input.to_i-1].artist.name}, #{Song.all[song_input.to_i-1].name}, #{Song.all[song_input.to_i-1].genre.name}
-    "
+    puts "Playing #{Song.all[song_input.to_i-1].artist.name} - #{Song.all[song_input.to_i-1].name} - #{Song.all[song_input.to_i-1].genre.name}"
   end
 
   def artist_songs
     puts "What artist would you like to list songs for?"
     artist_input = gets.strip
-    # puts "You don't have that artist in your library.
-    #   " 
-    #   artists unless
     if artist = Artist.find_by_name(artist_input)
       artist.songs.each_with_index do |song, index|
-        puts "#{index + 1}. #{song.name}
-        "
+        puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
       end
     end
   end
@@ -77,11 +72,9 @@ class MusicLibraryController
   def genre_songs
     puts "What genre would you like a list of songs for?"
     input = gets.strip
-    # puts "You don't have any songs with that genre in your library.
-    # " unless
     if genre = Genre.find_by_name(input)
-      genre.songs.each_with_index do |genre, index|
-        puts "#{index + 1}. #{genre.name}"
+      genre.songs.each_with_index do |song, index|
+        puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
       end
     end
   end
