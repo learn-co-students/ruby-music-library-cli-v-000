d4d223cd4a4744efff380ce9557bf576744d3322
gnfisher <me@gnfisher.com>
2016-03-15 16:29:36 -0300

    refactored new from file and create from file

--- lib/songs.rb
+++ lib/songs.rb
@@ -11,6 +11,10 @@ class Song
     self.artist = artist unless !artist
     self.genre = genre unless !genre
   end
+
+  def self.create(name, artist = nil, genre = nil)
+    new(name, artist, genre).tap(&:save)
+  end
   
   def artist=(artist)
     @artist = artist
@@ -24,24 +28,19 @@ class Song
 
   def save
     Song.all << self
-    self
-  end
-
-  def self.create(name)
-    song = Song.new(name)
-    song.save
   end
 
   def self.new_from_filename(fname)
     song_array = fname.split(' - ')
-    song = Song.find_or_create_by_name(song_array[1])
-    song.artist = Artist.find_or_create_by_name(song_array[0])
-    song.genre = Genre.find_or_create_by_name(song_array[2].chomp('.mp3'))
-    song
+    song_name, song_artist, song_genre = song_array[1], song_array[0], song_array[2].chomp('.mp3')
+    
+    artist = Artist.find_or_create_by_name(song_artist)
+    genre = Genre.find_or_create_by_name(song_genre)
+    self.new(song_name, artist, genre)
   end
 
   def self.create_from_filename(fname)
-    Song.new_from_filename(fname)
+    self.new_from_filename(fname).tap(&:save)
   end
 
   def self.all
