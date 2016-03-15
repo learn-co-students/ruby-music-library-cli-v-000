6d66bd364521aeeed652c66b4a1c7c0af0e3098d
gnfisher <me@gnfisher.com>
2016-03-15 16:30:29 -0300

    reorganized

--- lib/songs.rb
+++ lib/songs.rb
@@ -8,12 +8,25 @@ class Song
 
   def initialize(name, artist = nil, genre = nil)
     @name = name
-    self.artist = artist unless !artist
-    self.genre = genre unless !genre
+    self.artist = artist if artist
+    self.genre = genre if genre
   end
 
   def self.create(name, artist = nil, genre = nil)
     new(name, artist, genre).tap(&:save)
+  end
+
+  def self.new_from_filename(fname)
+    song_array = fname.split(' - ')
+    song_name, song_artist, song_genre = song_array[1], song_array[0], song_array[2].chomp('.mp3')
+    
+    artist = Artist.find_or_create_by_name(song_artist)
+    genre = Genre.find_or_create_by_name(song_genre)
+    self.new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(fname)
+    self.new_from_filename(fname).tap(&:save)
   end
   
   def artist=(artist)
@@ -30,24 +43,7 @@ class Song
     Song.all << self
   end
 
-  def self.new_from_filename(fname)
-    song_array = fname.split(' - ')
-    song_name, song_artist, song_genre = song_array[1], song_array[0], song_array[2].chomp('.mp3')
-    
-    artist = Artist.find_or_create_by_name(song_artist)
-    genre = Genre.find_or_create_by_name(song_genre)
-    self.new(song_name, artist, genre)
-  end
-
-  def self.create_from_filename(fname)
-    self.new_from_filename(fname).tap(&:save)
-  end
-
   def self.all
     @@all
   end
-
-  #def self.destroy_all
-  #  self.all.clear
-  #end
 end
