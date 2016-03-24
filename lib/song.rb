4e5e39deda2ca1f02a2e5f1deb611aac870fe645
viparthasarathy <viparthasarathy@gmail.com>
2016-03-23 18:13:46 -0700

    create and define song model methods

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+
+require 'pry'
+class Song
+  attr_accessor :name, :artist, :genre
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+  end
+
+end
