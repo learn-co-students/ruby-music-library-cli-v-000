7295457a189bbeaebd41f7e4aeded21139aca11f
joshnevius <joshnevius92@gmail.com>
2016-03-24 20:24:59 -0400

    Built music library CLI.

---
+++ lib/artist.rb
@@ -0,0 +1,41 @@
+class Artist
+  extend Concerns::Findable
+
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(artist)
+    artist = self.new(artist)
+    artist.save
+    artist
+  end
+
+  def add_song(song)
+    @songs << song unless @songs.include?(song)
+    song.artist = self unless song.artist == self
+  end
+
+  def genres 
+    self.songs.map{|song| song.genre}.uniq
+  end
+
+end
