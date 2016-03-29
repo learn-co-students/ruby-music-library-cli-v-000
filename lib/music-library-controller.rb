f090c4dbe35e7af6c062f2ca4230fe38b4100a3c
McN33ly <rmcneely23@gmail.com>
2016-03-29 10:09:30 -0400

    Done.

--- lib/music-library-controller.rb
+++ lib/music-library-controller.rb
@@ -10,7 +10,7 @@ extend Concerns::Findable
 
   end
 
-  def call
+  def main_menu
     puts "What would you like to do?"
     puts "Show all artists?"
     puts "  Type 'list artists'"
@@ -21,46 +21,50 @@ extend Concerns::Findable
     puts "Play music now"
     puts "  Type 'play song'"
     input = gets.strip
+  end
+
+  def call
+    main_menu
     until input == "exit"
-        if input == "list songs"
-          @library.files.each.with_index(1) do |file, index|
-            puts "#{index}. #{file.split(".").first}"
-          end
+                  if input == "list songs"
+                    @library.files.each.with_index(1) do |file, index|
+                      puts "#{index}. #{file.split(".").first}"
+                    end####END DO IN LIST SONGS
 
-        elsif input == "list artists"
-          Artist.all.each {|artist| puts artist.name}
-          puts "Type Artist to show their songs"
-          artist_input = gets.strip
-          lib_copy = @library.files.collect {|file|file.split(".").first.split(" - ")}
-          lib_copy.keep_if {|file| file[0] == artist_input}
-          lib_copy.each {|file| puts"#{file.join(" - ")}"}
-            #file[0] == artist_input
+                  elsif input == "list artists"
+                    Artist.all.each {|artist| puts artist.name}
+                    puts "Type Artist to show their songs"
+                    artist_input = gets.strip
+                    lib_copy = @library.files.collect {|file|file.split(".").first.split(" - ")}
+                    lib_copy.keep_if {|file| file[0] == artist_input}
+                    lib_copy.each {|file| puts"#{file.join(" - ")}"}
+                      #file[0] == artist_input
 
 
-          #{|index| puts "#{index}" }
-        elsif input == "list genres"
-          Genre.all.each {|genre| puts genre.name}
-          puts "Type Genre to show its songs"
-          genre_input = gets.strip
-          lib_copy = @library.files.collect {|file|file.split(".").first.split(" - ")}
-          #lib_copy.each {|x| puts "#{x}"}
-          lib_copy.keep_if {|file| file.last == genre_input}
-          lib_copy.each {|file| puts"#{file.join(" - ")}"}
+                    #{|index| puts "#{index}" }
+                  elsif input == "list genres"
+                    Genre.all.each {|genre| puts genre.name}
+                    puts "Type Genre to show its songs"
+                    genre_input = gets.strip
+                    lib_copy = @library.files.collect {|file|file.split(".").first.split(" - ")}
+                    #lib_copy.each {|x| puts "#{x}"}
+                    lib_copy.keep_if {|file| file.last == genre_input}
+                    lib_copy.each {|file| puts"#{file.join(" - ")}"}
 
-        elsif input == "play song"
-          puts"Select Number"
-          @library.files.each.with_index(1) do |file, index|
-            puts "#{index}. #{file.split(".").first}"
-          end
-          song_num = gets.strip.to_i
-          puts"Playing #{@library.files[(song_num - 1)]}"
+                  elsif input == "play song"
+                    puts"Select Number"
+                    @library.files.each.with_index(1) do |file, index|
+                      puts "#{index}. #{file.split(".").first}"
+                    end####END DO IN PLAY SONG
+                    song_num = gets.strip.to_i
+                    puts"Playing #{@library.files[(song_num - 1)]}"
 
-        else
-          puts "Invalid choice"
-          input = gets.strip
-        end
-        puts "Make another selection or 'exit'"
-        input = gets.strip
+                  else
+                    puts "Invalid choice"
+                    puts "Make another selection or 'exit'"
+                    input = gets.strip
+                  end####END GIANT IF STATEMENT
+        
     end####END OF UNTIL   .include?("artist_input")
   end####END OF CALL METHOD
 #binding.pry
