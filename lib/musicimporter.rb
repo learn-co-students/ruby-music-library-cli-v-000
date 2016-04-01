7081bbe552563d66fc8ebaa3d4ffc4350beb0c25
phaltoe <reverse9@live.com>
2016-04-01 09:27:26 -0300

    Done.

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -8,17 +8,4 @@ class MusicImporter
   def files
     Dir.entries(path).select { |file| file.end_with?(".mp3") }
   end
-
-  def self.new_from_filename(filename)
-    # song = self.create(filename)
-    song = filename.chomp!(".mp3")
-    # song = filename.split(" - ")
-    name = song.split(" - ")[1]
-    artist = song.split(" - ")[0]
-    genre = song.split(" - ")[2]
-    new_song = Song.new(name)
-    new_song.artist = artist
-    new_song.genre = genre
-    new_song
-  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -49,17 +49,17 @@ class Song
     new_song.artist = new_artist
   end
 
-  # def self.new_from_filename(filename)
-  #   # song = self.create(filename)
-  #   song = filename.chomp!(".mp3")
-  #   # song = filename.split(" - ")
-  #   name = song.split(" - ")[1]
-  #   artist = song.split(" - ")[0]
-  #   genre = song.split(" - ")[2]
-  #   new_song = Song.new(name)
-  #   new_song.artist = artist
-  #   new_song.genre = genre
-  #   new_song
-  # end
+  def self.new_from_filename(filename)
+    # song = self.create(filename)
+    song = filename.chomp!(".mp3")
+    # song = filename.split(" - ")
+    name = song.split(" - ")[1]
+    artist = song.split(" - ")[0]
+    genre = song.split(" - ")[2]
+    new_song = Song.new(name)
+    new_song.artist=(artist)
+    new_song.genre = genre
+    new_song
+  end
 
 end
