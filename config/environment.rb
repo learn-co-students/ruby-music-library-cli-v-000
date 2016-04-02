697348314ef0d38eca23709f28e1fa9fe94ec611
David Ward <david_ward@Users-MacBook-Pro.local>
2016-04-02 04:27:10 -0400

    save 1

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,11 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+module Findable
 end
+end
+
+Concerns::Findable
+
 
 require_all 'lib'
---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+class Genre
+    attr_accessor :name, :songs
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        self.all.clear
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.create(name)
+        genre = self.new(name)
+        genre.save
+        genre
+    end
+
+end
