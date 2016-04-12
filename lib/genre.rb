2d1d4a18ef5ffa1d7acc84763e1c1e2a9807a191
kirangp <kirangp@gmail.com>
2016-04-13 07:02:45 +1000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,29 @@
+class Genre
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+    genre
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+require 'pry'
+class Song
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def  self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end
+
+
+end
