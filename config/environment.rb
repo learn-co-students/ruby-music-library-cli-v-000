0c521fa34356c5e4726d6aa9ce1e77a559f8d23a
CodeByLine <yleventhal@ymail.com>
2016-04-26 14:21:08 -0400

    more or less

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,15 @@ Bundler.require
 module Concerns
 end
 
+require_relative '../lib/concerns/findable'
+
 require_all 'lib'
+
+
+# require 'bundler'
+# Bundler.require
+#
+# module Concerns
+# end
+#
+# require_all 'lib'
