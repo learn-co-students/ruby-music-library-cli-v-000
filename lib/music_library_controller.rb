ff14c61bcf3a727eb7ffbbb1d2413d7e5470a6fd
karuna24s <karuna24s@gmail.com>
2016-04-28 19:27:31 -0400

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -15,30 +15,62 @@ class MusicLibraryController
       input = gets.strip
       case input
       when "list songs"
-        Song.all.each_with_index{|song,index|
-        puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
-      when "list artists"
-        Artist.all.each{|artist| puts artist.name}
-      when "list genres"
-        Genre.all.each{|genre| puts genre.name}
-      when "play song"
-        puts "Which song would you like to play? Hint: Pick a number"
-        song = gets
-        song = song.to_i
-        s = Song.all[song - 1]
-        puts "Playing #{s.artist.name} - #{s.name} - #{s.genre.name}"
+          songs
+        when "list artists"
+          artists
+        when "list genres"
+          genres
+        when "play song"
+          play_song
+        when "list artist"
+          list_artist
+        when "list genre"
+          list_genre
+        end
+    end
+  end
 
-      when "list artist"
-        puts "Enter an artist name:"
-        artist = gets
-        artist = Artist.find_by_name(artist)
-        artist.songs.each{|s| puts "#{artist.name} - #{s.name} - #{s.genre.name}"}
-      when "list genre"
-        puts "Enter an genre:"
-        genre = gets
-        genre = Genre.find_by_name(genre)
-        genre.songs.each{|s| puts "#{s.artist.name} - #{s.name} - #{s.genre.name}"}
+  def songs
+    Song.all.each_with_index do |song,index|
+      puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+  end
+
+  def artists
+    Artist.all.each do |artist|
+      puts "#{artist.name}"
+    end
+  end
+
+  def genres
+    Genre.all.each do |genre|
+      puts "#{genre.name}"
+    end
+  end
+
+  def play_song
+    self.songs
+    puts "Which song would you like to play? Hint: Pick a number"
+    input = gets.strip.to_i
+    Song.all.each_with_index do |song,index|
+      if input == index + 1
+        puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
       end
     end
   end
+
+  def list_artist
+    puts "Enter an artist name:"
+    artist = gets
+    artist = Artist.find_by_name(artist)
+    artist.songs.each{|s| puts "#{s.artist.name} - #{s.name} - #{s.genre.name}"}
+  end
+
+  def list_genre
+    puts "Enter a genre:"
+    genre = gets
+    genre = Genre.find_by_name(genre)
+    genre.songs.each{|s| puts "#{s.artist.name} - #{s.name} - #{s.genre.name}"}
+  end
+
 end
