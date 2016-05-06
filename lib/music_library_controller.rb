5b46e31037011fdf234cdaff852215e47ca785cd
Christian Whitesides <whitesidescr@gmail.com>
2016-05-06 17:43:33 -0600

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -2,15 +2,37 @@ class MusicLibraryController
   attr_reader :path
 
   def initialize(path="./db/mp3s")
-    @path = path
-    music_file = MusicImporter.new(path).import
+    @path = MusicImporter.new(path).import
   end
 
   def call
-    input = ""
+    input = gets
 
     while input != "exit"
-      input = gets.chomp
+
+      counter = 1
+      playlist = Song.all.collect do |s|
+        puts "#{counter}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+        counter += 1
+      end
+
+      if input == "list songs"
+        playlist.each do |s|
+          puts s
+        end
+      elsif input == "list artists"
+        Artist.all.each do |a|
+          puts a.name
+        end
+      elsif input == "list genres"
+        Genre.all.each do |g|
+          puts g.name
+        end
+      else 
+        s = Song.all[0]
+        puts "Playing #{s.artist.name} - #{s.name} - #{s.genre.name}"
+      end
+      input = gets
     end
   end
 end
