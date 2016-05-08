0b8825d6dd10ceb82d54058f51e2d71c2c053ef2
hangloos <ericloos00@gmail.com>
2016-05-08 18:14:36 -0400

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,6 +17,15 @@ input = gets
 until input == "exit"
 if input == "list songs"
 list_songs
+elsif input == "list artists"
+  list_artists
+elsif input == "list genres"
+  list_genres
+  elsif input = "play a song"
+  play_a_song
+  elsif input == "list artists songs"
+  list_artists_songs
+    
 end
 input = gets
 end
@@ -27,12 +36,33 @@ def list_songs
   Song.all.each_with_index {|x,index|
     print index + 1,". ",x.artist.name," - ",x.name," - ",x.genre.name
  
-
-
 }
 end
 
 
+def list_artists
+Song.all.each {|x| puts x.artist.name}
+
+end
+
+def list_genres
+Song.all.each {|x| puts x.genre.name}
+end
+
+def play_a_song
+
+ Song.all.each_with_index {|x,index|
+    print "Playing ",x.artist.name," - ",x.name," - ",x.genre.name
+}
+end
+
+def list_artists_songs
+Song.all.each {|x| 
+print x.artist.name," - ",x.name," - ",x.genre.name
+
+}
+
+end
 
 
 end
