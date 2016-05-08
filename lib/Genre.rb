69089731af88831ffdeb6a788fb374e52f674eee
brad72287 <brad72287@gmail.com>
2016-05-08 16:09:47 +0000

    Done.

---
+++ lib/Genre.rb
@@ -0,0 +1,35 @@
+require 'pry'
+
+class Genre
+
+  attr_accessor :name, :songs, :artists
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(input)
+    genre = Genre.new(input)
+    genre.save
+    genre
+  end
+  
+  def artists
+    self.songs.map {|x| x.artist }.uniq
+  end
+
+end
--- lib/Song.rb
+++ lib/Song.rb
@@ -2,11 +2,25 @@ require 'pry'
 
 class Song
 
-  attr_accessor :name
+  attr_accessor :name, :artist, :genre
   @@all = []
 
-  def initialize(name)
+  def initialize(name, artist=nil, genre=nil)
     @name = name
+    @artist = artist
+    @genre = genre
+    if genre!=nil
+      self.genre.songs << self unless self.genre.songs.include?(self)
+    end
+    if artist!=nil
+      self.artist.add_song(self)
+    end
+    #self.artist.songs << self
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
   end
 
   def self.all
@@ -21,12 +35,29 @@ class Song
     @@all << self
   end
 
-  def self.create(name)
-    bla = self.new(name).save
-    puts @@all
-    puts "hello"
-    puts self.all
-    #bla.save
+  def self.create(input)
+    song = Song.new(input)
+    song.save
+    song
+  end
+
+  def genre=(genre)
+    @genre = genre
+    self.genre.songs << self unless self.genre.songs.include?(self)
+  end
+
+  def self.find_by_name(name)
+    @@all.select {|x| x.name == name}[0]
+  end
+
+  def self.find_or_create_by_name(name)
+
+    if find_by_name(name)==nil
+      Song.new(name).save
+    else
+      find_by_name(name).name = name
+    end
+    name
     #binding.pry
   end
 
