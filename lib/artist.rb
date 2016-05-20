58dbf990cc7867dc9ee669b028c59762f47ade9a
Jason <jasonkwong11@gmail.com>
2016-05-20 19:34:19 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,13 @@
 require 'pry'
 
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name, :songs, :genres
   @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
+    @genres = []
     self.save
   end
 
@@ -31,7 +32,20 @@ class Artist
   end
 
   def add_song(song)
-    song.artist = self
-    @songs << song
+    song.artist = self unless song.artist == self
+    @songs << song unless @songs.detect{|s| s.name == song.name }
   end
+
+  def genres
+    self.songs.each do |song|
+      @genres << song.genre unless @genres.detect{|genre_object| genre_object.name == song.genre.name} 
+    end
+    @genres
+  end
+
 end
+
+
+
+
+
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,12 @@
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, :genre
 
   @@all = []
 
-  def initialize(name)
+  def initialize(name, artist=0, genre=0)
     @name = name
+    artist.add_song(self) if artist != 0
+    self.genre = genre if genre != 0 
     self.save
   end
 
@@ -28,4 +30,10 @@ class Song
     @artist = artist
     artist.add_song(self)
   end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.detect{|s| s.genre == genre }
+  end
+
 end
