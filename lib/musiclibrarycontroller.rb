2a756f15121945188ff4707738d0eeadb81c349c
kaileeagray <kaileeagray@gmail.com>
2016-05-24 15:53:02 -0500

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -13,6 +13,23 @@ class MusicLibraryController
     until input == "exit"
       puts "Please enter input:"
       input = gets
+      if input == "list songs"
+        index = 1
+        Song.all.each do |song|
+          puts index.to_s + ". " + song.artist.name + " - " + song.name + " - " + song.genre.name
+          index += 1
+        end
+      elsif input == "list artists"
+        Song.all.each do |song|
+          puts song.artist.name
+        end
+      elsif input == "list genres"
+        Song.all.each do |song|
+          puts song.genre.name
+        end
+      elsif input == "play"
+        puts "Playing " + song.artist.name + " - " + song.name + " - " + song.genre.name
+      end
     end
   end
 
