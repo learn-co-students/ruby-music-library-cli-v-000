8fc9a750db1306834beb58b3e343eeb263cfd0b2
Drew Pham <drewvpham@gmail.com>
2016-05-24 19:31:45 -0500

    Done.

---
+++ lib/music_importer.rb
@@ -0,0 +1,19 @@
+class MusicImporter
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{|f| f.gsub("#{path}/", "")}
+  end
+
+  def import
+    files.each do |file|
+
+      Song.create_from_filename(file)
+end
+
+  end
+end
