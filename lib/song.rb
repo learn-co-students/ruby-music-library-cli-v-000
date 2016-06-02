91f4516788149b0cde0237ea810fca850525fdf9
Yechiel Kalmenson <chilikkal@gmail.com>
2016-06-02 01:00:06 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@all = []
+  extend CONCERNS::FINDABLE
+
+  #class methods
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist != nil
+    self.genre = genre if genre != nil
+  end
+  def self.all
+    @@all
+  end
+  def self.destroy_all
+    self.all.clear
+  end
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+
+
+  #instance methods
+  def save
+    self.class.all << self
+  end
+  def artist=(artist)
+    @artist = artist
+    @artist.add_song(self)
+  end
+  def genre=(genre)
+    @genre = genre
+    @genre.add_song(self)
+  end
+end
