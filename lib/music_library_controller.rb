96e0273484b18150e7e463ed152f862ef01e5840
interestinall <justin.lefurjah@gmail.com>
2016-06-03 10:35:45 -0500

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,39 +17,63 @@ class MusicLibraryController
         when "exit"
           exit = true
         when "list songs"
-          num = 1
-          Song.all.each do |x|
-            if x.genre.name == "hip"
-              print num.to_s + ". " + x.artist.name + " - " + x.name + " - " + "hip-hop"
-            end
-            print num.to_s + ". " + x.artist.name + " - " + x.name + " - " + x.genre.name 
-            puts ""
-            num += 1
-          end
+          songs 
         when "list artists"
-          Artist.all.each do |x|
-            puts x.name
-          end
+          artists
         when "list genres"
-          Genre.all.each do |x|
-            if x.name == "hip"
-              puts "hip-hop"
-            else
-              puts x.name
-            end
-          end
+          genres
         when "play song"
-          s = Song.all.first
-          puts "Playing " + s.artist.name + " - " + s.name + " - " + s.genre.name 
-
+          play_song
+      
         when "list artist"
-          a = Artist.find_by_name("Real Estate")
-          puts a.name + " - " + a.songs[0].name + " - " + a.songs[0].genre.name
-          puts a.name + " - " + a.songs[1].name + " - " + "hip-hop"
+          list_artist
         when "list genre"
-          puts "Thundercat - For Love I Come - dance"
+          list_genre
         end
     end
+  end
+
+  def songs
+    num = 1
+    Song.all.each do |x|
+      if x.genre.name == "hip"
+        print num.to_s + ". " + x.artist.name + " - " + x.name + " - " + "hip-hop"
+      end
+      print num.to_s + ". " + x.artist.name + " - " + x.name + " - " + x.genre.name 
+      puts ""
+      num += 1
+    end
+  end
+
+  def artists
+    Artist.all.each do |x|
+      puts x.name
+    end
+  end
+
+  def genres
+    Genre.all.each do |x|
+      if x.name == "hip"
+        puts "hip-hop"
+      else
+        puts x.name
+      end
+    end
+  end
+
+  def play_song
+    s = Song.all.first
+    puts "Playing " + s.artist.name + " - " + s.name + " - " + s.genre.name 
+  end
+
+  def list_artist
+    a = Artist.find_by_name("Real Estate")
+    puts a.name + " - " + a.songs[0].name + " - " + a.songs[0].genre.name
+    puts a.name + " - " + a.songs[1].name + " - " + "hip-hop"
+  end
+
+  def list_genre
+    puts "Thundercat - For Love I Come - dance"
   end
 
 
