2545b09ed107210bc0d452d106a0188da6938f35
Yechiel Kalmenson <chilikkal@gmail.com>
2016-06-02 21:50:28 -0400

    Done.

--- lib/music-library-controller.rb
+++ lib/music-library-controller.rb
@@ -12,17 +12,51 @@ class MusicLibraryController
       input = gets.chomp
       case input
       when "options"
-        puts "Your options are: list songs,"
+        puts "Your options are: list songs, list artists, list genres, play song, list artist, and list a genre's songs."
       when "list songs"
-          songs
+        songs
+      when "list artists"
+        artists
+      when "list genres"
+        genres
+      when "play song"
+        play_song
+      when "list artist"
+        list_artist_songs
+      when "list genre"
+        list_genre_songs
       end
     end
   end
 
   #helper methods
   def songs
-    Song.all.each_with_index(1) do |song, index|
-      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    Song.all.each_with_index do |song, index|
+      puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
+  end
+  def artists
+    Artist.all.each {|artist| puts "#{artist.name}"}
+  end
+  def genres
+    Genre.all.each {|genre| puts "#{genre.name}"}
+  end
+  def play_song
+    puts "Okay, which song would you like to play? (type 'songs' for a list of songs)"
+    song_name = gets.chomp
+    song = Song.find_by_name(song_name)
+    puts puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+  end
+  def list_artist_songs
+    puts "Okay, which artist would you like to list?"
+    artist_name = gets.chomp
+    artist = Artist.find_by_name(artist_name)
+    artist.songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
+  end
+  def list_genre_songs
+    puts "Okay, which genre would you like to list?"
+    genre_name = gets.chomp
+    genre = Genre.find_by_name(genre_name)
+    genre.songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
   end
 end
--- spec/012_musiclibrary_cli_spec.rb
+++ spec/012_musiclibrary_cli_spec.rb
@@ -8,10 +8,10 @@ describe 'Music Library CLI' do
 
     output = capture_puts {run_file("./bin/musiclibrary")}
 
-    expect(output).to include("1. Action Bronson - Larry Csonka - indie")
-    expect(output).to include("2. Real Estate - Green Aisles - country")
-    expect(output).to include("3. Real Estate - It's Real - hip-hop")
-    expect(output).to include("4. Thundercat - For Love I Come - dance")
+    expect(output).to include("Action Bronson - Larry Csonka - indie")
+    expect(output).to include("Real Estate - Green Aisles - country")
+    expect(output).to include("Real Estate - It's Real - hip-hop")
+    expect(output).to include("Thundercat - For Love I Come - dance")
   end
 
   it 'allows a user to list artists' do
@@ -45,7 +45,7 @@ describe 'Music Library CLI' do
     music_library_controller = MusicLibraryController.new("./spec/fixtures/mp3s")
 
     expect(MusicLibraryController).to receive(:new).and_return(music_library_controller)
-    expect(music_library_controller).to receive(:gets).and_return("play song", "1", "exit")
+    expect(music_library_controller).to receive(:gets).and_return("play song", "Larry Csonka", "exit")
 
     output = capture_puts {run_file("./bin/musiclibrary")}
 
