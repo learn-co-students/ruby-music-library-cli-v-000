ef128bc478f3ff5c565023ed0ee4e49228c89cba
joelbitart1986 <joelbitar1986@gmail.com>
2016-06-07 17:45:44 -0400

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -14,10 +14,24 @@ class MusicLibraryController
 			puts "What would you like me to do?"
 			puts "Type 'options' for a list of available commands or type 'exit' to exit"
 			x = gets.strip
-			if x != "exit"
+			if x == "list songs"
+				self.list_songs			
+			elsif x != "exit"
 				self.call
 			end
 		end				
+	end
+	
+	def commands
+		puts "list songs"
+	end
+	
+	def list_songs
+		Song.all.each_with_index do |o, i|
+			binding.pry
+			puts "#{i + 1}. #{o}"
+			
+		end
 	end
 	
 	
