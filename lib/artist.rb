4255a43f64ed5df79a54ede4cd8ac3e8c506b649
Jonathan Stone <jon_p_stone@yahoo.com>
2016-06-07 20:17:12 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
-  include Concerns
-  attr_accessor :name
+  extend Concerns::Findable
+  attr_accessor :name, :songs
   @@all = []
 
   def initialize(name)
@@ -8,25 +8,32 @@ class Artist
     @songs = []
   end
 
-  def save
-    @all << self
-  end
-
-  def destroy_all
-    @all.clear
-  end
-
   def self.create(name)
-    self.new(name).tap {|artist| artist.save}
+    new(name).tap{|n| n.save}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
   def add_song(song)
-    @songs << song
+    song.artist = self unless song.artist == self
+    @songs << song unless @songs.include?(song)
   end
 
   def genres
-    self.songs.collect do |song|
-      song.genre
-    end
+    self.songs.collect{|s| s.genre}.uniq
+  end
+
+  def to_s
+    self.name
+  end
+
+  def save
+    @@all << self
   end
 end
