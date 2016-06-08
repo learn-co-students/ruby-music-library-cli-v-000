83b0cfda0bbd2a2a9c611df0d5f562bdae6d55c4
tpstar <hanlee12@gmail.com>
2016-06-07 22:38:11 -0400

    code music library cli

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,14 +1,46 @@
+require 'pry'
+
 class MusicLibraryController
 
   attr_accessor :path
 
   def initialize(path = "./db/mp3s")
     @path = path
-    MusicImporter.new(path).import
+    @music_importer = MusicImporter.new(path)
+    @music_importer.import
   end
 
   def call
-    input = gets.strip until input == "exit"
-  end
+    input = ""
+    until input == "exit"
+      input = gets.strip
+      case input
+      when "list songs"
+        i = 0
+        @music_importer.files.each do |file|
+          i += 1
+          puts "#{i}. #{file.gsub(".mp3","")}"
+        end
+      when "list artists"
+        Artist.all.each {|artist| puts artist.name}
+      when "list genres"
+        Genre.all.each {|genre| puts genre.name}
+      when "play song"
+        second_input2index = gets.chomp.to_i - 1
+        puts "Playing #{@music_importer.files[second_input2index].gsub(".mp3","")}"
+      when "list artist"
+        selected_artist = gets.chomp
+        @music_importer.files.select { |file| file.split(" - ")[0] == selected_artist}.each do |file|
+          puts "#{file.gsub(".mp3","")}"
+        end
+      when "list genre"
+        selected_genre = gets.chomp
+        @music_importer.files.select { |file| file.split(" - ")[-1].gsub(".mp3","") == selected_genre}.each do |file|
+          puts "#{file.gsub(".mp3","")}"
+        end
+      end #case
+    end #until
+
+  end #call
 
 end
