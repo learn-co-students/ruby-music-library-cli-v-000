7681f42c98c793a20a5f9ab0dd87ae05d578f47a
bmolina-nyc <bruce.d.molina@gmail.com>
2016-06-20 20:17:11 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,110 @@
 require 'bundler'
 Bundler.require
 
+require_all 'lib'
+
 module Concerns
 end
 
-require_all 'lib'
+module Concerns::Findable
+end
+
+class Song
+  attr_accessor :name, :artist 
+  
+  @@all = [] 
+
+  def initialize(name)
+    @name = name  
+    @@all << self 
+  end
+
+  def artist=(artist)
+   @artist = artist
+   artist.songs << self
+  end
+
+
+  def Song.all 
+    @@all 
+  end 
+
+  def Song.destroy_all 
+    self.all.clear 
+  end
+
+  def save 
+    @@all
+  end
+
+  def Song.create(song)
+    item = Song.new(song)
+  end
+end
+
+
+class Artist
+  attr_accessor :name
+  attr_reader :songs 
+
+  @@all = [] 
+
+  def initialize(name)
+    @name = name 
+    @@all << self 
+    @songs = []
+  end
+
+
+
+  def Artist.all 
+    @@all 
+  end
+
+  def Artist.destroy_all 
+    Artist.all.clear 
+  end
+
+  def save 
+    @@all
+  end
+
+  def Artist.create(artist)
+    item = Artist.new(artist)
+  end
+
+  def add_song(song)
+    songs << song if songs.include?(song.name)
+    song.artist = self unless !song.artist.nil?
+  end
+
+end
+
+class Genre
+  attr_accessor :name 
+
+  @@all = [] 
+
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+
+  def Genre.all 
+    @@all 
+  end
+
+  def Genre.destroy_all
+    Genre.all.clear
+  end
+
+  def save 
+    Genre.all 
+  end
+
+  def Genre.create(genre)
+    item = Genre.new(genre)
+  end
+end
+
+
