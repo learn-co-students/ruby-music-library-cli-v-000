3ff990831f49eda6e51c906eb05b3be913b3c880
mmelmer <mikemelmer@gmail.com>
2016-06-22 19:28:23 -0400

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,14 @@
+module Concerns::Findable
+    def find_by_name(name)
+      all.each{|song| return song if song.name == name}
+    end
+
+    def find_or_create_by_name(name)
+      if find_by_name(name) != nil
+        find_by_name(name)
+      else
+        self.create(name)
+      end
+
+    end
+end
