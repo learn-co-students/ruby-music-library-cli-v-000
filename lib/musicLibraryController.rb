a53f4e1c032be593faea270eeae1315d3ffb4b7e
bmolina-nyc <bruce.d.molina@gmail.com>
2016-06-23 21:09:48 -0400

    Done.

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -2,45 +2,81 @@ class MusicLibraryController
 
   attr_accessor :path, :files
 
+  @@all = []
+
   def initialize(path = "./db/mp3s")
     files = MusicImporter.new(path).import
+    @@all << files 
     @files = files 
   end
 
-  def number_files(files)
-    i = 0 
-    index = 1 
-
-    while i < files.length
-      files[i] = "#{index}. " + "#{files[i]}"
-      index += 1
-      i += 1  
-    end
-    files 
+  def MusicLibraryController.all 
+    @@all
   end
 
-  def play_files(files)
-    items = number_files(files).each do |file|
-      file.gsub!(/[1.]/, "Playing")
-    end
-    items
-  end
 
   def call      
-    puts number_files(files)
-    puts play_files(files)
-    
-    while true 
+    input = ""
+
+    while input != "exit"
+      puts "Welcome to Your Music Library!"
+      puts "What would you like to do? - type 'list songs, list artists, list genres, play song, or list artist'"
+
       input = gets.chomp
 
       case input 
-        when "exit"
-          break
-        end
-     end
+        when "list songs"
+          puts songs
+        when "list artists"
+          puts artists
+        when "list genres"
+          puts genres
+        when "play song"
+          puts play_song
+        when "list artist"
+          puts artists
+        when "list genre"
+          puts genres
+        end # case 
 
-  end
+    end # while 
 
+
+  end  # def call 
+    
+
+    def songs
+      i = 0 
+      index = 1 
+
+      while i < files.length
+        files[i] = "#{index}. " + "#{files[i]}"
+        index += 1
+        i += 1  
+      end
+        files 
+    end
+    
+    def artists
+      MusicLibraryController.all.each do |artist|
+        puts "#{artist}"
+      end
+    end
+
+    def genres
+      MusicLibraryController.all.each do |genres|
+        puts "#{genres}"
+      end
+    end
+
+    def play_song
+      i = 0
+      while i < files.length
+        files[i] = "Playing " + "#{files[i]}"
+        i += 1  
+      end
+      files
+    end
 
 
 end
