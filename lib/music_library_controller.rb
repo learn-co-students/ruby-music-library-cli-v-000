87c9b28f6f3570c7e742effab87e02ac44147d13
Jennifer Pazos <jmp521@gmail.com>
2016-07-03 17:24:54 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -14,19 +14,51 @@ class MusicLibraryController
       user_input = gets.strip
 
       case user_input
-      when "songs"
-      
+      when "list songs"
 
-      when "artist"
+        Song.all.each_with_index do |song, index|
+          index +=1
+        puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+        end
 
-      when "genre"
+      when "list artists"
+        Artist.all.each do |artist|
+          puts "#{artist.name}"
+
+        end
+      when "list genres"
+        Genre.all.each do |genre|
+          puts "#{genre.name}"
+
+        end
+      when "play song"
+        Song.all.each do |songs|
+          puts "Playing #{songs.artist.name} - #{songs.name} - #{songs.genre.name}"
+
+        end
 
 
+      when "list artist"
+        puts "Which artist?"
+        input = gets.strip
+        found = Artist.find_by_name(input)
+        found.songs.each do |each_song|
+
+          puts "#{found.name} - #{each_song.name} - #{each_song.genre.name}"
+        end
+
+      when "list genre"
+        puts "Which genre"
+        input = gets.strip
+        found = Genre.find_by_name(input)
+        found.songs.each do |each_genre|
+          puts "#{each_genre.artist.name} - #{each_genre.name} - #{found.name}"
+
+        end
       end
-
-      # user_input = "exit"
     end
   end
+
 
 
 end
