7798cff09774f976b6e80769063f8bcaa1cf9be6
Perla Yates <pianistps@yahoo.com>
2016-07-19 16:56:19 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,12 +1,21 @@
 class MusicImporter
   attr_accessor :path
+  attr_reader :files
 
   def initialize(path)
     @path = path
   end
 
   def files
-    
+    @files = Dir.entries("#{@path}")
+    @files.delete_if {|dir| dir == "." || dir == ".."}
+    @files
+  end
+
+  def import
+    files.each do |file|
+      Song.create_from_filename(file)
+    end
   end
 
 end
--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -2,7 +2,7 @@ class MusicLibraryController
   attr_accessor :path
 
   def initialize(path = "./db/mp3s")
-    
+    @path = path
   end
 
 end
