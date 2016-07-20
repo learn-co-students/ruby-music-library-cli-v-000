273660246d2ce6f62ba9627f45e5a8acf56e969a
Perla Yates <pianistps@yahoo.com>
2016-07-20 02:29:25 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -26,7 +26,7 @@ class MusicLibraryController
 
   def songs
     Song.all.each.with_index(1) do |song, index|
-      puts "#{index}. #{song}"
+      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
 
