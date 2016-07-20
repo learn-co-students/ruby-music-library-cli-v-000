e446eb68227b8acce922b6b489ffce374317b9a8
Perla Yates <pianistps@yahoo.com>
2016-07-20 03:48:19 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -22,6 +22,10 @@ class MusicLibraryController
         genres
       when "play song"
         play_song
+      when "list artist"
+        list_songs
+      when "list genre"
+        list_genres_songs
       end
     end
   end
@@ -47,7 +51,28 @@ class MusicLibraryController
   def play_song
     puts "What song number would you like to play?"
     song_input = gets.strip
-    puts "Playing #{Song.sort_all_alphabet[song_input.to_i-1]}"
+    song = Song.sort_all_alphabet[song_input.to_i-1]
+    puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+  end
+
+  def list_songs
+    puts "Which artist would you like to list the songs of?"
+    artist_input = gets.strip
+    if artist = Artist.find_by_name(artist_input)
+      artist.songs.each do |s,i|
+        puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+      end
+    end
+  end
+
+  def list_genres_songs
+    puts "Which genre would you like to list the songs of?"
+    genre_input = gets.strip
+      if genre = Genre.find_by_name(genre_input)
+        genre.songs.each do |s,i|
+          puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+        end
+      end
   end
 
 end
