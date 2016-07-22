d7d18b63410273975d0ea2b9bbd15dba90a743a2
Lenn Hypolite <lenntriniman@hotmail.com>
2016-07-22 01:57:26 +0000

    need to finish the call method

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,5 +6,11 @@ attr_accessor :path
     path = MusicImporter.new(path).import  # creates a MusicImporter with that path and imports the music
   end
 
+  def call
+    input = gets.strip
+    until input != "exit"
+      input
+    end
+  end
 
 end
