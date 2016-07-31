d1632da241e4cfdf4f98014ba4c4fc9e758d874c
Brad Watson <brad.watson.orlando@gmail.com>
2016-07-31 19:43:10 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,14 +1,13 @@
-module Concerns::Findable
+module Concerns
+  module Findable
 
-  def find_by_name(name)
-    self.all.detect{|a| a.name == name}
-  end
-
-  def find_or_create_by_name(name)
-    self.find_by_name(name)
-    if !self.find_by_name(name)
-      self.create(name)
+    def find_by_name(name)
+      self.all.detect{|a| a.name == name}
     end
-  end
 
+    def find_or_create_by_name(name)
+     self.find_by_name(name) || self.create(name)
+    end
+
+  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -42,4 +42,15 @@ class Song
     end
   end
 
+  def self.find_by_name(name)
+    self.all.detect{|a| a.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name)
+    if !self.find_by_name(name)
+      self.create(name)
+    end
+  end
+
 end
