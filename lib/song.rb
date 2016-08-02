a30410646f11db6eb9e4727385a0633fa5d75f65
Elias Ventura <eliasventura23@gmail.com>
2016-08-02 21:15:25 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,6 @@
 class Song
-
   attr_accessor :name, :artist, :genre
-
-@@all = []
+  @@all = []
 
   def initialize(name, artist = nil, genre = nil)
     @name = name
@@ -10,20 +8,42 @@ class Song
     self.genre = genre if genre
   end
 
+  def self.create(name, artist = nil, genre = nil)
+    new(name, artist, genre).tap{|s| s.save}
+  end
+
   def self.all
     @@all
   end
 
-  def save
-    @@all << self
+  def self.find_by_name(name)
+    self.all.detect{|s| s.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts.first, parts[1], parts[2].gsub(".mp3", "")
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts.first, parts[1], parts[2].gsub(".mp3", "")
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.create(song_name, artist, genre)
   end
 
   def self.destroy_all
     @@all.clear
-  end
-
-  def self.create(song)
-    new(song).tap{|s| s.save}
   end
 
   def artist=(artist)
@@ -36,11 +56,9 @@ class Song
     genre.songs << self unless genre.songs.include?(self)
   end
 
-  def self.find_by_name(name)
-      self.all.detect{|s| s.name == name}
-    end
-
-    def self.find_or_create_by_name(name)
-      self.find_by_name(name) || self.create(name)
-    end
+  def save
+    @@all << self
   end
+
+
+end
