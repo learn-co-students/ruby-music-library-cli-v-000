287d173f3d87ecb9617789fa9c30fbe728940fd2
KerryAlsace <kerrydougherty@gmail.com>
2016-08-09 17:30:37 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,2 +1,28 @@
 class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 end
