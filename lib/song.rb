5f50f89d82cf4329c4ed6fd4d5395a099b024193
William Jones <enquiries@forestfed.com>
2016-08-10 21:34:06 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = self.new(name)
+    puts "song = #{song}"
+    puts "@@all1 = #{@@all}"
+    song.save
+    puts "@@all2 = #{@@all}"
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+end
