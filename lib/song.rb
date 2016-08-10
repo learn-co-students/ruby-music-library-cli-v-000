4c18bc87a1133d4cf2fd95e2d248770143e253ed
KerryAlsace <kerrydougherty@gmail.com>
2016-08-09 20:56:41 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -37,11 +37,11 @@ class Song
     @@all.clear
   end
 
-  def find_by_name(name)
+  def self.find_by_name(name)
     self.all.detect {|song| song.name == name}
   end
 
-  def find_or_create_by_name(name)
+  def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create(name)
   end
 
