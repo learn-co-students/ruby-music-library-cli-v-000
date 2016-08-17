585dc409a9ff579cb9087a6e165fff44a87ecf3a
yeungn <nin.yeung@gmail.com>
2016-08-17 12:07:50 -0400

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 module Concerns::Findable
   def find_by_name(name)
-    self.all.detect {|o| o.name = name}
+    self.all.detect {|o| o.name == name}
   end
 
   def find_or_create_by_name(name)
