89acc21678361e2f4dcfa78e2206382901b53167
christinechoi <smi77y@gmail.com>
2016-08-16 23:06:27 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -7,37 +7,3 @@ end
 
 require_all 'lib'
 
-
-class Song
-  attr_accessor :name
-  
-  @@all = []
-
-  def initialize(name)
-    @name = name
-  end
-
-  def self.all
-    @@all 
-  end
-
-  def self.destroy_all
-    @@all.clear
-  end
-
-  def save
-    @@all << self
-  end
-
-  def self.create(string)
-    song = self.new(string)
-    song.save
-  end
-
-
-
-
-
-
-end
-
---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+
+
+class Song
+  attr_accessor :name
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+
+
+
+
+
+end
