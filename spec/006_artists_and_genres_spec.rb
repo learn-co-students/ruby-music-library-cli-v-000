17dcf53ffdba58ae1162d1ef8b87b2edfd2f6730
gabe <gj+code@mail.co.de>
2016-08-17 00:26:42 -0400

    correct song titles

--- spec/006_artists_and_genres_spec.rb
+++ spec/006_artists_and_genres_spec.rb
@@ -5,8 +5,8 @@ describe "Artists have many genres through songs" do
     it 'returns the unique genres belonging to all the songs of the artist' do
       genre = Genre.new("indie rock")
       artist = Artist.new("Neutral Milk Hotel")
-      song = Song.new("In an Aeroplane Over the Sea", artist, genre)
-      song = Song.new("Two Headed Boy pt 1", artist, genre)
+      song = Song.new("In the Aeroplane over the Sea", artist, genre)
+      song = Song.new("Two-Headed Boy Pt. 1", artist, genre)
 
       expect(artist.genres).to include(genre)
       expect(artist.genres.size).to eq(1)
@@ -17,7 +17,7 @@ describe "Artists have many genres through songs" do
       electro_pop = Genre.new("electro pop")
       artist = Artist.new("The Magnetic Fields")
 
-      song = Song.new("Long-forgotten Fairytale", artist, electro_pop)
+      song = Song.new("Long-Forgotten Fairytale", artist, electro_pop)
       song = Song.new("The Book of Love", artist, indie_rock)
       song = Song.new("Papa was a Rodeo", artist, indie_rock)
 
@@ -30,7 +30,7 @@ describe "Artists have many genres through songs" do
       electro_pop = Genre.new("electro pop")
       artist = Artist.new("The Magnetic Fields")
 
-      song = Song.new("Long-forgotten Fairytale", artist, electro_pop)
+      song = Song.new("Long-Forgotten Fairytale", artist, electro_pop)
 
       expect(artist.genres).to match_array([electro_pop])
       expect(artist.instance_variable_defined?(:@genres)).to be_falsey
@@ -49,7 +49,7 @@ describe "Genres have many artists through songs" do
       neutral_milk_hotel = Artist.new("Neutral Milk Hotel")
       the_magnetic_fields = Artist.new("The Magnetic Fields")
 
-      Song.new("In an Aeroplane Over the Sea", neutral_milk_hotel, genre)
+      Song.new("In the Aeroplane over the Sea", neutral_milk_hotel, genre)
       Song.new("The Book of Love", the_magnetic_fields, genre)
       Song.new("Papa was a Rodeo", the_magnetic_fields, genre)
 
@@ -69,7 +69,7 @@ describe "Genres have many artists through songs" do
       expect(genre.artists).to match_array([the_magnetic_fields])
       expect(genre.instance_variable_defined?(:@artists)).to be_falsey
 
-      Song.new("In an Aeroplane Over the Sea", neutral_milk_hotel, genre)
+      Song.new("In the Aeroplane over the Sea", neutral_milk_hotel, genre)
 
       expect(genre.artists).to match_array([neutral_milk_hotel, the_magnetic_fields])
     end
