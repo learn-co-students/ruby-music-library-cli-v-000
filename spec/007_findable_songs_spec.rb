d0c612a803471ea54d321de1cb9f2b8649ea49d9
gabe <gj+code@mail.co.de>
2016-08-17 00:31:43 -0400

    capitalize 'Over'

--- spec/007_findable_songs_spec.rb
+++ spec/007_findable_songs_spec.rb
@@ -1,21 +1,21 @@
 describe 'Findable Songs' do
   describe ".find_by_name" do
     it 'finds a song instance in @@all by the name property of the song' do
-      song = Song.new("In the Aeroplane over the Sea")
+      song = Song.new("In the Aeroplane Over the Sea")
       song.save
 
-      expect(Song.find_by_name("In the Aeroplane over the Sea")).to eq(song)
+      expect(Song.find_by_name("In the Aeroplane Over the Sea")).to eq(song)
     end
   end
 
   describe ".find_or_create_by_name" do
     it 'finds or creates a song by name maintaining uniqueness of objects by their name property' do
-      song_1 = Song.find_or_create_by_name("In the Aeroplane over the Sea")
-      song_2 = Song.find_or_create_by_name("In the Aeroplane over the Sea")
+      song_1 = Song.find_or_create_by_name("In the Aeroplane Over the Sea")
+      song_2 = Song.find_or_create_by_name("In the Aeroplane Over the Sea")
 
       expect(song_1).to be_a(Song)
       expect(song_1).to eq(song_2)
-      expect(song_1.name).to eq("In the Aeroplane over the Sea")
+      expect(song_1.name).to eq("In the Aeroplane Over the Sea")
     end
   end
 end
