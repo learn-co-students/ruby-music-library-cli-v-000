9f89c171eac9507dc4f9c9443c4efef7743f34be
christinechoi <smi77y@gmail.com>
2016-08-16 22:22:09 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,43 @@
 require 'bundler'
+require 'pry'
 Bundler.require
 
 module Concerns
 end
 
 require_all 'lib'
+
+
+class Song
+  attr_accessor :name
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(string)
+    song = self.new(string)
+    song.save
+  end
+
+
+
+
+
+
+end
+
