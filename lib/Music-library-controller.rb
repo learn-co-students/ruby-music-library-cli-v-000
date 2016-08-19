447739548544d7cdfd4380e942514fccb96c245b
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-08-19 19:45:28 +0000

    Done.

--- lib/Music-library-controller.rb
+++ lib/Music-library-controller.rb
@@ -1,23 +1,25 @@
 require "pry"
 class MusicLibraryController
 
-  attr_reader :path
+  attr_reader :path, :imported, :song_array
+
 
   def initialize(path='./db/mp3s')
-    new = MusicImporter.new(path)
-    new.import
-
+    @imported = MusicImporter.new(path).import
   end
+
+
 
   def call
     input = ""
     while input != "exit"
-      puts "Welcome to your music library!"
-      puts "what would you like to do?"
+      #puts "Welcome to your music library!"
+      #puts "what would you like to do?"
       input = gets.chomp
       case input
       when "list songs"
-        songs
+          song_array = @imported
+          song_array.each.with_index(1) {|song, index| print  "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name} " }
       end
 
     end
