fdf8642af44bd1962f2a719acd85171df24970c7
Bob Habig <rfhab2@gmail.com>
2016-08-30 03:10:11 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,6 @@
 module Concerns::Findable
   def find_by_name(name)
-    self.all.find{|object| object.name == name}
+    self.all.find{|object| object.name.upcase == name.upcase}
   end
 
   def find_or_create_by_name(name)
--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -37,36 +37,60 @@ class MusicLibraryController
     exit_back
   end
 
-  def exit_back
-    input = gets.strip until input == "back" || input == "exit"
-    if input == "back"
-      call
-    elsif input == "exit"
-      exit
-    end
-  end
-
   def play_song
     puts "\n\nenter the number of the song from the song list that you would like to play:\ntype 'back' to return to the menu\ntype 'exit' to leave\n\n\n"
     print ":"
-    exit_back
+    input = gets.strip
     until input == "back" || input == "exit"
+      if input.to_i - 1 < 0
+        until input.to_i - 1 >= 0
+          puts "\nplease enter a song number"
+          print ":"
+          input = gets.strip
+        end
+      end
       song_num = input.to_i - 1
       alphabetical = Song.all.sort_by!{|song| song.artist.name}
-      puts "\n\nPlaying #{alphabetical[song_num].artist.name} - #{alphabetical[song_num].name} - #{alphabetical[song_num].genre.name}"
+      puts "\n\n\nPlaying #{alphabetical[song_num].artist.name} - #{alphabetical[song_num].name} - #{alphabetical[song_num].genre.name}"
       puts "\n\nenter a song number to play a new song\ntype 'back' to go back to the main menu\nor type 'exit' to leave the program\n\n"
       print ":"
-      exit_back
-      song_num = input.to_i - 1
+      input = gets.strip
     end
+    call if input == "back"
+  end
+
+  def artist_songs
+    puts "\n\nPlease type the name of the artist to display their songs"
+    print ":"
+    input = gets.strip
+    unless input == "exit"
+      if artist = Artist.find_by_name(input)
+        artist.songs.each do |song|
+          puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+        end
+      else
+        artist_songs
+      end
+    end
+      exit_back
+  end
+
+  def genre_songs
+    puts "\n\nPlease type the name of the genre to display its songs"
+    print ":"
+    input = gets.strip
+    genre = Genre.find_by_name(input)
+      genre.songs.each do |song|
+        puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
+      exit_back
   end
 
   def call
     puts  "Type 'list songs' for a list of songs.\nType 'list artists' for a list of artists.\nType 'list genres' for a list of genres.\nType 'play song' and enter the corresponding number from the song list to play a song.\nType 'list artist' followed by an artist's name to see their songs.\nType 'list genre' followed by a genre to see all songs in a genre.\n  Or type 'exit' to leave.\n\n"
     print ":"
     input = gets.strip
-    exit if input == "exit"
-    until input == "exit"
+    unless input == "exit"
       if input == "list songs"
         list_songs
       elsif input == "list artists"
@@ -75,8 +99,18 @@ class MusicLibraryController
         list_genres
       elsif input == "play song"
         play_song
+      elsif input == "list artist"
+        artist_songs
+      elsif input == "list genre"
+        genre_songs
+      else
+        call unless input == "exit"
       end
-      exit if input = gets.strip
     end
+  end
+
+  def exit_back
+    input = gets.strip
+    call if input != "exit"
   end
 end
