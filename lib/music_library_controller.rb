80a3fc3a4e3845673674a23ac75fb923498f4245
Mike Robey <robey.mc@gmail.com>
2016-09-08 22:23:07 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,8 +1,34 @@
+require 'pry'
 class MusicLibraryController
-attr_reader :path
+attr_accessor :path
 
   def initialize(path = './db/mp3s')
     @path = path
+    MusicImporter.new(path).import
+  end
+
+  def call
+    input = gets.chomp until input == "exit"
+    case input
+    when "list songs"
+      list_songs
+    when "list artists"
+      list_artists
+    when "list genres"
+      list_genres
+  end
+end
+
+  def list_songs
+    Song.all.each_with_index {|song, index| puts "#{index}. #{song}"}
+  end
+
+  def list_artists
+    Artist.all.each {|artist| puts "#{artist}"}
+  end
+
+  def list_genres
+    Genre.all.each {|genre| puts "#{genre}"}
   end
 
 end
