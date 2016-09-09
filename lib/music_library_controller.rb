59bc181669931ef5432eb803c107803e65a0dbb2
Mike Robey <robey.mc@gmail.com>
2016-09-09 00:19:29 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,6 +17,12 @@ class MusicLibraryController
       list_artists
     when "list genres"
       list_genres
+    when "play song"
+      play_song
+    when "list artist"
+      list_artist
+    when "list genre"
+      list_genre
   end
 end
 end
@@ -31,6 +37,24 @@ end
 
   def list_genres
     Genre.all.each {|genre| puts "#{genre.name}"}
+  end
+
+  def play_song
+    input = gets.chomp.to_i
+    song = Song.all[input]
+    puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+  end
+
+  def list_artist
+    input = gets.chomp
+    artist = Artist.all.detect {|a| a.name == input}
+    artist.songs.each {|b| puts "#{b.artist.name} - #{b.name} - #{b.genre.name}"}
+  end
+
+  def list_genre
+    input = gets.chomp
+    genre = Genre.all.detect {|a| a.name == input}
+    genre.songs.each {|b| puts "#{b.artist.name} - #{b.name} - #{b.genre.name}"}
   end
 
 end
