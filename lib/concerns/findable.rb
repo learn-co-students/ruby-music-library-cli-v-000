a3e100c058919be7c6c2a9a5af4a8ed8d9c9d179
Janine Rosen <janinemrosen@gmail.com>
2016-09-30 17:58:00 +0000

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,13 @@
+module Findable
+  module ClassMethods
+    
+    def self.find_by_name(name)
+      self.all.detect{|s| s.name == name}
+    end
+
+    def self.find_or_create_by_name(name)
+      self.find_by_name(name) || self.create(name)
+    end
+
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+class Song
+  extend Concerns::Findable
+  attr_accessor :name, :artist, :genre
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def self.create(name, artist = nil, genre = nil)
+    new(name, artist, genre).tap{|s| s.save}
+  end
+
+  def self.all
+    @@all
+  end
+
+  # def self.find_by_name(name)
+  #   self.all.detect{|s| s.name == name}
+  # end
+  #
+  # def self.find_or_create_by_name(name)
+  #   self.find_by_name(name) || self.create(name)
+  # end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def save
+    @@all << self
+  end
+
+end
