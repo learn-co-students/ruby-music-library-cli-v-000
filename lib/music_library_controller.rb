96345a11a5a0d98ce4b613a4bf9f1900e3433395
Justin  Gore <justin.gore@outlook.com>
2016-11-03 22:23:20 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -15,7 +15,17 @@ class MusicLibraryController
       user_input = gets.chomp
       case user_input
       when "list songs"
-        
+        Song.all.each.with_index(1) {|song, index| puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+      when "list artists"
+        Song.all.each {|song| puts "#{song.artist.name}"}
+      when "list genres"
+        Song.all.each {|song| puts "#{song.genre.name}"}
+      when "play song"
+        Song.all.each {|song| puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+      when "list artist"
+        Song.all.each {|song| puts "-#{song.artist.name} - #{song.name} - #{song.genre.name}"}
+      when "list genre"
+        Song.all.each {|song| puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"}
       end
     end
 
