fe1262daf27ea8664192eee2e7481d1334769080
Brie Coyle <brie.coyle@gmail.com>
2016-11-28 21:03:41 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,44 @@
+class Artist
+  extend Concerns::Findable
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
+  def add_song(song)
+    if !self.songs.include?(song)
+      self.songs << song
+    else
+      self.songs.find(song)
+    end
+    song.save
+    song.artist = self if song.artist == nil
+  end
+
+  def genres
+    self.songs.collect {|song| song.genre}.uniq
+  end
+end
