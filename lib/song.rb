b7d8cd7e013305ecc94897cdb061c8e13f9e7e32
Tayari Appiah <cjhhh.ta@gmail.com>
2016-12-03 20:08:44 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ attr_accessor :name, :artist, :genre
 
   def genre=(genre)
    @genre = genre
-   genre.songs << self unless genre.songs.include?(self)
+   genre.songs << self unless artist.songs.include?(self)
   end
 
   def self.all
