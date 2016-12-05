cbfb872fc073c09b0f44a6b064ce8be721f0e42d
Callie Brazil <cmabrazil@gmail.com>
2016-12-05 20:06:12 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name, artist = nil)
+    @name = name
+    @artist = artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new(name).tap{|s| s.save}
+  end
+
+end
