1c26ce00f0cc2184a4d3eb490a474fff4500c815
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-01-27 13:19:13 -0600

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+require 'pry'
+class Song
+attr_accessor :name 
+@@all = []
+
+def initialize(name)
+  @name = name 
+end# of initialize
+
+
+def self.all
+  @@all
+end# of self.all
+
+def self.destroy_all
+  self.all.clear 
+end# of self.destroy_all
+
+
+def save
+  @@all << self
+end# of save 
+
+
+def self.create(name)
+  song = self.new(name)
+  #binding.pry 
+  @@all << song  
+end# of self.create
+
+end# of Song
