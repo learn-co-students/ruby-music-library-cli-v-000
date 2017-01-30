967100d1cdaf6c5f027248c2a4a82d5780c82189
Julie Anne McManus <julieannemcmanus@Julies-MacBook-Pro.local>
2017-01-30 10:29:11 -0500

    Done

---
+++ lib/genre.rb
@@ -0,0 +1,40 @@
+class Genre 
+	extend Concerns::Findable 
+	attr_accessor :name, :songs
+
+	@@all = []
+
+	def initialize(name)
+		@name = name 
+		@songs = []
+	end 
+
+	def self.all 
+		@@all
+	end
+
+	def self.destroy_all
+		@@all.clear 
+	end 
+
+	def save
+		@@all << self 
+	end 
+
+	def self.create(name)
+		new(name).tap{|s| s.save}
+	end
+
+	def add_song(song)
+		@songs << song
+	end
+
+	def artists 
+		self.songs.collect{|s| s.artist}.uniq
+	end
+
+	def to_s
+		self.name
+	end
+
+end
