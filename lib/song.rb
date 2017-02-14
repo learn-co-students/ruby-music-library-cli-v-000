de0273c80675c3da1625c6d47394462b8ee669c5
Ryan McMahon <ryan@mcmahon.work>
2017-02-14 21:04:32 +0000

    passing 055 specs

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,13 @@
 require 'pry'
 
 class Song
-  attr_accessor :name, :artist, :genres
+  attr_accessor :name, :artist, :genre
   @@all = []
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist= artist unless !artist
+    self.genre= genre unless !genre
   end
 
   def self.all
@@ -29,4 +30,11 @@ class Song
       @artist = artist unless !!@artist
       artist.add_song(self) unless artist.songs.include?(self)
   end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+
 end
