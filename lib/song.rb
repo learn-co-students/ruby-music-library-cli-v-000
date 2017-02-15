4fb782a254debc883144d7245cb1d4afe68886cb
Ryan McMahon <ryan@mcmahon.work>
2017-02-15 20:41:35 +0000

    passing 007 tests

--- lib/song.rb
+++ lib/song.rb
@@ -36,5 +36,18 @@ class Song
     genre.songs << self unless genre.songs.include?(self)
   end
 
+  def self.find_by_name(name)
+    all.find{ |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    search = find_by_name(name)
+    if search
+      search
+    else
+      create(name)
+    end
+  end
+
 
 end
