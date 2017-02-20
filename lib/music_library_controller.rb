36d090d9918205c18dea4834e4616eadbda7aa35
Ryan McMahon <ryan@mcmahon.work>
2017-02-20 22:09:27 +0000

    started music library controller

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,34 @@
+
+
+class MusicLibraryController
+  attr_accessor :path
+  def initialize(path = "./db/mp3s")
+    music_importer = MusicImporter.new(path)
+    music_importer.import
+  end
+
+  def call
+    input = ""
+    song_list = Song.all.sort{|a,b| a.artist.name <=> b.artist.name}
+    while input != "exit"
+      input = gets.chomp
+      case input
+      when "list songs"
+        song_list.each{|song| puts "#{song_list.index(song)+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+
+      when "list artists"
+        artist_list = Artist.all.map{|artist| artist.name}
+        artist_list.each{|artist| puts artist}
+
+      when "list genres"
+        genre_list = Genre.all.map{|genre| genre.name}
+        genre_list.each{|genre| puts genre}
+
+      when "play song"
+
+      end
+
+    end
+  end
+
+end
