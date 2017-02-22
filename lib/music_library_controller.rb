ec3efbf504c6adb867a17d52c5788395752d8f36
ryanpmcmahon <ryan@mcmahon.work>
2017-02-22 13:23:12 -0500

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -10,6 +10,9 @@ class MusicLibraryController
   def call
     input = ""
     song_list = Song.all.sort{|a,b| a.artist.name <=> b.artist.name}
+    artist_list = Artist.all.map{|artist| artist.name}
+    genre_list = Genre.all.map{|genre| genre.name}
+
     while input != "exit"
       input = gets.chomp
       case input
@@ -17,11 +20,9 @@ class MusicLibraryController
         song_list.each{|song| puts "#{song_list.index(song)+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
 
       when "list artists"
-        artist_list = Artist.all.map{|artist| artist.name}
         artist_list.each{|artist| puts artist}
 
       when "list genres"
-        genre_list = Genre.all.map{|genre| genre.name}
         genre_list.each{|genre| puts genre}
 
       when "play song"
@@ -29,8 +30,13 @@ class MusicLibraryController
         song = song_list[input-1]
         puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
 
-      when Artist.find_by_name(input).name
+      when artist_list.select{|x| x==input}[0]
         Artist.find_by_name(input).songs.each do |song|
+          puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+        end
+
+      when genre_list.select{|x| x==input}[0]
+        Genre.find_by_name(input).songs.each do |song|
           puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
         end
       end
