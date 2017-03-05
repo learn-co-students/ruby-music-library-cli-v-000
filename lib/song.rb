a4d9586afc6774046fa75a9d69c8d99763f666e0
Erika Hughes <ehughes@knights.ucf.edu>
2017-03-05 04:49:42 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,25 @@
+require 'pry'
 class Song
   extend Concerns::Findable
   attr_accessor :name, :artist
   @@all = []
 
-  def initialize(name, artist = '')
+  def initialize(name, artist = nil)
     @name = name
-    if artist != ''
-      self.artist = artist
+    # binding.pry
+    if artist != nil
+      artist.add_song(self)
     end
-
   end
 
   def save
     @@all << self
   end
 
-  def artist_name= (name)
-      self.artist = Artist.find_or_create_by_name(name) #sets artist to artist object using the find_or_create_by_name method in Artist class which takes in a string
-      self.artist.add_song(self) #adds song in artists collection using the Artist add_song method
-  end
-
   def self.create(name)
     Song.new(name).tap{|song| song.save}
   end
+
 
   def self.all
     @@all
