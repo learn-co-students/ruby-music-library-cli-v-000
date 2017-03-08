0911803f0433af1b51da5d1c67b9fb71526b2f1e
Erika Hughes <ehughes@knights.ucf.edu>
2017-03-08 20:46:23 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,39 +7,60 @@ class MusicLibraryController
   end
 
   def call
-    # puts "Welcome to Emergence"
+    puts "Yo what up?"
     input = gets.downcase
-
-    if input == "list songs"
+    case input
+    when "list songs"
       n=0
       Song.all.each do |song|
         puts "#{n+=1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
       end
-
-    elsif input == "list artists"
+    when "list artists"
       Artist.all.each do |artist|
         puts "#{artist.name}"
       end
-
-    elsif input == "list genres"
+    when "list genres"
       Genre.all.each do |genre|
         puts "#{genre.name}"
       end
-
-    elsif input == "list an artist's songs"
-      artist = gets.downcase
-      binding.pry
-      Artist.all.each do |artist|
-        if artist.name.downcase == artist
-          artist.songs.each do |song|
-            puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
-          end
-        end
-      end
-
-    elsif input == "exit"
-     return
+    when "play song"
+      play_song
+    when "list artist"
+      list_artist_songs
+    when "list genre"
+      list_genre_songs
+    when "exit"
+      puts "Peace out, Seacrest."
+      return
     end
     call
   end
+
+  def list_artist_songs
+    puts "What artist?"
+    artist_name = gets.strip
+    if artist = Artist.find_by_name(artist_name)
+      artist.songs.each do |song|
+        puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+
+  def list_genre_songs
+    puts "What genre?"
+    genre_name = gets.strip
+    if genre = Genre.find_by_name(genre_name)
+      genre.songs.each do |song|
+        puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "What song?"
+    song_index = gets.to_i
+    song = Song.all[song_index-1]
+    puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+  end
+
 end
