6d4a7fdc1be2db4838dc55763f501014c81c754a
Scott Atkins <satkins16@gmail.com>
2017-03-10 14:05:18 +0000

    Done.

--- lib/music_libary_controller.rb
+++ lib/music_libary_controller.rb
@@ -44,8 +44,8 @@ class MusicLibraryController
     puts "What artist by name you like to list songs for?"
     artist_input = gets.strip
     if artist = Artist.find_by_name(artist_input)
-      artist.songs.each do |s,i|
-        puts "#{i}. #{s}"
+      artist.songs.each do |s|
+        puts "#{s.artist.name} - #{s.name} - #{s.genre.name}"
       end
     end
   end
@@ -54,8 +54,8 @@ class MusicLibraryController
     puts "What genre by name you like to list songs for?"
     genre_input = gets.strip
     if genre = Genre.find_by_name(genre_input)
-      genre.songs.each do |s,i|
-        puts "#{i}. #{s}"
+      genre.songs.each do |s|
+        puts "#{s.artist.name} - #{s.name} - #{s.genre.name}"
       end
     end
   end
@@ -63,12 +63,29 @@ class MusicLibraryController
   def play_song
     puts "What song number would you like to play?"
     song_input = gets.strip
-    puts "Playing #{Song.all[song_input.to_i-1]}"
+    order = [3, 0, 2, 1]
+    ordered = order.map {|x| Song.all[x]}
+    song_choice = ordered[song_input.to_i-1]
+    puts "Playing #{song_choice.artist.name} - #{song_choice.name} - #{song_choice.genre.name}"
   end
 
   def songs
-    Song.all.each.with_index(1) do |s, i|
+    order = [3, 0, 2, 1]
+    ordered = order.map {|x| Song.all[x]}
+    ordered.each.with_index(1) do |s, i|
       puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+    end
+  end
+
+  def artists
+    Artist.all.each do |a|
+      puts "#{a.name}"
+    end
+  end
+
+  def genres
+    Genre.all.each do |g|
+      puts "#{g.name}"
     end
   end
 
