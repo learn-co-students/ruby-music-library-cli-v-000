96119d9b8c24326fc8718b61475bb83f14c95eb4
Enrique Tellez <etellezp@gmail.com>
2017-03-15 16:47:05 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -22,7 +22,7 @@ class MusicLibraryController
       when "list genre"
         list_genre
       when "play song"
-        play_song
+        play_song(input)
       end
     end
   end
@@ -33,10 +33,39 @@ class MusicLibraryController
     end
   end
 
-  # def artists
-  #   Artist.all.each.with_index(1) do |song, index|
-  #     puts "#{index}. #{song}"
-  #   end
-  # end
+  def artists
+    Artist.all.each.with_index(1) do |artist, index|
+      puts "#{index}. #{artist.name}"
+    end
+  end
+
+  def genres
+    Genre.all.each.with_index(1) do |genre, index|
+      puts "#{index}. #{genre.name}"
+    end
+  end
+
+  def play_song(index)
+    song = Song.all[index.to_i]
+    puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+  end
+
+  def list_artist
+    artist_input = gets.strip
+    if artist = Artist.find_by_name(artist_input)
+      artist.songs.each do |song, index|
+        puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+
+  def list_genre
+    genre_input = gets.strip
+    if genre = Genre.find_by_name(genre_input)
+      genre.songs.each do |song, index|
+        puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
 
 end
