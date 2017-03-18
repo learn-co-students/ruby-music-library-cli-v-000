b1db93fa278e0f73a87dda949cf4774d3ee03570
Seth Alexander <seth.a.alexander@gmail.com>
2017-03-18 19:01:14 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,6 @@
 class Song
+  extend Concerns::Findable
+
   attr_accessor :name, :artist, :genre
   @@all = []
 
@@ -37,9 +39,9 @@ class Song
     @genre.songs << self unless @genre.songs.include?(self)
   end
 
-  def self.find_by_name(name)
-    self.all.find {|song| song.name == name }
-  end
+  # def self.find_by_name(name)
+  #   self.all.find {|song| song.name == name }
+  # end
 
   def self.find_or_create_by_name(name)
     self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
