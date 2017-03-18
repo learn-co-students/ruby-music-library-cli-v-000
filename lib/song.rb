ed12dc43b2fb4a9e685890916db65b7de11a9281
Seth Alexander <seth.a.alexander@gmail.com>
2017-03-18 18:40:52 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -36,4 +36,12 @@ class Song
     @genre = genre
     @genre.songs << self unless @genre.songs.include?(self)
   end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+  end
 end
