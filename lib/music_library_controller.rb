0891a2276cec9828f86cab6641a4f95e4272488b
Sergio Mendoza <smendoza787@gmail.com>
2017-03-29 00:53:07 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,55 +9,49 @@ class MusicLibraryController
   end
 
   def call
-    puts "Welcome to your Music Library CLI! To begin, please enter a command: "
-    user_input = gets.chomp.downcase
+    user_input = ""
+    while user_input != "exit"
+      puts "Welcome to your Music Library!"
+      puts "To begin, please enter a command:"
+      user_input = gets.strip
 
-    case user_input
-    when "exit"
-      puts "Thank you for using our app!"
-    when "list songs"
-      self.list_songs
-      self.call
-    when "list artists"
-      self.list_artists
-      self.call
-    when "list genres"
-      self.list_genres
-      self.call
-    when "play song"
-      self.list_songs
-      puts "Please enter the number of the song you want to listen to:"
-      song_number = gets.chomp.to_i - 1
-      user_choice = Song.all[song_number]
-      puts "Playing #{user_choice.artist.name} - #{user_choice.name} - #{user_choice.genre.name}"
-      self.call
-    when "list artist"
-      puts "Please enter an artist whose songs you wish to see:"
-      artist = gets.chomp
-      found_artist = Artist.find_by_name(artist)
+      case user_input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "play song"
+        list_songs
+        puts "Please enter the number of the song you want to listen to:"
+        song_number = gets.strip.to_i - 1
+        user_choice = Song.all[song_number]
+        puts "Playing #{user_choice.artist.name} - #{user_choice.name} - #{user_choice.genre.name}"
+      when "list artist"
+        puts "Please enter an artist whose songs you wish to see:"
+        artist = gets.chomp
+        found_artist = Artist.find_by_name(artist)
 
-      found_artist.songs.each do |song|
-        puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+        found_artist.songs.each do |song|
+          puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+        end
+
+      when "list genre"
+        puts "Please enter a genre whose songs you wish to see:"
+        genre = gets.chomp
+        found_genre = Genre.find_by_name(genre)
+
+        found_genre.songs.each do |song|
+          puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+        end
       end
 
-      self.call
-    when "list genre"
-      puts "Please enter a genre whose songs you wish to see:"
-      genre = gets.chomp
-      found_genre = Genre.find_by_name(genre)
-
-      found_genre.songs.each do |song|
-        puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
-      end
-
-      self.call
-    else
-      self.call
     end
   end
 
   def list_songs
-    Song.all.sort_by!{|a| a.artist.name}.each_with_index{|s, index| puts "#{index + 1}. #{s.artist.name} - #{s.name} - #{s.genre.name}" }
+    Song.all.sort_by!{|a| a.artist.name}.each.with_index(1){|s, index| puts "#{index}. #{s.artist.name} - #{s.name} - #{s.genre.name}" }
   end
 
   def list_artists
