cbb0268f5abe5208eac6272e37097d774bc5f9f0
Zenia Villa <zeniavilla@hotmail.com>
2017-04-07 15:54:36 -0700

    Done.

---
+++ lib/music_importer.rb
@@ -0,0 +1,16 @@
+class MusicImporter
+    attr_reader :path
+
+    def initialize(path)
+        @path = path
+    end
+
+    def files
+        Dir.entries(@path).select {|f| !File.directory? f}
+    end
+
+    def import
+        self.files.each { |filename| Song.create_from_filename(filename) }
+    end
+
+end
