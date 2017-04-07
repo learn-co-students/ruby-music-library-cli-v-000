b78d30e1ce8a01667e475d7225c2beabe15896ac
thesilentc <thesilentc@me.com>
2017-04-07 16:19:57 -0400

    created Genre.rb and set Class Genre

---
+++ lib/Genre.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class Genre
+  extend Concerns::Findable
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
+  end
+
+  def self.all
+      @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    self.new(name)
+  end
+
+  def add_song(song)
+    self.songs << song unless self.songs.include?(song)
+  end
+
+  def artists
+    self.songs.collect {|song| song.artist}.uniq
+  end
+
+end
