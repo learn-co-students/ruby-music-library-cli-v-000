81f0707db70ce375e3b01137d5389022cbc8e49d
thesilentc <thesilentc@me.com>
2017-04-07 16:03:41 -0400

    created artist.rb and song.rb files

---
+++ lib/Artist.rb
@@ -0,0 +1,16 @@
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+end
---
+++ lib/Concerns/findable_module.rb
@@ -0,0 +1,12 @@
+module Concerns::Findable
+
+    def find_by_name(name)
+      all.detect {|s| self.name == name}
+    end
+
+
+    def find_or_create_by_name(name)
+      find_by_name(name) || create(name)
+    end
+
+end
