340097edf9d1c17c6abf9fe9e73d84e3dd8851af
William Barela <william.barela.dev@gmail.com>
2017-04-12 13:44:48 +0900

    add song / genre collaboration

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,10 +1,11 @@
 class Genre
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def save
--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,23 @@
 class Song
   attr_accessor :name
-  attr_reader :artist
+  attr_reader :artist, :genre
 
   @@all = []
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist = artist unless artist == nil
+    self.genre = genre unless genre == nil
   end
 
   def artist=(artist)
     artist.add_song(self) unless artist.songs.include?(self)
     @artist = artist
+  end
+
+  def genre=(genre)
+    genre.songs << self unless genre.songs.include?(self)
+    @genre = genre
   end
 
   def save
