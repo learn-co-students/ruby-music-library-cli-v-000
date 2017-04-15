f5abfb8caf96de5b5269fa49683b24dd4dd398ff
Terry Blue <terryblue@comcast.net>
2017-04-15 01:15:05 +0000

    Done.

--- lib/011_MusicLibraryController.rb
+++ lib/011_MusicLibraryController.rb
@@ -1,16 +1,18 @@
+require 'pry'
+
 class MusicLibraryController
-  attr_accessor :importer
 
   def initialize(path = './db/mp3s')
-    @importer = MusicImporter.new(path)
-    @importer.import
+    importer = MusicImporter.new(path)
+    importer.import
   end
 
   def call
+    puts "Welcome to Your Music Library!"
+    puts "What would you like to do?"
+    @request = ""
     input = ""
     while  input != "exit"
-      puts "Welcome to Your Music Library!"
-      puts "What would you like to do?"
       input = gets.strip
       case input
       when "list songs"
@@ -19,12 +21,27 @@ class MusicLibraryController
       	artists
       when "list genres"
       	genres
-      when "play song"
-      	play_song
       when "list artist"
-      	list_artist
+      	@request = "list artist"
+        puts "Please enter the artist"
       when "list genre"
-      	list_genre
+      	@request = "list genre"
+        puts "Please enter the genre"
+      when "play song"
+        @request = "play song"
+        puts "Please enter the song number"
+      else
+        case @request
+        when "play song"
+          @song_num = input.to_i
+          play_song
+        when "list artist"
+          @artist = input
+          list_artist
+        when "list genre"
+          @genre = input
+          list_genre
+        end
       end
     end
   end
@@ -40,10 +57,44 @@ class MusicLibraryController
       puts "#{i+1}. #{artist.name}"
     end
   end
-  
+
   def genres
     Genre.all.each_with_index do |genre, i|
       puts "#{i+1}. #{genre.name}"
+    end
+  end
+
+  def play_song
+    Song.all.each_with_index do |song, i|
+      if @song_num == (i+1)
+        puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+
+  def list_artist
+    @artist_songs = 0
+    Song.all.each do |song|
+      if song.artist.name == @artist
+        puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+        @artist_songs+= 1
+      end
+    end
+    if @artist_songs == 0
+      puts "#{@artist} not found!"
+    end
+  end
+
+  def list_genre
+    @genre_songs = 0
+    Song.all.each do |song|
+      if song.genre.name == @genre
+        puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+        @genre_songs+= 1
+      end
+    end
+    if @genre_songs == 0
+      puts "#{@genre} not found!"
     end
   end
 
