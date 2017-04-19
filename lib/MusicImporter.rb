4df7affb7128fc8d13ed15c96a9dfed1bb056b6e
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-19 19:02:26 +0000

    Done.

---
+++ lib/MusicImporter.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+class MusicImporter
+  attr_reader :path, :song, :artist, :genre
+
+  def initialize(path)
+    @path = path
+  end
+
+  # def files
+  #   Dir.entries(path).select{|file| file.end_with?(".mp3")}
+  # end
+  #
+  # def import
+  #   files.each{ |file| Song.new_by_filename(file) }
+  # end
+
+
+end
+
+# class MusicImporter
+#   attr_accessor :artist, :name, :genre, :songs, :song
+#
+#   def initialize(url)
+#
+#   end
+#
+# end
