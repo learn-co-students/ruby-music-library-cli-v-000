1769bbff8607a5a52b49739eacd7819e809cccb9
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-19 16:54:02 +0000

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -7,10 +7,10 @@ class Song
     @@all = []
     @@count = 0
 
-    def initialize(name, artist=nil)
+    def initialize(name, artist=nil, genre=nil)
       @name = name
       self.artist = artist if artist
-
+      self.genre = genre if genre
     end
 
     def self.all
