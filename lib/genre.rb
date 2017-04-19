83fa8d707484287a259a6fd9eaca91389bcec44a
Jerred Stahlberg <jerred.stahlberg@yahoo.com>
2017-04-19 16:33:11 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,40 @@
+class Genre
+
+  extend Concerns::Findable
+
+  attr_accessor :name
+  attr_reader :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    genre
+  end
+
+  def artists
+    self.songs.map{|song| song.artist}.uniq
+  end
+
+  def to_s
+   self.name
+  end
+  
+end
