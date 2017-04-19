93dc77cd73b852129af6d9d389aa740fb707c9ce
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-19 19:13:28 +0000

    Done.

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -7,9 +7,9 @@ class MusicImporter
     @path = path
   end
 
-  # def files
-  #   Dir.entries(path).select{|file| file.end_with?(".mp3")}
-  # end
+  def files
+    Dir.entries(path).select{|file| file.end_with?(".mp3")}
+  end
   #
   # def import
   #   files.each{ |file| Song.new_by_filename(file) }
