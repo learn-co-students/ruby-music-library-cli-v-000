e6fc9340af80bb23d0bf487dea6cbd07faa1b9a3
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-20 18:12:04 +0000

    Done.

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -12,7 +12,8 @@ class MusicImporter
   end
 
   def import
-    files.each{ |file| Song.new_by_filename(file) }
+    # binding.pry
+    files.each{ |file| Song.new_from_filename(file) }
   end
 
 end
