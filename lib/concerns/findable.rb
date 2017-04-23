d72962ef5c63f6dbfe0a1e76ce1d13ae10c33145
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-04-23 21:04:41 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,10 @@
 module Concerns::Findable
-    def self.find_by_name(name)
-      @@all.select {|x| x.name == name}
+    def find_by_name(name)
+      all.select {|x| x.name == name}[0]
     end
+    def find_or_create_by_name(name)
+      self.find_by_name(name) || self.create(name)
+    end 
 end
 
 
