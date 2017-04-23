0cf256877808ce55dad14e382476df3b55a06ecc
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-23 01:28:22 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -10,31 +10,30 @@ class MusicLibraryController
     end
 
     def call
-      puts "What would you like?"
-      input = gets.chomp
       while input != "exit"
-        call
-        if input == "list songs"
-          sorted = Song.all.sort_by { |song| song.artist.name }
-          sorted.each.with_index do |song, index|
-            puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+        puts "What would you like?"
+        input = gets.chomp
+          if input == "list songs"
+            sorted = Song.all.sort_by { |song| song.artist.name }
+            sorted.each.with_index do |song, index|
+              puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+            end
+          elsif input == "list artists"
+            sort_artists = Artist.all.sort_by { |song| song.artist.name }
+            sort_artists.each do |song|
+                puts "#{song.artist.name}"
+              end
+          elsif input == "list genres"
+            # sort_genres = Artist.all.sort_by { |song| song.artist.name }
+            Genre.all.each do |song|
+                puts "#{song.genre.name}"
+              end
+          elsif input == "play song"
+            puts "Which number?"
+            number = gets.chomp
+          else
+            return "exit"
           end
-        elsif input == "list artists"
-          sort_artists = Artist.all.sort_by { |song| song.artist.name }
-          sort_artists.each do |song|
-              puts "#{song.artist.name}"
-            end
-        elsif input == "list genres"
-          # sort_genres = Artist.all.sort_by { |song| song.artist.name }
-          Genre.all.each do |song|
-              puts "#{song.genre.name}"
-            end
-        elsif input == "play song"
-          puts "Which number?"
-          number = gets.chomp
-        else
-          return "exit"
-        end
       end
     end
 
