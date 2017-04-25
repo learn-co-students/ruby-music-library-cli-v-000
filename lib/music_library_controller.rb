a96c6d1b0542f3fe1f6dea274f497b66bd60e840
Eva Navarror <emanao@me.com>
2017-04-25 21:19:46 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,2 +1,11 @@
 class MusicLibraryController
+  def initialize(path="./db/mp3s")
+    MusicImporter.new(path).import
+  end
+  def call
+    user_input = nil
+    while user_input != "exit"
+      user_input = gets.chomp
+    end
+  end
 end
