bb9a49315021440c4172215ebb906754cbb42dd6
Eva Navarror <emanao@me.com>
2017-04-26 15:57:55 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,4 +1,4 @@
-require "pry"
+
 class MusicLibraryController
   attr_accessor :importer
   def initialize(path="./db/mp3s")
@@ -6,7 +6,6 @@ class MusicLibraryController
   end
   def call
     user_input = nil
-
     while user_input != "exit"
       user_input = gets.chomp
       case user_input
@@ -18,12 +17,10 @@ class MusicLibraryController
         list_genres
       when "play song"
         play_a_song
-      when "artists song"
+      when "list artist"
         artists_song
-      when "genres song"
+      when "list genre"
         genres_song
-      # else
-      #   puts"Remember only numbers between 1 and 6 are valid"
       end
     end
   end
@@ -34,23 +31,24 @@ class MusicLibraryController
     end
   end
   def list_artists
-    Artist.all.each.with_index do |artist, index|
-      puts "#{index+1}. #{artist.name}"
-    end
+    Artist.all.each.with_index {|artist, index| puts "#{index+1}. #{artist.name}"}
   end
   def list_genres
-    Genre.all.each.with_index do |genre, index|
-      puts "#{index+1}. #{genre.name}"
-    end
+    Genre.all.each.with_index {|genre, index| puts "#{index+1}. #{genre.name}"}
   end
   def play_a_song
     user_input = gets.chomp
     song = Song.all[user_input.to_i-1]
     puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
-
   end
   def artists_song
+    user_input = gets.chomp
+    artist = Artist.find_by_name(user_input)
+    artist.songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
   end
   def genres_song
+    user_input = gets.chomp
+    genre = Genre.find_by_name(user_input)
+    genre.songs.each {|song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
   end
 end
