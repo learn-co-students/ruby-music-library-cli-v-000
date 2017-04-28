cf84137abc0127ee7e1d3b9b9953fa78995d264d
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-04-28 15:58:27 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -24,9 +24,11 @@ class Song
     @@all << self
   end
 
-  def self.create(name)
+  def self.create(name,artist=nil,genre=nil)
     song = self.new(name)
     song.save
+    song.artist = artist unless artist == nil
+    song.genre = genre unless genre == nil
     song
   end
 
@@ -38,6 +40,24 @@ class Song
   def genre=(genre)
     @genre = genre
     genre.add_song(self)
+  end
+
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts.first, parts[1], parts[2].gsub(".mp3", "")
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.create(song_name, artist, genre)
+  end
+
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts.first, parts[1], parts[2].gsub(".mp3", "")
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name,artist,genre)
   end
 
 end
