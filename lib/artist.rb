738e3567d5262ee106b598322bcaa5ae2cd8e478
Will-Sauer <will@heueso.com>
2017-05-05 07:37:57 -0700

    oo4 passing

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,16 +3,31 @@ class Artist
 #vars and macros
 attr_accessor :name
 @@all = []
+
 #----------------------------------------------------------------------------------------
 #instance
 def initialize(name)
-    @name = name
+        @name = name
+        @songs = []
 end
 
 def save
-    @@all << self
+        @@all << self
 end
 
+def songs
+        @songs
+end
+
+def add_song(song)
+        if !@songs.include?(song)
+        @songs << song
+        end
+
+        if song.artist == nil
+        song.artist = self
+        end
+end
 
 
 
@@ -31,10 +46,10 @@ def self.create(name)
     this_artist = self.new(name)
     this_artist.save
     this_artist
+    
 end
 
 
 
 #---
-
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,17 @@
 class Song
 #----------------------------------------------------------------------------------------
 #vars and macros
-attr_accessor :name, :genre, :artist
+attr_accessor :name, :genre
 @@all = []
 #----------------------------------------------------------------------------------------
 #instance
-def initialize(name)
+def initialize(name, artist=nil)
     @name = name
+    @artist = artist
+    #adds song to artist iff artist was provided at init
+    artist.add_song(self) if @artist != nil
+
+    @genre = nil
 end
 
 def save
@@ -14,6 +19,23 @@ def save
 end
 
 
+#artist setter/getter w reciprocity to song
+def artist= (artist)
+# artist.add_song(self)
+@artist = artist
+        #if its not already added
+        artist.add_song(self)
+        
+        #to add song w/o use of artist #add_song
+        # if ! artist.songs.include?(self)
+        # artist.songs << self
+        # end
+
+end
+#---
+def artist
+@artist
+end
 
 
 
