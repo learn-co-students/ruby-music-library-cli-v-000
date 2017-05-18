da42f57537108dcfdd0dcf66dfc7ce4dda3095be
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-05-18 14:12:20 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -12,29 +12,34 @@ class MusicLibraryController
      while input != "exit"
       puts "Welcome To Your Music Library"
       puts "What Would You Like To Do"
-    
+
       input = gets.strip
-    
+
       case input
       when "list songs"
-        Song.all.each{|song| puts "#{song.name}"}
 
-      when "list artists"
-        Artist.all.each{|artist| puts "#{artist.name}"}
-      when "list genres"
-        Genre.all.each{|genre| puts "#{genre.name}"}
+      Song.all.each_with_index do |s, i|
+           puts "#{i + 1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+         end
 
-      when "list artist"
-        Artist.all.each do |a| 
-          a.songs.each{|s| puts "#{s.artist.name} - #{s.name} - #{s.genre.name}" }
-        end  
+       when "list artists"
+         Artist.all.each{|artist| puts "#{artist.name}"}
 
-      when "list genre"
-        Genre.all.each do |a| 
-          a.songs.each{|s| puts "#{s.artist.name} - #{s.name} - #{s.genre.name}" }
-        end
+       when "list genres"
+         Genre.all.each{|genre| puts "#{genre.name}"}
 
-      end
+       when "list artist"
+         Artist.all.each do |a|
+           a.songs.each{|s| puts "#{s.artist.name} - #{s.name} - #{s.genre.name}" }
+         end
+
+       when "list genre"
+         Genre.all.each do |a|
+           a.songs.each{|s| puts "#{s.artist.name} - #{s.name} - #{s.genre.name}" }
+         end
+       when "play song"
+         Song.all.each{|s| puts "Playing #{s.artist.name} - #{s.name} - #{s.genre.name}"}
+       end
 
     end
   end
