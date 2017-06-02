8bd99a95c4aa5dcad3d385ffff13828b5f1c02c7
Gabe Jackson <gj@mail.co.de>
2017-06-02 14:56:19 -0400

    Reorganize and rewrite the tests for the Song <-> Artist association to better cohere them with the lab instructions

--- spec/004_songs_and_artists_spec.rb
+++ spec/004_songs_and_artists_spec.rb
@@ -1,102 +1,85 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe "Songs and Artists" do
-  context 'Artists have many songs' do
-    it 'initializes with a songs property set to an empty array' do
-      artist = Artist.new("Neutral Milk Hotel")
-
-      expect(artist.songs).to eq([])
-    end
-
-    it 'can push a song into the songs collection' do
-      artist = Artist.new("Neutral Milk Hotel")
-      song = Song.new("In the Aeroplane Over the Sea")
-
-      artist.songs << song
-      expect(artist.songs).to include(song)
-    end
+describe "Associations — Song and Artist:" do
+  before(:each) do
+    @song = Song.new("In the Aeroplane Over the Sea")
+    @artist = Artist.new("Neutral Milk Hotel")
   end
 
-  context 'Songs belong to an artist' do
-    describe '#artist=' do
-      it 'accepts an artist for the song' do
-        song = Song.new("In the Aeroplane Over the Sea")
-        artist = Artist.new("Neutral Milk Hotel")
-        song.artist = artist
-
-        expect(song.artist).to eq(artist)
+  context "Artist" do
+    describe "#initialize" do
+      it "creates a 'songs' property set to an empty array (artist has many songs)" do
+        expect(@artist.instance_variable_defined?(:@songs)).to be(true)
+        expect(@artist.instance_variable_get(:@songs)).to eq([])
       end
+    end
 
-      # it 'enforces the type of object assigned to Artist'
-
-      it 'adds the song to the artist\'s songs' do
-        song = Song.new("In the Aeroplane Over the Sea")
-        artist = Artist.new("Neutral Milk Hotel")
-        song.artist = artist
-
-        expect(artist.songs).to include(song)
+    describe "#songs" do
+      it "returns the artist's 'songs' collection (artist has many songs)" do
+        expect(@artist.songs).to eq([])
+        @artist.songs << @song
+        expect(@artist.songs).to eq([@song])
       end
     end
   end
 
-  context 'adding a song to an artist' do
-    describe '#add_song' do
-      it 'adds the song to the artist\'s song collection' do
-        song = Song.new("In the Aeroplane Over the Sea")
-        artist = Artist.new("Neutral Milk Hotel")
-        artist.add_song(song)
+  context "Song" do
+    describe "#initialize" do
+      it "can be invoked with an optional second argument, an Artist object to be assigned to the song's 'artist' property (song belongs to artist)" do
+        song = Song.new("Two-Headed Boy", @artist)
 
-        expect(artist.songs).to include(song)
-      end
-      it 'assigns the artist to the song' do
-        song = Song.new("In the Aeroplane Over the Sea")
-        artist = Artist.new("Neutral Milk Hotel")
-        artist.add_song(song)
-
-        expect(song.artist).to eq(artist)
-      end
-      it 'does not assign the artist to the song if the song already has the artist' do
-        song = Song.new("In the Aeroplane Over the Sea")
-        artist = Artist.new("Neutral Milk Hotel")
-
-        song.artist = artist
-
-        expect(song).to_not receive(:artist=)
-
-        artist.add_song(song)
-      end
-
-      it 'does not add the song to the artist\'s song collection if the artist already has the song' do
-        song = Song.new("In the Aeroplane Over the Sea")
-        artist = Artist.new("Neutral Milk Hotel")
-
-        artist.add_song(song)
-        artist.add_song(song)
-
-        expect(artist.songs).to include(song)
-        expect(artist.songs.size).to eq(1)
+        expect(song.instance_variable_defined?(:@artist)).to be(true)
+        expect(song.instance_variable_get(:@artist)).to be(@artist)
       end
     end
 
-    describe 'Song#artist=' do
-      it 'uses add_song to add the song to the artist\'s collection' do
-        song = Song.new("In the Aeroplane Over the Sea")
-        artist = Artist.new("Neutral Milk Hotel")
+    describe "#artist" do
+      it "returns the artist of the song (song belongs to artist)" do
+        @song.instance_variable_set(:@artist, @artist)
+        expect(@song.artist).to be(@artist)
+      end
+    end
 
-        expect(artist).to receive(:add_song)
-
-        song.artist = artist
+    describe "#artist=" do
+      it "assigns an artist to the song (song belongs to artist)" do
+        @song.artist = @artist
+        expect(@song.artist).to eq(@artist)
       end
     end
   end
 
-  context 'initializing a song with an artist' do
-    it 'new songs accept an optional argument for the artist' do
-      artist = Artist.new("Neutral Milk Hotel")
-      song = Song.new("In the Aeroplane Over the Sea", artist)
+  context "Artist" do
+    describe "#add_song" do
+      it "assigns the current artist to the song's 'artist' property (song belongs to artist)" do
+        @artist.add_song(@song)
+        expect(@song.artist).to eq(@artist)
+      end
 
-      expect(artist.songs).to include(song)
-      expect(song.artist).to eq(artist)
+      it "does not re-assign the artist to the song if the song already has an artist" do
+        @song.artist = @artist
+        expect(@song).to_not receive(:artist=)
+        @artist.add_song(@song)
+      end
+
+      it "adds the song to the artist's collection of songs (artist has many songs)" do
+        @artist.add_song(@song)
+        expect(@artist.songs).to include(@song)
+      end
+
+      it "does not add the song to the artist's song collection if the artist already has the song" do
+        2.times { @artist.add_song(@song) }
+        expect(@artist.songs).to include(@song)
+        expect(@artist.songs.size).to eq(1)
+      end
+    end
+  end
+
+  context "Song" do
+    describe "#artist=" do
+      it "invokes Artist#add_song to add itself to the artist's collection of songs (artist has many songs)" do
+        expect(@artist).to receive(:add_song)
+        @song.artist = @artist
+      end
     end
   end
 end
