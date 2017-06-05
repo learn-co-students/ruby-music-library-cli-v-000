b9bd3f85fc96038c44646d7aa3a6db93d963844a
Gabe Jackson <gj@mail.co.de>
2017-06-05 14:55:07 -0400

    Expand and improve test coverage for the Concerns::Findable module (#24 #32)

--- spec/008_findable_module_spec.rb
+++ spec/008_findable_module_spec.rb
@@ -1,20 +1,59 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe 'Concerns::Findable' do
-  it 'defines a module Concerns::Findable' do
+describe "Concerns::Findable" do
+  it "defines a module named Concerns::Findable" do
     expect(defined?(Concerns::Findable)).to be_truthy
     expect(Concerns::Findable).to be_a(Module)
   end
 
-  class Person
+  class FindableTest
     extend Concerns::Findable
+    attr_accessor :name
+    @@all = []
+
+    def self.all
+      @@all
+    end
+
+    def self.create(*args)
+    end
   end
 
-  it 'adds a find_by_name class method to classes that are extended by the module' do
-    expect(Person).to respond_to(:find_by_name)
+  instance1 = FindableTest.new.tap{ |ft| ft.name = "Test Double 1" }
+  instance2 = FindableTest.new.tap{ |ft| ft.name = "Test Double 2" }
+  FindableTest.all.push(instance1, instance2)
+
+  describe ".find_by_name" do
+    it "is added as a class method to classes that extend the module" do
+      expect(FindableTest).to respond_to(:find_by_name)
+    end
+
+    context "works exactly like a generic version of Song.find_by_name," do
+      it "searching the extended class's @@all variable for an instance that matches the provided name" do
+        expect(FindableTest.find_by_name("Test Double 2")).to be(instance2)
+      end
+    end
   end
 
-  it 'adds a find_or_create_by_name class method to classes that are extended by the module' do
-    expect(Person).to respond_to(:find_or_create_by_name)
+  describe ".find_or_create_by_name" do
+    it "is added as a class method to classes that extend the module" do
+      expect(FindableTest).to respond_to(:find_or_create_by_name)
+    end
+
+    context "works exactly like a generic version of Song.find_or_create_by_name:" do
+      it "finds (does not recreate) an existing instance with the provided name if one exists in @@all" do
+        expect(FindableTest.find_or_create_by_name("Test Double 2")).to be(instance2)
+      end
+
+      it "invokes .find_by_name instead of re-coding the same functionality" do
+        expect(FindableTest).to receive(:find_by_name)
+        FindableTest.find_or_create_by_name("Test Double 3")
+      end
+
+      it "invokes the extended class's .create method, passing in the provided name, if an existing match is not found" do
+        expect(FindableTest).to receive(:create)
+        FindableTest.find_or_create_by_name("Test Double 3")
+      end
+    end
   end
 end
