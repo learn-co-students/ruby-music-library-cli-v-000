7a259f2b19ea890a021d65d537348640d954cd64
Gabe Jackson <gj@mail.co.de>
2017-06-05 14:54:35 -0400

    add additional test coverage to the Findable class methods on Song

--- spec/007_findable_songs_spec.rb
+++ spec/007_findable_songs_spec.rb
@@ -1,21 +1,33 @@
-describe 'Findable Songs' do
+context "Song" do
   describe ".find_by_name" do
-    it 'finds a song instance in @@all by the name property of the song' do
-      song = Song.new("In the Aeroplane Over the Sea")
-      song.save
-
-      expect(Song.find_by_name("In the Aeroplane Over the Sea")).to eq(song)
+    it "finds a song instance in @@all by the name property of the song" do
+      song = Song.create("In the Aeroplane Over the Sea")
+      expect(Song.find_by_name("In the Aeroplane Over the Sea")).to be(song)
     end
   end
 
   describe ".find_or_create_by_name" do
-    it 'finds or creates a song by name maintaining uniqueness of objects by their name property' do
-      song_1 = Song.find_or_create_by_name("In the Aeroplane Over the Sea")
-      song_2 = Song.find_or_create_by_name("In the Aeroplane Over the Sea")
+    it "finds (does not recreate) an existing song with the provided name if one exists in @@all" do
+      song1 = Song.find_or_create_by_name("In the Aeroplane Over the Sea")
+      song2 = Song.find_or_create_by_name("In the Aeroplane Over the Sea")
 
-      expect(song_1).to be_a(Song)
-      expect(song_1).to eq(song_2)
-      expect(song_1.name).to eq("In the Aeroplane Over the Sea")
+      expect(song1).to be_a(Song)
+      expect(song1).to eq(song2)
+    end
+
+    it "invokes .find_by_name instead of re-coding the same functionality" do
+      expect(Song).to receive(:find_by_name)
+      Song.find_or_create_by_name("Kaohsiung Christmas")
+    end
+
+    it "creates a song if an existing match is not found" do
+      song = Song.find_or_create_by_name("I'd Rather Go Blind")
+      expect(Song.all).to include(song)
+    end
+
+    it "invokes .create instead of re-coding the same functionality" do
+      expect(Song).to receive(:create)
+      Song.find_or_create_by_name("Kaohsiung Christmas")
     end
   end
 end
