662427d0287f88cceba85d5fdf9f019b6803d5b1
Julie Widman <juliewdmn@gmail.com>
2017-06-07 00:27:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -46,22 +46,35 @@ class Song
       genre.add_song(self)
     end
 
-  #   def find_by_name(name)
-  #     self.all.detect {|objekt| objekt.name == name}
-  #   end
-  #
-  #   def new_by_name(name)
-  #     self.new(name)
-  #   end
-  #
-  #   def find_or_create_by_name(name)
-  #     self.name != name
-  #     self.find_by_name(name) || self.new_by_name(name).tap{|s| s.save}
-  #   end
-  end
+    def find_by_name(name)
+      self.all.detect {|objekt| objekt.name == name}
+    end
 
+    def new_by_name(name)
+      self.new(name)
+    end
 
+    def find_or_create_by_name(name)
+      self.name != name
+      self.find_by_name(name) || self.new_by_name(name).tap{|s| s.save}
+    end
 
-#end
-    # Song.new("Gold and Silver", "Stavesacre")
-    # binding.pry
+#     def new_from_filename(file)
+#       file = file.split(" - ")
+#       artist = file[0]
+#       song = file[1].gsub(".mp3", "")
+#       #binding.pry
+#       new_song = self.new_by_name(song)
+#       new_song.artist = Artist.find_or_create_by_name(artist)
+#       new_song.artist.add_song(new_song)
+#       new_song #want to use tap here
+#
+#
+#     end
+#   end
+#
+#
+#
+end
+#     #Song.new("Gold and Silver", "Stavesacre")
+#     #binding.pry
