2343f3aa5aa23e5acf835dc3e933c5961b897f82
ctab33 <ctab33@me.com>
2017-06-09 11:07:23 -0400

    Done.

---
+++ lib/controller.rb
@@ -0,0 +1,22 @@
+class MusicLibraryController
+  def initialize(path = "./db/mp3s")
+    importer = MusicImporter.new(path)
+    importer.import
+  end
+
+  def call
+    input = ""
+    while input !="exit"
+      puts "Welcome to Your Music Library!"
+      puts "What would you like to do?"
+      input = gets.strip
+      case input
+      when "list songs"
+        songs
+      when "list artists"
+        artists
+      when "list genres"
+        genres
+      end
+    end 
+end
