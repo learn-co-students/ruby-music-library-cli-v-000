90ab20350468c4663968651c2956612f80f7cbaa
Julie Widman <juliewdmn@gmail.com>
2017-06-12 22:11:35 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -24,18 +24,18 @@ class MusicLibraryController
   end
 
   def list_commands
-    #puts "Please choose from this list of options"
+    puts "Please choose from this list of options"
     #user_input = gets.strip
     input = " "
   while input != "exit" do
-    # puts "To list all of your songs, enter 'list songs'."
-    #   puts "To list all of the artists in your library, enter 'list artists'."
-    #   puts "To list all of the genres in your library, enter 'list genres'."
-    #   puts "To list all of the songs by a particular artist, enter 'list artist'."
-    #   puts "To list all of the songs of a particular genre, enter 'list genre'."
-    #   puts "To play a song, enter 'play song'."
-    #   puts "To quit, type 'exit'."
-    #   puts "What would you like to do?"
+    puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
 
     input = user_input
     case input
