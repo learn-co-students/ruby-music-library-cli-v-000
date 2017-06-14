146f45d7d989c944819e668b69dec2bb58304d01
Pat  Pignataro <pat6284@msn.com>
2017-06-14 17:24:42 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,43 @@
+class Genre
+
+  extend Concerns::Findable
+
+  attr_accessor :name, :songs, :genre, :artist
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(song)
+    new_song = self.new(song)
+    new_song.save
+    new_song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def artists
+    self.songs.collect{|genre| genre.artist}.uniq
+  end
+end
