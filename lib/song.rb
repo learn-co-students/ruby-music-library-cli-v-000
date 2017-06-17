2182bf8b13bbe189a7bc35ae6c70ebd7cc1712b9
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-06-17 15:37:32 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Song
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end
+
+end
