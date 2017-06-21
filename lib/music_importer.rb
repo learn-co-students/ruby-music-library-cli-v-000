a8e5e62a3e3da3229b63050f32f0adf6e1e9783a
Jack Xu <jackexyou@gmail.com>
2017-06-21 06:03:16 +0000

    Done.

---
+++ lib/music_importer.rb
@@ -0,0 +1,15 @@
+class  MusicImporter
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files = Dir.entries(@path).select {|file| file.include?(".mp3")}
+  end
+
+  def import
+    self.files.each {|file| Song.create_from_filename(file)}
+  end
+end
