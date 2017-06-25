3e36376b7eb57b8f9fd5f68f5b441bc409bbd08b
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-06-25 18:16:26 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require_relative '../db/mp3s'
 class MusicLibraryController
   attr_accessor :path
 
@@ -8,20 +9,27 @@ class MusicLibraryController
   end
 
  def call
-     puts "Welcome to your music library!"
-     puts "To list all of your songs, enter 'list songs'."
-     puts "To list all of the artists in your library, enter 'list artists'."
-     puts "To list all of the genres in your library, enter 'list genres'."
-     puts "To list all of the songs by a particular artist, enter 'list artist'."
-     puts "To list all of the songs of a particular genre, enter 'list genre'."
-     puts "To play a song, enter 'play song'."
-     puts "To quit, type 'exit'."
-     puts "What would you like to do?"
-      input = gets.strip
-      if input != "exit"
-        call
-      end 
+   puts "Welcome to your music library!"
+   puts "To list all of your songs, enter 'list songs'."
+   puts "To list all of the artists in your library, enter 'list artists'."
+   puts "To list all of the genres in your library, enter 'list genres'."
+   puts "To list all of the songs by a particular artist, enter 'list artist'."
+   puts "To list all of the songs of a particular genre, enter 'list genre'."
+   puts "To play a song, enter 'play song'."
+   puts "To quit, type 'exit'."
+   puts "What would you like to do?"
+   input = gets.strip
 
+    if input != "exit"
+      call
+    end
  end
+
+ def list_songs
+   import.sort do |a,b|
+     a <=> b
+   end
+ end
+
 
 end
