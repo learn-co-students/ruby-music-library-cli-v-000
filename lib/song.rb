2861c48e1c8bc2ee9d9a935b57cb11e09dea7006
Mason Ellwood <masonellwood@gmail.com>
2017-06-27 04:58:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,25 @@
 require "pry"
 class Song
   attr_accessor :name
+
   @@all = []
+
   def initialize(name)
     @name = name
   end
+
   def self.all
     @@all
   end
+
   def self.destroy_all
     @@all.clear
   end
+
   def save
     @@all << self
   end
+
   def self.create(name)
     song = self.new(name)
     @@all << song
