fe5a92820e503b93bd8782c3839bb967383db95f
Mason Ellwood <masonellwood@gmail.com>
2017-06-28 02:41:56 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -38,4 +38,17 @@ class Song
     @artist = artist
     artist.add_song(self)
   end
+
+  def self.find_by_name(name)
+    self.all.detect {|x| x.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
 end
