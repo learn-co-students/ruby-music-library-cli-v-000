2efc7c5003c771e3255656279b274860618a3330
Seb Gertz <sebgertz@gmail.com>
2017-07-02 01:24:46 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,76 @@
+
+class Song
+  extend Concerns::Findable
+  attr_accessor :name, :artist, :genre
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+
+#if 'artist' is truthy, calls custom setter method with argument 'artist'
+    if artist
+      self.artist = artist
+    end
+#if 'genre' is truthy, calls custom setter method with argument 'genre'
+    if genre
+      self.genre = genre
+    end
+
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song #'song' provides return value asked for by test; the shovel method only returns array
+  end
+
+  #custom setter methods
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self)
+  end
+
+  def self.new_from_filename(filename)
+   clean_filename = filename.gsub(".mp3", "")
+   song_name = clean_filename.split(" - ")[1]
+   song = Song.new(song_name)
+   artist_name = clean_filename.split(" - ")[0]
+   song.artist_name = artist_name
+   genre_name = clean_filename.split(" - ")[2]
+   song.genre_name = genre_name
+   song
+  end
+
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(self)
+ end
+
+ def genre_name=(genre_name)
+   self.genre = Genre.find_or_create_by_name(genre_name)
+   genre.add_song(self)
+ end
+
+  def self.create_from_filename(filename)
+    new_song = self.new_from_filename(filename)
+    new_song.save
+  end
+
+end
