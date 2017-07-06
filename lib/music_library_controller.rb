79c7e4d50e5c4992cef23a0f7cfdc4746fd373bc
Neil Ryan <neilmarkoliverryan@gmail.com>
2017-07-06 05:25:58 +0000

    Done.

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,30 @@
+class MusicLibraryController
+
+  attr_reader :path
+
+  def initialize(path = './db/mp3s')
+    @path = path
+    music_importer = MusicImporter.new(@path).import
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    input = gets.chomp
+    while input != "exit"
+      input = gets.chomp
+    end
+  end
+
+  def list_songs
+    
+  end
+
+end
--- lib/song.rb
+++ lib/song.rb
@@ -56,9 +56,9 @@ class Song
     self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
   end
 
-  def self.print_song_names
-    self.all.each {|song| puts song.name}
-  end
+  #def self.print_song_names
+  #  self.all.each {|song| puts song.name}
+  #end
 
   def self.new_from_filename(file_name)
     artist, name, genre = file_name.split(" - ")
