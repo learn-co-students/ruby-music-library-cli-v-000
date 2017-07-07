d2d442b665445614e9f661400500932c972db41f
Danielle Croxton <danikrist@gmail.com>
2017-07-07 23:43:36 +0000

    Done.

---
+++ lib/songs.rb
@@ -0,0 +1,26 @@
+class Song
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    new(name).save
+  end
+
+end
