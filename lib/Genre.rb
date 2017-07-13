176ff7395d5516f36ff7f3d805535d62bc861b14
jonathan <jonathanyi81@gmail.com>
2017-07-12 21:00:10 -0400

    initial genre class created

---
+++ lib/Genre.rb
@@ -0,0 +1,26 @@
+class Genre
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  attr_accessor :name
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    genre = Genre.new(name)
+  end
+end
