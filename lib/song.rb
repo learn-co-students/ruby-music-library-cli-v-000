584196fb12e58ff3553849f124932e4047333eef
Robert Peckham <peckham.rob@gmail.com>
2017-07-16 23:30:03 +0000

    song class passing

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+
+  end
+
+  def self.all
+    @@all
+
+  end
+
+  def self.destroy_all
+    @@all.clear
+
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+    #new(name).tap{|s| s.save}
+  end
+end
