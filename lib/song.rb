4fdd1cca8cc4820f0e1210a30ee9f6ee6164d689
Robert Peckham <peckham.rob@gmail.com>
2017-07-17 19:39:58 +0000

    0007 specs passing

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,40 @@
 class Song
   attr_accessor :name
+  attr_reader :genre, :artist
 
   @@all = []
 
-  def initialize(name)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
 
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def self.find_by_name(name)
+    @@all.detect{|s| s.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
   end
 
   def self.all
     @@all
-
   end
 
   def self.destroy_all
     @@all.clear
-
   end
 
   def save
