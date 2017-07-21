857de62ac01c41e94d80b5072408c547b4c06b4d
Danielle Croxton <danikrist@gmail.com>
2017-07-21 04:24:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -42,12 +42,12 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    # artist_name, song_name = filename.split(" - ")[0], filename.split(" - ")[1]
-    song_name = filename.split(" - ")[1]
-    song = self.new(song_name)
-    # song.artist=(artist)
+    artist_name, song_name, genre_name = filename.split(" - ")[0], filename.split(" - ")[1], filename.split(" - ")[2]
+    self.new(song_name, artist_name, genre_name)
+    # song.artist=(artist_name)
+    # song.genre=(genre_name)
     # song.artist.add_song(song)
-    song
+    # song
   end
 
 end
