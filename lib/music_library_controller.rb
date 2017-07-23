d287ec400940e20a646590a4ba18d992bc73e52a
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-07-23 18:17:40 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -4,8 +4,6 @@ class MusicLibraryController
   end
 
   def call
-    input = ""
-
     while input != "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
