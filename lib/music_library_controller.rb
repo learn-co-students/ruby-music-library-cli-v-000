9966b9a332c4d3e776c3ee38b79f01e3fddf1aba
Donna <github email address>
2017-08-19 16:08:00 -0400

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,26 +1,44 @@
 require 'pry'
+
 class MusicLibraryController
 	extend Concerns::Findable
 	attr_accessor :path
+
 	def initialize(path = './db/mp3s')
 		@path = path
-		library = MusicImporter.new(path).import
+		library = MusicImporter.new(path)
+		library.import
 	end
 
 	def call
-		puts "Welcome to your music library!"
-		puts "To list all of your songs, enter 'list songs'."
-		puts "To list all of the artists in your library, enter 'list artists'."
-		puts "To list all of the genres in your library, enter 'list genres'."
-		puts "To list all of the songs by a particular artist, enter 'list artist'."
-		puts "To list all of the songs of a particular genre, enter 'list genre'."
-		puts "To play a song, enter 'play song'."
-		puts "To quit, type 'exit'."
-		puts "What would you like to do?"
-		input = gets.strip
-		until input == 'exit'
-			puts "what would you like to do?"
+		input = ""
+		while input != "exit"
+			puts "Welcome to your music library!"
+			puts "To list all of your songs, enter 'list songs'."
+			puts "To list all of the artists in your library, enter 'list artists'."
+			puts "To list all of the genres in your library, enter 'list genres'."
+			puts "To list all of the songs by a particular artist, enter 'list artist'."
+			puts "To list all of the songs of a particular genre, enter 'list genre'."
+			puts "To play a song, enter 'play song'."
+			puts "To quit, type 'exit'."
+			puts "What would you like to do?"
+			
 			input = gets.strip
+
+			case input
+			when "list songs"
+				list_songs
+			when "list artists"
+				list_artists
+			when "list genres"
+				list_genres
+			when "list artist"
+				list_songs_by_artist
+			when "list genre"
+				list_songs_by_genre
+			when "play song"
+				play_song
+			end
 		end
 	end
 	
