cd5cd3e068c21f153430446819e1e35795c265a4
roitblatari <roitblatari@gmail.com>
2017-08-22 19:58:22 -0400

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,6 +3,7 @@ module Concerns::Findable
    def find_by_name(name)
     all.detect{|i| i.name == name} 
   end
+  
   def find_or_create_by_name(name)
     self.find_by_name(name) ?
     self.find_by_name(name) : self.create(name)
--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -37,12 +37,32 @@ class MusicLibraryController
     def list_songs_by_artist 
         puts "Please enter the name of an artist:"
         input = gets
+        artist = Artist.find_by_name(input)
+        if artist
+            artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+                puts "#{i}. #{s.name} - #{s.genre.name}"
+            end  
+        end
+    end
+    def list_songs_by_genre
+        puts "Please enter the name of a genre:"
+        input = gets
+        genre = Genre.find_by_name(input)
+        if genre
+            genre.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+                puts "#{i}. #{s.artist.name} - #{s.name}"
+            end 
+        end
+    end
+    def play_song
+        puts "Which song number would you like to play?"
+        input = gets.strip.to_i
         
-        Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
-            puts "#{i}. #{a.name} - #{a.song.name} - #{a.genre.name}"
-         end   
-       
-        
+        if (1..Song.all.length).include?(input)
+            song = Song.all.sort{ |a, b| a.name <=> b.name }[input-1]
+        end
+        puts "Playing #{song.name} by #{song.artist.name}" if song
     end
 end
 
+ 
