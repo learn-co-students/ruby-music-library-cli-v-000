f5a9773a505ac48584e0ebc7b020803f552525ce
Justin Tooley <jstooley823@gmail.com>
2017-09-18 02:30:41 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -21,36 +21,43 @@ class MusicLibraryController
   end
 
   def list_songs
-    song = []
-    name = []
-    sort_list = []
+    song_names = []
+    Songs.all.each_with_indexdo |x, i| #gets names of all songs
+      song_names[i] = x.name
+    end
+    #song = []
+    #name = []
+    #sort_list = []
 
-    @list.each_with_index do |list, i| #creates classes
-      song[i] = Song.create_from_filename(list)
-      name[i] = song[i].name # just a vector of song names
-    end
+    #@list.each_with_index do |list, i| #creates classes
+      #song[i] = Song.create_from_filename(list)
+      #name[i] = song[i].name # just a vector of song names
+    #end
 
-    name_list = name.sort #sorts into alpha order
-    song.each do |list|
-      name_list.each_with_index do |name , i|
-        if list.name == name
-          sort_list[i] = list
-        end
-      end
-    end
-    sort_list.each_with_index do |list, i|  # print in order
-      puts "#{i+1}. #{list.artist.name} - #{list.name} - #{list.genre.name}"
-    end
+    #name_list = name.sort #sorts into alpha order
+    #song.each do |list|
+      #name_list.each_with_index do |name , i|
+        #if list.name == name
+          #sort_list[i] = list
+        #end
+      #end
+    #end
+    #sort_list.each_with_index do |list, i|  # print in order
+      #puts "#{i+1}. #{list.artist.name} - #{list.name} - #{list.genre.name}"
+    #end
+    binding.pry
   end
 
   def list_artists
     artist_names =[]
-    Artist.all.each_with_index do |x, i|
+
+    Artist.all.each_with_index do |x, i| #gets names of all arthist
       artist_names[i] = x.name
     end
-    artist_names.sort!
-  
-    artist_names.each_with_index do |name, i|
+
+    artist_names.sort! # alpha order
+
+    artist_names.each_with_index do |name, i| # prints names
       puts "#{i+1}. #{name}"
     end
 
