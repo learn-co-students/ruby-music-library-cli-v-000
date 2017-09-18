caaa249c72b34bfc4cd958f5804f4d043e58a950
Justin Tooley <jstooley823@gmail.com>
2017-09-18 05:36:53 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,6 +17,19 @@ class MusicLibraryController
     input = nil
     until input == "exit"
       input = gets.chomp
+      if input == "list songs"
+        self.list_songs
+      elsif input == "list artists"
+        self.list_artists
+      elsif input == "list genres"
+        self.list_genres
+      elsif input == "list artist"
+        self.list_songs_by_artist
+      elsif input == "list genre"
+        self.list_songs_by_genre
+      elsif input == "play song"
+        self.play_song
+      end
     end
   end
 
