dc6864132f424fdca270969fadbaa9d874da7629
Justin Tooley <jstooley823@gmail.com>
2017-09-18 00:40:39 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  def self.all
+    @@all.dup.freeze
+  end
+  def save
+    @@all << self
+    self
+  end
+  def self.destroy_all
+    @@all.clear
+  end
+  def add_song(song)
+    self.songs << song unless self.songs.include?(song)
+    song.artist = self unless song.artist != nil
+  end
+  def songs
+    @songs
+  end
+  def genres
+    genre = []
+    @songs.collect do |song|
+      genre << song.genre
+    end
+    genre.uniq
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+
+class Song
+  extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :artist, :genre
+  @@all = []
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist=(artist) unless artist == nil
+    self.genre=(genre) unless genre == nil
+  end
+  def self.all
+    @@all.dup.freeze
+  end
+  def save
+    @@all << self
+    self
+  end
+  def self.destroy_all
+    @@all.clear
+  end
+  def artist=(name)
+    @artist = name unless @artist != nil
+    name.add_song(self) unless name.songs.include?(self)
+  end
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+end
