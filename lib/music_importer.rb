643a6e21dac39cb76ca5bdbaceb56d24b14455df
Gustavo Villagrana <gusvilla303@gmail.com>
2017-09-20 19:37:42 +0000

    Add #list_songs to MusicLibraryController

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -4,7 +4,7 @@ class MusicImporter
 
   def initialize(path)
     @path = path
-    @files = Dir["#{path}/*.mp3"].each {|file| file.slice!("./spec/fixtures/mp3s/")}
+    @files = Dir["#{path}/*.mp3"].each {|file| file.slice!("#{path}/")}
   end
 
   def files
--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,22 +1,30 @@
+require 'pry'
+
 class MusicLibraryController
 
   def initialize(path="./db/mp3s")
-    music_importer = MusicImporter.new(path).import
+    @songs = MusicImporter.new(path).import
   end
 
   def call
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
+    loop do
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
 
-    input = gets.strip
-    controller(input)
+      input = gets.strip
+      if input == 'exit'
+        break
+      else
+        controller(input)
+      end
+    end
   end
 
   def controller(input)
@@ -27,10 +35,16 @@ class MusicLibraryController
     when 'list artist'  then list_artist
     when 'list genre'   then list_genre
     when 'play song'    then play_song
+    end
   end
 
   def list_songs
 
+
+    Song.all.sort_by {|song| song.name}
+    .each_with_index do |song, index|
+      puts "#{index +1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
   end
 
 
