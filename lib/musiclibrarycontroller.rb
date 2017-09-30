373cf4ed0da62e5f6a78875dd681e7a87715b844
Melissa Amaya <amaya.melissa@gmail.com>
2017-09-30 03:48:54 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -16,47 +16,58 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-      answer = gets
+      answer = gets.chomp
+
+      case answer
+        when "list songs"
+          list_songs
+        when "list artists"
+          list_artists
+        when "list genres"
+          list_genres
+        when "list artist"
+          list_songs_by_artist
+        when "list genre"
+          list_songs_by_genre
+        when "play song"
+          play_song
+      end
+
       break if answer == "exit"
     end
   end
 
   def list_songs
-    records = []
-    @collection.collect do |instance|
-      records << instance.split(" - ")
-    end
-
-    trimmed = records.each do |entry|
-      breakup = entry[2].split(".")
-      entry[2] = breakup[0]
-    end
-
-    sorted = records.sort { |a, b| a[1] <=> b[1] }
-
+    songs = Song.all
+    sorted = songs.uniq.sort { |a, b| a.name <=> b.name }
     sorted.each_with_index do |entry, index|
-      sorted = entry.join(" - ")
-
-      puts "#{index + 1}. #{sorted}"
+      puts "#{index + 1}. #{entry.artist.name} - #{entry.name} - #{entry.genre.name}"
     end
   end
 
   def list_artists
-    records = []
-    artists = []
-    @collection.collect do |instance|
-      records << instance.split(" - ")
+    artists = Artist.all
+    sorted = artists.uniq.sort { |a, b| a.name <=> b.name }
+    sorted.each_with_index do |entry, index|
+      puts "#{index + 1}. #{entry.name}"
     end
+  end
 
-    records.each do |entry|
-    artists <<  entry[0]
+  def list_genres
+    genres = Genre.all
+    sorted = genres.uniq.sort { |a, b| a.name <=> b.name }
+    sorted.each_with_index do |entry, index|
+      puts "#{index + 1}. #{entry.name}"
     end
+  end
 
-    sorted = artists.sort { |a, b| a <=> b}
-
-    sorted.uniq.each_with_index do |entry, index|
-      puts "#{index + 1}. #{entry}"
-      binding.pry
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    artist = gets.chomp
+    singer = Artist.find_or_create_by_name(artist)
+    sorted = singer.songs.sort { |a, b| a.song.name <=> b.song.name }
+    sorted.songs.each_with_index do |song, index|
+      puts "#{index + 1}. #{song.name} - #{song.genre.name}"
     end
   end
 end
