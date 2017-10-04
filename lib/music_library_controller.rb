ddbc5682fff9aac230c11cf7741216fa387a4583
Christopher Harvey <chharvey@icloud.com>
2017-10-04 21:33:57 +0000

    Done.

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,90 @@
+class MusicLibraryController
+
+  def initialize(path="./db/mp3s")
+    MusicImporter.new(path).import
+  end
+
+  def call
+    loop do
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      input = gets.strip
+      if input == "list songs"
+        list_songs
+      elsif input == "list artists"
+        list_artists
+      elsif input == "list genres"
+        list_genres
+      elsif input == "list artist"
+        list_songs_by_artist
+      elsif input == "list genre"
+        list_songs_by_genre
+      elsif input == "play song"
+        play_song
+      end
+      break if input == "exit"
+    end
+  end
+
+  def list_songs
+    #There is something wrong with the way that I am accessing the second level information (song artist and genre for example)
+    #Song.all.sort_by!{|song| song.name}
+    Song.all.sort_by{|song| song.name}.each_with_index do |song, index|
+      puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+  end
+
+  def list_artists
+    alpha_artists = Artist.all.sort_by{|artist| artist.name}
+    alpha_artists.each_with_index do |artist, index|
+      puts "#{index+1}. #{artist.name}"
+    end
+  end
+
+  def list_genres
+    alpha_genres = Genre.all.sort_by{|genre| genre.name}
+    alpha_genres.each_with_index do |genre, index|
+      puts "#{index+1}. #{genre.name}"
+    end
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+    artist = Artist.find_by_name(input)
+    if artist != nil
+      artist.songs.sort_by{|song| song.name}.each_with_index do |song, index|
+        puts "#{index+1}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+    genre = Genre.find_by_name(input)
+    if genre != nil
+      genre.songs.sort_by{|song| song.name}.each_with_index do |song, index|
+        puts "#{index+1}. #{song.artist.name} - #{song.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip.to_i
+    alphabetized_songs = Song.all.sort_by!{|song| song.name}
+    if input > 0 && input < alphabetized_songs.length
+      user_selected_song = alphabetized_songs[input.to_i-1]
+      puts "Playing #{user_selected_song.name} by #{user_selected_song.artist.name}"
+    end
+  end
+
+end
