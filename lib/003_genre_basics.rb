470528756ada6ea7d13085b6318d8e376a63c886
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-10-04 23:46:40 +0000

    Done.

---
+++ lib/003_genre_basics.rb
@@ -0,0 +1,32 @@
+class Genre
+  attr_accessor :name, :songs, :artists
+  extend Concerns::Findable
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+    genre
+  end
+
+  def artists
+    self.songs.collect {|x| x.artist}.uniq
+  end
+end
