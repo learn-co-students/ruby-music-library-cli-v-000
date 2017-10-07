39895e6243aca862477ac92bb609fd52eb3ded69
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-10-07 02:29:49 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    self.new(name).save
+    binding.pry
+  end
+end
