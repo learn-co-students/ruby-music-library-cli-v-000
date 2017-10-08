2a5560e90881de15e94a21c37e450ae0b31b2b19
Tony Distinti <adistinti@gmail.com>
2017-10-08 17:55:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -42,4 +42,16 @@ class Song
     self.genre.add_song(self)
   end
 
+  def self.find_by_name(name)
+    self.all.detect { |s| s.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create(name)
+    end
+  end
+
 end
