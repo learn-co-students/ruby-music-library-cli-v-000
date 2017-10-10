212d1fe5f44cf6355a660cbd172c85f107f6c162
Hannah Danziger <hdanziger8@gmail.com>
2017-10-10 13:13:55 +0000

    Done.

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -1,9 +1,15 @@
 class MusicImporter
 
-  directory = "./spec/fixtures/mp3s"
+  def initialize
+    #accepts file path to directory of MP3 files
+  end
 
-  def initialize(directory)
-    
+  def files
+    #returns filenames
+  end
+
+  def self.import
+    #imports all files from library, instantiating a new song for each file
   end
 
 end
