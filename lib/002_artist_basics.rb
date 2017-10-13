6ae0a3114e544ed051ee52bb0ae14c43e064a2c3
Yomaira Escano <yjescano@gmail.com>
2017-10-13 15:21:48 +0000

    Done.

---
+++ lib/002_artist_basics.rb
@@ -0,0 +1,39 @@
+class Artist
+  attr_accessor :name, :songs
+  extend Concerns::Findable
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    self.new(name).tap{|artist| artist.save}
+  end
+
+  def add_song(song)
+    song.artist = self unless song.artist == self
+    @songs << song unless @songs.include?(song)
+  end
+
+  def genres
+    self.songs.collect do |song|
+      song.genre
+    end.uniq
+  end
+
+end
---
+++ lib/concerns/008_findable_module.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+module Concerns::Findable
+
+  def find_by_name(name)
+    self.all.detect do |song|
+      song.name == name
+    end
+  end
+
+  def find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+
+end
