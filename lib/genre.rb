e2b7d8a961038793a7fe1968de70afce1447b7e8
Melissa Rudick <mel.madrugada@gmail.com>
2017-10-14 22:42:24 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    genre
+  end
+end
