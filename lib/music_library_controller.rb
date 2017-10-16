423eb99b10ea7e53b33cff727383a10e3a904277
Sergio Correa <correa.sergio@gmail.com>
2017-10-16 02:29:22 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -15,6 +15,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+
     input = gets.strip
 
     case input
