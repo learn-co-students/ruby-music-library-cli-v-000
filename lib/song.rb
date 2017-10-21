684fe1ebe46cfa9263d32371c5950a0c083443ae
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-21 12:16:43 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -18,17 +18,11 @@ class Song
   end
 
   def self.find_by_name(song_name)
-    @@all.first {|song| song.name == song_name}
+    @@all.detect {|song| song.name == song_name}
   end
 
   def self.find_or_create_by_name(song_name)
-    song = self.find_by_name(song_name)
-      if(song == nil)
-        #binding.pry
-        self.create(song_name)
-      else
-        song
-      end
+    self.find_by_name(song_name) || self.create(song_name)
   end
 
   def self.create(name)
