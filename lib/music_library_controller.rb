8542b2626a790836ae6d74868efd60e285a16716
Jessica Bensch <github email address>
2017-10-30 17:49:46 -0400

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,5 +1,7 @@
 class MusicLibraryController
 
+  attr_accessor :artist, :genre
+
   def initialize(path="./db/mp3s")
     @importer = MusicImporter.new(path)
     @importer.import
@@ -23,13 +25,57 @@ class MusicLibraryController
   end
 
   def list_songs
-    puts @importer.files.sort
+    counter = 1
+    sort = []
+    @importer.files.collect do |song|
+      info = song.split(" - ")
+      genre = info[2].split(".")
+      info.pop()
+      genre.pop()
+      genre_s = genre.join()
+      info << genre_s
+      sort << info
+    end
+    sorted = sort.sort_by do |array|
+      array[1]
+    end
+    sorted_final = sorted.collect do |array|
+      array.join(" - ")
+    end
+    sorted_final.each do |song|
+      puts "#{counter}. #{song}"
+      counter += 1
+    end
+
   end
 
   def list_artists
+    artists = []
+    counter = 1
+    all_songs = Artist.all
+    all_songs.each do |song|
+      artists << song.name
+    end
+    artists_sorted = artists.sort()
+    artists_sorted.each do |artist|
+      puts "#{counter}. #{artist}"
+      counter += 1
+    end
   end
 
   def list_genres
+    genres = []
+    counter = 1
+    all_songs = Genre.all
+    all_songs.each do |song|
+      genres << song
+    end
+    genres_sorted = genres.sort()
+    genres_sorted.each do |genre|
+      puts "#{counter}. #{genre}"
+      counter += 1
+      binding.pry
+    end
   end
 
   def list_songs_by_artist
