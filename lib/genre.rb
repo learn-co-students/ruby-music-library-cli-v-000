6d2bd15183875d498795c2bd972de2cb926823d4
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-30 21:42:14 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,27 @@
 class Genre
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(genre)
+    created_genre = self.new(genre)
+    created_genre.save
+    created_genre
+  end
 
 end
