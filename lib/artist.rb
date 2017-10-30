f1a217685e7de2843bdcddffbe7a129c57e2d4a4
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-30 21:37:37 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,27 @@
 class Artist
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(artist)
+    created_artist = self.new(artist)
+    created_artist.save
+    created_artist
+  end
 
 end
