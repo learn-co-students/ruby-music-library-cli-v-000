61e845c9d7579535080936b4f36282cab5b64b4b
Jessica Bensch <github email address>
2017-10-31 17:26:12 -0400

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,45 +1,43 @@
 class MusicLibraryController
 
-  attr_accessor :artist, :genre,
+  attr_accessor :artist, :genre, :path
 
-  def initialize(path="./db/mp3s")
-    @importer = MusicImporter.new(path)
-    @importer.import
-  end
-
-  def call
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    input = gets
-    while input != "exit"
-      puts "What would you like to do?"
-      input = gets.strip
-
-      case input
-      when "list songs"
-        list_songs
-      when "list artists"
-        list_artists
-      when "list genres"
-        list_genres
-      when "list artist"
-        list_songs_by_artist
-      when "list genre"
-        list_songs_by_genre
-      when "play song"
-        play_song
-      end
-
-
+  def initialize(path = "./db/mp3s")
+      MusicImporter.new(path).import
     end
-  end
+
+    def call
+      input = ""
+
+      while input != "exit"
+        puts "Welcome to your music library!"
+        puts "To list all of your songs, enter 'list songs'."
+        puts "To list all of the artists in your library, enter 'list artists'."
+        puts "To list all of the genres in your library, enter 'list genres'."
+        puts "To list all of the songs by a particular artist, enter 'list artist'."
+        puts "To list all of the songs of a particular genre, enter 'list genre'."
+        puts "To play a song, enter 'play song'."
+        puts "To quit, type 'exit'."
+        puts "What would you like to do?"
+
+        input = gets.strip
+
+        case input
+        when "list songs"
+          list_songs
+        when "list artists"
+          list_artists
+        when "list genres"
+          list_genres
+        when "list artist"
+          list_songs_by_artist
+        when "list genre"
+          list_songs_by_genre
+        when "play song"
+          play_song
+        end
+      end
+    end
 
   def list_songs
     #counter = 1
@@ -123,13 +121,14 @@ class MusicLibraryController
   end
 
   def play_song
-     puts "Which song number would you like to play?"
+    puts "Which song number would you like to play?"
 
-     input = gets.strip.to_i
-     if (1..Song.all.length).include?(input)
-       song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
-     end
+    input = gets.strip.to_i
+    if (1..Song.all.length).include?(input)
+      song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+    end
 
-     puts "Playing #{song.name} by #{song.artist.name}" if song
-   end
- end
+    puts "Playing #{song.name} by #{song.artist.name}" if song
+  end
+
+end
