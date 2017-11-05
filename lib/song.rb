4da6d107897c6d759d1c2240ad0d1b9388f8b5fc
arianaberger <ariana.berger@gmail.com>
2017-11-05 14:50:59 -0500

    Update song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -38,4 +38,19 @@ class Song
     song
   end
 
+def self.find_by_name(name)
+    all.detect {|song| song.name == name}   #use double equals!!
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+  def self.new_from_filename(file_name) # not done, needs tweaking
+    song = Song.new(file_name.split(" - ")[1])
+    song.artist = file_name.split(" - ")[0]
+    song
+  end
+
 end
+
