28cdb1eef48b01c1a4ec914068d7c8160b3fa567
Anastasia  Barkett <anastasia.barkett@gmail.com>
2017-11-08 02:20:55 +0000

    Created classes and relationships

---
+++ lib/genre.rb
@@ -0,0 +1,35 @@
+class Genre
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new(name).tap{|g| g.save}
+  end
+
+  def artists
+    songs.collect {|s| s.artist}.uniq
+  end
+end
