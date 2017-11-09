701e6cb279c0d483f94561d70735e29b393c4344
Shibani Mookerjee <shibani.mookerjee@gmail.com>
2017-11-09 03:03:12 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -20,6 +20,21 @@ class MusicLibraryController
     until input == "exit"
       puts "What would you like to do?"
       input = gets.chomp
+
+      case input
+        when "list songs"
+          list_songs
+        when "list artists"
+          list_artists
+        when "list genres"
+          list_genres
+        when "list artist"
+          list_songs_by_artist
+        when "list genre"
+          list_songs_by_genre
+        when "play song"
+          play_song
+      end
     end
   end
 
