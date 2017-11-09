ffdfd66de8c9a7160e608634851c6cfba5015f9d
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-09 23:47:04 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -17,11 +17,24 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-      input = gets.chomp
-      if input == "exit"
+      input = gets.strip.downcase
+      if input == "list songs"
+        list_songs
+      elsif input == "list artists"
+        list_artists
+      elsif input == "list genres"
+        list_genres
+      elsif input == "list artist"
+        list_songs_by_artist
+      elsif input == "list genre"
+        list_songs_by_genre
+      elsif input == "play song"
+        play_song
+      elsif input == "exit"
         break
       end
     end
+
   end
 
   def list_songs
@@ -74,8 +87,11 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
-    song_to_play = gets.chomp
-    
+    song_to_play = gets.chomp.to_i
+    #binding.pry
+    if song_to_play > 0 && song_to_play <= Song.all.length
+      puts "Playing #{Song.all.sort_by{|song| song.name}[song_to_play-1].name} by #{Song.all.sort_by{|song| song.name}[song_to_play-1].artist.name}"
+    end
   end
 
 end
