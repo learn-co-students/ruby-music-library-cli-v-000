d70ca32fd04294b5025f75e62fd8cd19a45c5a0d
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-15 01:27:34 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -42,5 +42,10 @@ class Song
   end
 
   def self.find_by_name(name)
+    self.all.detect { |obj| obj.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
   end
 end
