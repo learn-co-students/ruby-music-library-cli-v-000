5566809ec1a3f1a3c8ab96d862465de27f9ec77c
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-15 22:21:55 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -15,6 +15,15 @@ class MusicLibraryController
     puts "What would you like to do?"
     user_input = gets.chomp
     while user_input != 'exit'
+      self.list_songs if user_input == 'list songs'
+      self.list_artists if user_input == 'list artists'
+      self.list_genres if user_input == 'list genres'
+      self.list_songs_by_artist if user_input == 'list artist'
+      self.list_songs_by_genre if user_input == 'list genre'
+      if user_input == 'play song'
+        self.list_songs
+        self.play_song
+      end
       user_input = gets.chomp
     end
   end
@@ -41,13 +50,30 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     user_artist = gets.chomp
 
-    while !(Artist.find_by_name(user_artist))
-      puts "Please enter the name of an artist:"
-      user_artist = gets.chomp
+    if Artist.all.any?{ |artist| artist.name == user_artist }
+      Artist.all.detect{ |artist| artist.name == user_artist }.songs.sort_by{ |song| song.name }.each.with_index do |song, index|
+        puts "#{index + 1}. #{song.name} - #{song.genre.name}"
+      end
     end
+  end
 
-    Artist.all.detect{ |artist| artist.name == user_artist }.songs.sort_by{ |song| song.name }.each.with_index do |song, index|
-      puts "#{index + 1}. #{song.name}"
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    user_genre = gets.chomp
+
+    if Genre.all.any?{ |genre| genre.name == user_genre }
+      Genre.all.detect{ |genre| genre.name == user_genre }.songs.sort_by{ |song| song.name }.each.with_index do |song, index|
+        puts "#{index + 1}. #{song.artist.name} - #{song.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    user_selection = gets.chomp.to_i
+    song = Song.all.sort_by{ |song| song.name }[user_selection - 1]
+    if (song.instance_of? Song) && (user_selection >= 1) && (user_selection <= Song.all.size)
+      puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
 end
