e85b2142b239b53185b9f3aefa64edb89d8cbf8e
Olena Ageyeva <eageeva@hotmail.com>
2017-11-20 13:15:09 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Concerns
   module Findable
     def find_by_name(name)
-      all.detect{ |song| song.name == name }.class
+      all.detect{ |song| song.name == name }
     end
 
     def find_or_create_by_name(name)
