59e060c6678cfb00429b650b3cbd869c289cf251
github username <github email address>
2017-11-20 21:33:43 -0600

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        self.all.clear
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.create(name)
+        self.new(name).save.last
+    end
+
+    def songs
+        @songs
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+class Song
+
+    attr_accessor :name, :song
+
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        self.all.clear
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.create(name)
+        self.new(name).save.last
+    end
+end
