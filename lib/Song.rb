6bc4a10eed21fe63a8aaf89a23148075f13265c9
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2017-11-28 21:01:40 -0500

    Rspeclemented 001_song_basics_spec

---
+++ lib/Song.rb
@@ -0,0 +1,27 @@
+class Song
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+    end
+
+    def self.create(name)
+        song = Song.new(name)
+        song.save
+        song
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        @@all.clear
+    end
+
+    def save
+        @@all << self
+    end
+end
