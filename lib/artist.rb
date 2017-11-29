e0d9ff24ff319191f64a55f691a478a0d4dc5877
Blakeley Bond <burrick2003@yahoo.com>
2017-11-29 00:52:19 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,10 +25,9 @@ attr_accessor :name, :songs
   end
 
   def add_song(song)
-    @songs << song #need to add test also for preexist
-      if song.artist.is_a?(Artist) #meet spec, don't overwrite existing artists instance in song/artist slot
-      else
+      if !song.artist.is_a?(Artist) && !@songs.include?(song)#meet spec, don't overwrite existing artists instance in song/artist slot
         song.artist = self
+        @songs << song #need to add test also for preexist / done, works
       end
   end
 
