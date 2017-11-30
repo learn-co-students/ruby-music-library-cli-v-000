a9096dda6ea1fb75129dcda29a56b78799c1bb3f
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-30 00:37:42 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,27 @@
+class Genre
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+    genre
+  end
+end
