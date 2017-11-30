7608eb9cc53967a17f728e08747c8427f9735d74
Blakeley Bond <burrick2003@yahoo.com>
2017-11-30 05:08:15 +0000

    Done.

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,32 +1,24 @@
 class MusicImporter
-attr_accessor :path, :dir
-
-
+attr_accessor :path, :files
   def initialize(path)
     @path = path
   end
 
   def files
-    @dir = Dir["#{@path}/*.mp3"].select{ |f| File.file? f }.map{ |f| File.basename f } #thank you stack overflow, I know DOS why isn't there just a flag to exclude path?
-    @dir
+    @files = Dir["#{@path}/*.mp3"].select{ |f| File.file? f }.map{ |f| File.basename f } #thank you stack overflow, I know DOS why isn't there just a flag to exclude path?
+    #@dir
   end
-  def files2
+#  def files2
     #dir = Dir.entries("#{@path}").glob(*.mp3)
-    Dir.chdir("#{@path}")
-    dir = Dir.glob("*.mp3")
-    dir
-  end
+#    Dir.chdir("#{@path}")
+#    dir = Dir.glob("*.mp3")
+#    dir
+#  end
 
-  def self.import
-    self.dir.each do |file|
+  def import
+    self.files.each do |file|
       Song.create_from_filename(file)
     end
   end
-
-
-
-
-
-
 
 end
