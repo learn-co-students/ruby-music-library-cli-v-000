d605c562e67e116b5b8164a3e456004a936dd937
Carlota Bindner <cev.bindner@gmail.com>
2017-12-06 18:07:38 +0000

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,12 @@
+
+module Concerns::Findable
+
+  def find_by_name(name)
+    all.detect { |f| f.name == name }
+  end
+
+  def find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,41 @@
+class Genre
+  extend Concerns::Findable
+
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    genre = new(name)
+    genre.save
+    genre
+  end
+
+  def add_song(song)
+    song.genre = self unless song.genre
+    songs << song unless songs.include?(song)
+  end
+
+  def artists
+    songs.collect { |s| s.artist }.uniq
+  end
+
+end
