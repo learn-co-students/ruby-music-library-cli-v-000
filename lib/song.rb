1cc99dddfdbcd71d102d63e8e8372044c4336cb1
Jacqueline Lee <anginblu@gmail.com>
2017-12-08 22:23:05 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,135 @@
+class Song
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist
+    self.genre = genre
+  end
+
+  def artist
+    @artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) unless artist == nil
+  end
+
+  def genre
+    @genre
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self) unless genre == nil
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new = self.new(name)
+    new.save
+    new
+  end
+
+end
+
+class Artist
+  attr_accessor :name, :song
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new = self.new(name)
+    new.save
+    new
+  end
+
+  def songs
+    @songs.uniq
+  end
+
+  def add_song(song)
+    @songs << song unless @songs.include?(song)
+    song.artist = self unless song.artist == self
+  end
+
+  def genres
+    collection = []
+    @songs.each do |song| collection << song.genre end
+    collection.uniq
+  end
+
+end
+
+class Genre
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    collection = []
+    @songs.each do |song| collection << song.artist end
+    collection.uniq
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new = self.new(name)
+    new.save
+    new
+  end
+
+  def add_song(song)
+    @songs << song unless @songs.include?(song)
+    song.genre = self unless song.genre == self
+  end
+
+end
