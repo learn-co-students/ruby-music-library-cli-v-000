abc64642b1e359085cd40d7a3e0fd49be0496888
Chad Montoya <chadmontoya21@yahoo.com>
2017-12-08 05:32:23 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,9 @@
 require 'pry'
 class MusicLibraryController
+  attr_accessor :path
 
   def initialize(path="./db/mp3s")
+    @path = path
     importer = MusicImporter.new(path)
     importer.import
   end
@@ -9,22 +11,47 @@ class MusicLibraryController
   def call
     input = ""
     until input == "exit"
-      puts "Welcome to your music library!"
-      puts "To list all of your songs, enter 'list songs'."
-      puts "To list all of the artists in your library, enter 'list artists'."
-      puts "To list all of the genres in your library, enter 'list genres'."
-      puts "To list all of the songs by a particular artist, enter 'list artist'."
-      puts "To list all of the songs of a particular genre, enter 'list genre'."
-      puts "To play a song, enter 'play song'."
-      puts "To quit, type 'exit'."
-      puts "What would you like to do?"
+      options
       input = gets.strip
+      case input
+      when 'list songs'
+        list_songs
+      when 'list artists'
+        list_artists
+      when 'list genres'
+        list_genres
+      when 'list artist'
+        list_songs_by_artist
+      when 'list genre'
+        list_songs_by_genre
+      when 'play song'
+        play_song
+      end
     end
   end
 
+  def options
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+  end
+
+
+# ////////////////CLI METHODS//////////////////////////////////
   def list_songs
     Song.all.sort_by! {|s| s.name}.collect.with_index(1) do |s, i|
        puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+     end
+  end
+
+  def list_songs_for_play
+    Song.all.sort_by! {|s| s.name}.collect.with_index(1) do |s, i|
        "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
      end
   end
@@ -77,9 +104,8 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     song_number = gets.strip
-      while song_number != nil
-        chosen = self.list_songs.find {|s| s.include?(song_number)}
-        binding.pry
+      if song_number.to_i > 0 && song_number.to_i <= Song.all.count
+        chosen = self.list_songs_for_play.find {|s| s.include?(song_number)}
         artist, song = chosen.split(' - ')
         artist.slice!(0, 3)
         puts "Playing #{song} by #{artist}"
