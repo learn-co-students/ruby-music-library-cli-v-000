fb3f49afd9c044a24de1bd766dd509ae779f9be9
Liz Burton <liz.burton147@gmail.com>
2017-12-10 15:56:07 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,4 +1,5 @@
 class MusicLibraryController
+  attr_accessor :song_list
 
   def initialize(path="./db/mp3s")
     music_importer = MusicImporter.new(path)
@@ -21,4 +22,53 @@ class MusicLibraryController
     end
   end
 
+  def list_songs
+    @song_list = Song.all.sort_by { | song | song.name }
+    @song_list.each.with_index(1) { | song, i | puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}" }
+  end
+
+  def list_artists
+    sorted = Artist.all.sort_by { | artist | artist.name }
+    sorted.each.with_index(1) { | artist, i | puts "#{i}. #{artist.name}" }
+  end
+
+  def list_genres
+    sorted = Genre.all.sort_by { | genre | genre.name }
+    sorted.each.with_index(1) { | genre, i | puts "#{i}. #{genre.name}" }
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    artist_name_input = gets.strip
+    selected_artist = Artist.find_by_name(artist_name_input)
+    if selected_artist
+      sorted = selected_artist.songs.sort_by { | song | song.name }
+      sorted.each.with_index(1) { | song, i | puts "#{i}. #{song.name} - #{song.genre.name}" }
+    end
+
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    genre_name_input = gets.strip
+    selected_genre = Genre.find_by_name(genre_name_input)
+    if selected_genre
+      sorted = selected_genre.songs.sort_by { | song | song.name }
+      sorted.each.with_index(1) { | song, i | puts "#{i}. #{song.artist.name} - #{song.name}" }
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    song_number_input = gets.strip.to_i
+    if song_number_input > 0 && song_number_input <= Song.all.length
+      playing = Song.all.sort_by { | song | song.name }.detect.with_index(1) { | song, index | index == song_number_input }
+      puts "Playing #{playing.name} by #{playing.artist.name}"
+    end
+  end
+
 end
+
+
+# music_library_controller = MusicLibraryController.new("./spec/fixtures/mp3s")
+# music_library_controller
