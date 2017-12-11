fb3f87f888bb4f4f455b6c25811bfdd91be2642f
Araba Taylor <araba.taylor20@gmail.com>
2017-12-11 02:19:00 +0000

    Done.

--- lib/concerns.rb
+++ lib/concerns.rb
@@ -1,9 +1,18 @@
 module Concerns
   module Findable
     def find_by_name(name)
-      if  self.all.find {|song| song.name == name}
-        self
+      if  y =  self.all.find {|x| x.name == name}
+        y
       end
     end
+
+    def find_or_create_by_name(name)
+      if self.find_by_name(name)
+        self.find_by_name(name)
+      else
+        self.create(name)
+      end
+    end
+
   end
 end
