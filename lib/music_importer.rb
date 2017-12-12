e74686d9244125a5fcd4a56ba23236b49da39cff
Alyssa Clair <clairaly13@gmail.com>
2017-12-12 21:06:27 +0000

    Done.

---
+++ lib/music_importer.rb
@@ -0,0 +1,17 @@
+class MusicImporter
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+   Dir.entries(@path).select do |filename|
+     filename.include? ".mp3"
+   end
+  end
+
+  def import
+    files.each{ |file| Song.create_from_filename(file) }
+  end
+end
