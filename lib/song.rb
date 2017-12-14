c230673e161fcc511c78ac910c6075aaa0117bf0
Charity <Youngblood.charity@gmail.com>
2017-12-14 13:52:51 -0800

    added comments on how to structure code in song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,17 @@
 require 'pry'
+
+# create class Song
+
+# create initialize method w/argument (name)
+
+# create attr_accessor for name
+
+# create @@all = [] - class variable 
+
+# create class methods for:
+    # - self.all - will return contents of @@all array
+    # - self.destroy_all - set @@all to an empty array
+
+# create save instance method - this method will add Song instances to the @@all array
+
+# create self.create class method - this will initialize a new song and save the song in the @@all array 
