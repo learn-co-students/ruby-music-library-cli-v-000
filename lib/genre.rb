c6b5807fbb6ed80618fd908c61d1c0bffd3fbee0
Charity <Youngblood.charity@gmail.com>
2017-12-14 14:05:35 -0800

    added initialize method to song.rb

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,17 @@
 require 'pry'
+
+# create class Genre 
+
+# create initialize method w/argument (name)
+
+# create attr_accessor for name
+
+# create @@all = [] - class variable 
+
+# create class methods for:
+    # - self.all - will return contents of @@all array
+    # - self.destroy_all - set @@all to an empty array
+
+# create def save (instance method) - this method will add Artist instances to the @@all array
+
+# create self.create class method - this will initialize a new Artist instance and save the Artist in the @@all array 
--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
 require 'pry'
 
 # create class Song
-
+class Song 
 # create initialize method w/argument (name)
-
+  def initialize(name)
+  
+  end 
 # create attr_accessor for name
 
 # create @@all = [] - class variable 
@@ -15,3 +17,5 @@ require 'pry'
 # create save instance method - this method will add Song instances to the @@all array
 
 # create self.create class method - this will initialize a new song and save the song in the @@all array 
+
+end 
