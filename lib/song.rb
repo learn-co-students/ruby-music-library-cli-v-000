57e56a3a56f86602a92184469cfc935b72271852
Charity <Youngblood.charity@gmail.com>
2017-12-14 14:10:02 -0800

    added self.all and self.destroy_all to song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -4,20 +4,28 @@ require 'pry'
 class Song 
 # create attr_accessor for name
   attr_accessor :name 
-  
+# create @@all = [] - class variable
+  @@all = []
 # create initialize method w/argument (name)
   def initialize(name)
   
   end 
-# create attr_accessor for name
-  
-# create @@all = [] - class variable 
 
 # create class methods for:
-    # - self.all - will return contents of @@all array
-    # - self.destroy_all - set @@all to an empty array
+    # - self.all
+  def self.all 
+    # - this method will return contents of @@all array
+    @@all
+  end 
+    # - self.destroy_all 
+  def self.destroy_all
+    # - set @@all to an empty array
+    @@all = []
+  end
 
-# create save instance method - this method will add Song instances to the @@all array
+# create save instance method
+  
+  # - this method will add Song instances to the @@all array
 
 # create self.create class method - this will initialize a new song and save the song in the @@all array 
 
