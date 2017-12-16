771011a2ae8fa6d67e2e3479afefea4d3a36440f
charityyoungblood <youngblood.charity@gmail.com>
2017-12-15 17:51:35 -0800

    added placeholder for find_or_create_by_bname method to findable.rb

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,12 +3,12 @@ module Concerns
   module Findable
     def find_by_name(name) # class method
       # searches the extended class' @@all array for an instance that matches the provided name
-      @@all.collect do |m|
-        binding.pry
-        self.name
-      end
     end
-  end 
+    
+    def find_or_create_by_name(name) # class method
+      # finds an existing instance with the name provided if one exists in @@all array
+    end 
+  end
   
 #  extend Findable::ClassMethods
 #  include Memorable::InstanceMethods
