88cd3154a5ebedc453f0ca83e8889ac99da48bb8
charityyoungblood <youngblood.charity@gmail.com>
2017-12-15 17:43:36 -0800

    added Concerns and Findable modules to findable.rb

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,10 +1,17 @@
-module Findable 
+module Concerns 
   
-  module ClassMethods
+  module Findable
+    def find_by_name # class method
     
+    end
   end 
   
-  
-  
+#  extend Findable::ClassMethods
+#  include Memorable::InstanceMethods
+#  
+#  extend Findable 
+#  
+#  include Paramable
+#  
   
 end 
