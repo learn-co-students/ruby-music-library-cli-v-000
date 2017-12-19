4ff4c2b2f30c40eec452230a870e82763efc8d2d
charityyoungblood <youngblood.charity@gmail.com>
2017-12-19 14:50:18 -0800

    added placeholder for initialize method

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,6 +1,17 @@
 require 'pry'
 
 # create MusicImporter class 
+class MusicImporter
 
 # create initialize method w/(file_path) argument
+  def initialize(file_path) 
+    
+  end 
+# create path method - no arguments 
+  # - this method will get the path passed through the MusicImporter object 
 
+# create files method
+  # this method will add the MP3 files into the path directory 
+  
+  
+end 
