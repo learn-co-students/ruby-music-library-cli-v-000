1a31baae8c0575a7a18a61cc379442c0a829ad2a
charityyoungblood <youngblood.charity@gmail.com>
2017-12-20 08:09:33 -0800

    added default value for path in initialize method for music_library_controller.rb

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -2,7 +2,7 @@ class MusicLibraryController
   
   extend Concerns::Findable 
   
-  def initialize(path)
+  def initialize(path = "./db/mp3s")
     @path = path 
     MusicImporter.new(path)
   end 
