4484ae393f2df0abcda6ed6c0ff65e4048c80cc2
charityyoungblood <youngblood.charity@gmail.com>
2017-12-20 08:30:20 -0800

    created placeholder for call method in  music_library_controller.rb

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,12 +1,14 @@
-class MusicLibraryController 
-  # create 
-   
-  
+class MusicLibraryController  
+  # create initialize method 
   def initialize(path = "./db/mp3s")
     @path = path 
     MusicImporter.new(path).import 
   end 
   
+  # create call method 
+  def call 
+    
+  end 
   
   
   
