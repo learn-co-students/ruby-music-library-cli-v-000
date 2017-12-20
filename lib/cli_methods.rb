26675860619a50ced40dbb9025c8f6ba50eb4cde
charityyoungblood <youngblood.charity@gmail.com>
2017-12-20 09:10:01 -0800

    added comments for how to structure code in cli_methods.rb

--- lib/cli_methods.rb
+++ lib/cli_methods.rb
@@ -2,7 +2,10 @@ class CLI_Methods
   
   
   def list_songs
-    
+    # prints all songs in music library in numerical order
+    # songs are alphabetized by song name 
+    # sort - alphabetize
+    # - need to look up numerical order
   end 
   
   
