783dbb1c497aa747844652a6b074e204382ea83b
charityyoungblood <youngblood.charity@gmail.com>
2017-12-19 16:58:46 -0800

    added split method to new_from_filename in song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -49,14 +49,14 @@ class Song
   end 
   
   def self.new_from_filename(filename)
-    
-    song = Song.new(name)
-    artist = filename.split(" - ")[0] #is artist
-    name = filename.split(" - ")[1] #is name
-    genre = filename.split(" - ")[2] # is genre
-    song.name = name 
-    song.artist = artist
-    song.genre = genre
+    song = Song.new(filename)
+    song.name = filename.split(" - ")[0] #is name
+#    artist = filename.split(" - ")[1] #is artist
+#    genre = filename.split(" - ")[2] # is genre
+#    song.name = name 
+#    song.artist = artist
+#    song.genre = genre
+    song
 #    song = Song.new(name)
 #    song.artist = Artist.find_or_create_by_name(artist)
 #    song.artist.add_song(song)
