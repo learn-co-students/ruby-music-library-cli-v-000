6df42448fb4e262970c1c49ef54d34fff385ed0b
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-22 21:22:48 +0000

    added genre class that is passing basic spec

---
+++ lib/genre.rb
@@ -0,0 +1,25 @@
+class Genre
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    self.new(name).tap {|genre| genre.save}
+  end
+end
