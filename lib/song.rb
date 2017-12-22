fe54f193389ebfb6a200d7fab2202040bd4cf8b3
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-22 21:18:17 +0000

    added song class that is passing basic spec

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    self.new(name).tap {|song| song.save}
+  end
+end
