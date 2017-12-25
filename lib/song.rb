b883fc954dbe73f0e7acd53cd882a1db2ab69194
github username <github email address>
2017-12-24 20:19:57 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -47,5 +47,17 @@ class Song
     song
   end
 
+  def self.new_from_filename(name)
+    artist, song, genre_name = name.split(' - ')
+    fixed_name = genre_name.gsub('.mp3', '')
+    artist = Artist.find_or_create_by_name(artist)
+    genre = Genre.find_or_create_by_name(fixed_name)
+    new(song, artist, genre)
+  end
+
+  def self.create_from_filename(name)
+    new_from_filename(name).save
+  end
+
 
 end
