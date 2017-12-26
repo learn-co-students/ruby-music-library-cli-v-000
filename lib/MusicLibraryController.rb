cf65454fc6efa2a7e768548ba6a1e59e8bce7068
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2017-12-26 18:01:48 -0500

    implemented list_songs_by_artist, refactored list_songs to be mroe robust

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -23,20 +23,22 @@ class MusicLibraryController
     end
 
     def handle_user_input
-        user_input = gets
+        user_input = gets.chomp
         while ('exit' != user_input)
-            this.send(user_input.gsub(' ', '_'))
-            user_input = gets
+            case user_input
+            when 'list songs', 'list artists', 'list genres'
+                this.send(user_input.gsub(' ', '_'))
+            when 'list artist'
+                list_songs_by_artist
+            end
+            user_input = gets.chomp
         end
     end
 
     def list_songs
-        filenames = @music_importer.files
-        filenames.map! {|filename| (filename.split('.'))[0] }
-        filenames.map! {|filename| filename.split(' - ')}
-        filenames.sort_by! {|filename| filename[1]}
-        filenames.map! {|filename| filename.join(' - ')}
-        filenames.each_with_index {|filename, index| puts "#{index+1}. #{filename}"}
+        songs = Song.all
+        songs.sort_by! {|song| song.name}
+        songs.each_with_index {|song, index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
     end
 
     def list_artists
@@ -48,4 +50,16 @@ class MusicLibraryController
         genre_names = Genre.all.map! {|genre| genre.name}
         genre_names.sort.each_with_index {|name, index| puts "#{index+1}. #{name}"}
     end
+
+    def list_songs_by_artist
+        puts "Please enter the name of an artist:"
+        artist_name = gets.chomp
+        artist = Artist.find_by_name(artist_name)
+        if (artist)
+            songs_by_artist = artist.songs
+            songs_by_artist.sort_by! {|song| song.name}
+            songs_by_artist.each_with_index {|song, index| puts "#{index+1}. #{song.name} - #{song.genre.name}"}
+        end
+    end
+  
 end
