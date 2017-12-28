30fbea424cc4f3c0b914a5aeb86d035e0bdd11ad
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-28 15:04:20 +0000

    added music library command triggers

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -21,6 +21,22 @@ class MusicLibraryController
     puts "What would you like to do?"
     user_entry = gets
 
+    case user_entry.strip
+    when 'list songs'
+      list_songs
+    when 'list artists'
+      list_artists
+    when 'list genres'
+      list_genres
+    when 'list artist'
+      list_songs_by_artist
+    when 'list genre'
+      list_songs_by_genre
+    when 'play song'
+      play_song
+    else
+    end
+
     if user_entry != "exit"
       call
     end
