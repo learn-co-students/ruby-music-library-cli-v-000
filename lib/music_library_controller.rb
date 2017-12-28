09afdc9bbd63f9503ee4ce392a4e266f847bc636
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-28 14:54:17 +0000

    implemented recursive loop for #call in MusicLibraryController

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -21,20 +21,12 @@ class MusicLibraryController
     puts "What would you like to do?"
     user_entry = gets
 
-    while user_entry != "exit" do
-      puts "Welcome to your music library!"
-      puts "To list all of your songs, enter 'list songs'."
-      puts "To list all of the artists in your library, enter 'list artists'."
-      puts "To list all of the genres in your library, enter 'list genres'."
-      puts "To list all of the songs by a particular artist, enter 'list artist'."
-      puts "To list all of the songs of a particular genre, enter 'list genre'."
-      puts "To play a song, enter 'play song'."
-      puts "To quit, type 'exit'."
-      puts "What would you like to do?"
-      user_entry = gets
+    if user_entry != "exit"
+      call
     end
   end
 
+########## CLI Methods ##########
   def list_songs
     songs_listed = Song.all.sort { |x, y| x.name <=> y.name }
     songs_listed.each_with_index do |song, index|
