26b8645825b38a51abd70d02fd26a06061dc191d
Brittany Morris <brittmmendez@gmail.com>
2017-12-28 17:47:25 +0000

    Done.

---
+++ lib/genres.rb
@@ -0,0 +1,41 @@
+class Genre
+  attr_accessor :name
+  @@all=[]
+
+  def initialize(name)
+    @name=name
+    @songs=[]
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all<<self
+  end
+
+  def self.create(type)
+    genre = self.new(type)
+    genre.save
+    genre
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs<<song
+  end
+
+  def artists
+    self.songs.collect {|song| song.artist}.uniq
+
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,58 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@all=[]
+
+  def initialize(name, artist=nil, genre=nil)
+    @name=name
+    @artist=artist
+    @genre=genre
+    if artist != nil
+      self.artist=artist
+      end
+    if genre != nil
+     self.genre=genre
+   end
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all<<self
+  end
+
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+
+  def artist=(artist)
+    @artist=artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+   @genre=genre
+    genre.add_song(self) unless genre.songs.detect {|song| song==self}
+  end
+
+  def self.find_by_name(name)
+    all.detect {|song| song.name==name}
+  end
+
+  def self.find_or_create_by_name(name)
+       if song= self.find_by_name(name)
+         song
+      else
+       self.create(name)
+     end
+   end
+
+  end
