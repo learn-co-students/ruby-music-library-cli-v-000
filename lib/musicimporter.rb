2668664090fda6403e1f3cc0e929279876a3256b
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-28 01:50:52 +0000

    added musicimporter class

---
+++ lib/musicimporter.rb
@@ -0,0 +1,24 @@
+class MusicImporter
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+    @files = []
+  end
+
+  def files
+    entries = Dir.entries(self.path)
+    mp3_files = entries.select do |entry|
+    File.extname(entry) == ".mp3"
+    end
+
+    @files = mp3_files
+  end
+
+  def self.import
+    self.files.each do |filename|
+    Song.new_by_filename(filename)
+    end
+  end
+
+end
