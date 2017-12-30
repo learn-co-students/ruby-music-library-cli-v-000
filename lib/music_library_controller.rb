9a55843024f068a08e73fa670c3c94f361d1bb6b
Laura Trager <laura.trager@gmail.com>
2017-12-30 15:35:05 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -5,7 +5,6 @@ class MusicLibraryController
     @music_library = MusicImporter.new(path).import
   end
 
-  #####
   def call
     puts "Welcome to your music library!"
     input = nil
@@ -20,12 +19,33 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets.strip
+      cli_commands(input)
     end
 
   end
 
-  # prints all songs in the music library in a numbered list (alphabetized by song name) - is not hard-coded
+  def cli_commands(input)
 
+    if input == ("list songs")
+      list_songs
+    elsif input == ("list artists")
+      list_artists
+    elsif input == ("list genres")
+      list_genres
+    elsif input == ("list artist")
+      list_songs_by_artist
+    elsif input == ("list genre")
+      list_songs_by_genre
+    elsif input == ("play song")
+      play_song
+    elsif input == ("exit")
+      exit
+    end
+    
+  end
+
+
+  # prints all songs in the music library in a numbered list (alphabetized by song name) - is not hard-coded
   def alpha_song_list
     song_list = Song.all.sort_by!{|song| song.name}
     song_list.uniq #the program much preferred this here than in list_songs
@@ -48,10 +68,9 @@ class MusicLibraryController
   end
 
   # prompts the user to enter an artist
-  # accepts user input
-  # prints all songs by a particular artist in a numbered list (alphabetized by song name)
-  # does nothing if no matching artist is found
-
+    # accepts user input
+    # prints all songs by a particular artist in a numbered list (alphabetized by song name)
+    # does nothing if no matching artist is found
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     artist_name = gets.strip
@@ -67,9 +86,9 @@ class MusicLibraryController
   end
 
   # prompts the user to enter an genre
-  # accepts user input
-  # prints all songs by a particular genre in a numbered list (alphabetized by song name)
-  # does nothing if no matching genre is found
+    # accepts user input
+    # prints all songs by a particular genre in a numbered list (alphabetized by song name)
+    # does nothing if no matching genre is found
   def list_songs_by_genre
     puts "Please enter the name of a genre:"
     genre_name = gets.strip
@@ -84,10 +103,10 @@ class MusicLibraryController
   end
 
   # prompts the user to choose a song from the alphabetized list output by #list_songs
-  # accepts user input
-  # upon receiving valid input 'plays' the matching song from the alphabetized list output by #list_songs
-  # does not 'puts' anything out if a matching song is not found
-  # checks that the user entered a number between 1 and the total number of songs in the library
+    # accepts user input
+    # upon receiving valid input 'plays' the matching song from the alphabetized list output by #list_songs
+    # does not 'puts' anything out if a matching song is not found
+    # checks that the user entered a number between 1 and the total number of songs in the library
   def play_song
     puts "Which song number would you like to play?"
 
@@ -101,7 +120,5 @@ class MusicLibraryController
       end
 
     end
-
   end
-
 end
