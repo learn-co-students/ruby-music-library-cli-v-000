8829fc716661c1d5581f47d7322d83a89f45eee1
rh24 <ms.rebeccahong@gmail.com>
2017-12-31 22:10:00 -0500

    refactor puts statements

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -10,8 +10,8 @@ class MusicLibraryController
     while input != 'exit'
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
-      puts("To list all of the songs by a particular artist, enter 'list artist'.")
-      puts("To list all of the genres in your library, enter 'list genres'.")
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the genres in your library, enter 'list genres'."
       puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of the songs of a particular genre, enter 'list genre'."
       puts "To play a song, enter 'play song'."
