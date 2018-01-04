b3e2c1da8793b5bea425bcf3a9acae504c5ec1bc
jamiegiuliano <jamiepgiuliano@gmail.com>
2018-01-04 23:49:10 +0000

    delete class methods in Song class that are now in Concerns::Findable module.

--- lib/song.rb
+++ lib/song.rb
@@ -39,19 +39,6 @@ class Song
         new_song
     end
     
-    #def self.find_by_name(song_name)
-    #   self.all.find { |song| song.name == song_name }    
-    #end
-    
-    #def self.find_or_create_by_name(song_name)
-    #    song = self.find_by_name(song_name)
-    #    if song
-    #        song
-    #    else 
-    #        self.create(song_name)
-    #    end
-    #end
-    
     def self.new_from_filename(filename)
         song_name = (filename.split(" - ")[1])
         artist_name = (filename.split(" - ")[0])
