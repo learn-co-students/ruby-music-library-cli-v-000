f14bc1f9c7f3396cadf0340f514033409aee5498
Eric Lin <esotericlin@gmail.com>
2018-01-04 01:30:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 class Song
+  extend Concerns::Findable
   attr_accessor :name
   attr_reader :artist, :genre
   @@all = []
@@ -19,13 +20,7 @@ class Song
     genre.songs << self unless genre.songs.include?(self)
   end
 
-  def self.find_by_name(name)
-    @@all.detect{ |song| song.name == name}
-  end
 
-  def self.find_or_create_by_name(name)
-    find_by_name(name) || create(name)
-  end
 
   def self.new_from_filename(filename)
     parts = filename.split(" - ")
