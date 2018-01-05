6503f1b63bf5870ff7f45662a6a1e3ae720ef19d
Jess Hedges <jesskhedges@icloud.com>
2018-01-05 01:29:50 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,43 @@
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    artist = new(name)
+    artist.save
+    artist
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    songs.collect{ |s| s.genre}.uniq
+  end
+
+  def add_song(song)
+    song.artist = self unless song.artist
+    songs << song unless songs.include?(song)
+  end
+
+end
---
+++ lib/concerns/Findable.rb
@@ -0,0 +1,13 @@
+module Concerns::Findable
+
+  attr_accessor :name
+
+  def find_by_name(name)
+    all.detect{ |s| s.name == name}
+  end
+
+  def find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+end
