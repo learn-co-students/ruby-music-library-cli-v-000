9f19a396497cddde13893495f452d08abd09ee5b
Yina Zhu <yinagit@gmail.com>
2018-01-06 03:51:02 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -7,9 +7,7 @@ class MusicLibraryController
 
 
   def call
-
     input = ""
-
     until input == "exit"
 
       puts "Welcome to your music library!"
@@ -63,15 +61,36 @@ class MusicLibraryController
   end
 
   def list_songs_by_artist
-      puts "Please enter the name of an artist:"
-      input = gets.strip
-        if input == Artist.find_by_name
-          input.songs.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |song, index|
-          puts "#{index}. #{song.name} - #{song.genre.name}"
-        end
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+    # artist = Artist.find_by_name(input)
+    # if artist
+    # is the same as:
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |song, index|
+        puts "#{index}. #{song.name} - #{song.genre.name}"
       end
+    end
   end
 
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+      if genre = Genre.find_by_name(input)
+        genre.songs.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |song, index|
+        puts "#{index}. #{song.artist.name} - #{song.name}"
+        end
+      end
+    end
+
+    def play_song
+      puts "Which song number would you like to play?"
+      input = gets.strip.to_i
+        if (1..Song.all.length).include?(input)
+          song = Song.all.sort{|a, b| a.name <=> b.name}[input - 1]
+          puts "Playing #{song.name} by #{song.artist.name}" if song
+        end
+      end
 
 # https://github.com/yinazee/ruby-music-library-cli-v-000/blob/solution/lib/music_library_controller.rb
-# end
+end
