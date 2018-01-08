623ef01df0ac5427c3198dff35d081dbaa1ecb03
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-01-08 15:36:49 +0000

    ☠ Refactor

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,13 +1,11 @@
 module Concerns::Findable
 #==============================================
   def find_by_name(name)
-    # self.all.find(&:name) ?find out why this didnt work?
     self.all.find {|o| name == o.name}
   end
 
   def find_or_create_by_name(name)
-    find = self.find_by_name(name)
-    find == nil ? self.create(name) : find
+    self.find_by_name(name) || self.create(name)
   end
 #==============================================
 end
