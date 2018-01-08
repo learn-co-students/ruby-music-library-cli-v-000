a759a733478306fc4d1e6232be032bb0581f14c7
Emily Zhao <tong.zhao.92@gmail.com>
2018-01-08 02:49:56 +0000

    Done.

--- lib/musiclib.rb
+++ lib/musiclib.rb
@@ -1,28 +1,84 @@
+#Song class
+module SongArtists
+  module InstanceMethods
+    def save
+      self.class.all<<self
+    end
+  end
+
+  module ClassMethods
+
+    def create(name)
+      new_instance = self.new(name)
+      new_instance.save
+      self.all[0]
+    end
+
+    def destroy_all
+      self.all.clear
+    end
+  end
+end
+
 class Song
   attr_accessor :name, :artist, :genre
+  include SongArtists::InstanceMethods
+  extend SongArtists::ClassMethods
 
   @@all = []
 
-  def initialize(name)
+  def initialize(name, genre = "", artist = self.artist)
     @name = name
+    @genre = genre
+    @artist = artist
   end
 
   def self.all
     @@all
   end
+end
 
-  def save
-    @@all<<self
+#artist class
+class Artist
+  attr_accessor :name, :songs
+  include SongArtists::InstanceMethods
+  extend SongArtists::ClassMethods
+
+  def initialize(name)
+    @name = name
+    @songs = []
   end
 
-  def self.create(name)
-    new_song = Song.new(name)
-    new_song.save
-    self.all
+  @@all = []
+
+  def self.all
+    @@all
   end
 
-  def self.destroy_all
-    @@all.clear
+  def add_song(song)
+    if song.artist == nil
+      song.artist = self
+    end
+    if !self.songs.include?(song)
+      self.songs << song
+    end
+  end
+end
+
+#Genre class
+class Genre
+  attr_accessor :name, :songs
+  include SongArtists::InstanceMethods
+  extend SongArtists::ClassMethods
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
   end
 
+  def self.all
+    @@all
+  end
 end
