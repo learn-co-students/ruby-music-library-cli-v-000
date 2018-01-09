865c93c14f198976bfdbe5a4913514ba7cabadbb
Remmy Clay <remmy.lauren@gmail.com>
2018-01-09 21:29:10 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -19,21 +19,20 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets
-      if input != "exit"
-        if input == "list songs"
-          list_songs
-        elsif input == "list artists"
-          list_artists
-        elsif input == "list genres"
-          list_genres
-        elsif input == "list artist"
-          list_songs_by_artist
-        elsif input == "list genre"
-          list_songs_by_genre
-        elsif input == "play song"
-          play_song
-        else
-          call
+      if input == "list songs"
+        list_songs
+      elsif input == "list artists"
+        list_artists
+      elsif input == "list genres"
+        list_genres
+      elsif input == "list artist"
+        list_songs_by_artist
+      elsif input == "list genre"
+        list_songs_by_genre
+      elsif input == "play song"
+        play_song
+      else if input != "exit"
+        call
       end
     end
   end
