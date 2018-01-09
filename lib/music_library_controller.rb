fa9e39762df5899098a734abed820be3deece1c1
Remmy Clay <remmy.lauren@gmail.com>
2018-01-09 21:36:11 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class MusicLibraryController
 
   def initialize(path = './db/mp3s')
@@ -9,31 +7,30 @@ class MusicLibraryController
   end
 
   def call
-      puts "Welcome to your music library!"
-      puts "To list all of your songs, enter 'list songs'."
-      puts "To list all of the artists in your library, enter 'list artists'."
-      puts "To list all of the genres in your library, enter 'list genres'."
-      puts "To list all of the songs by a particular artist, enter 'list artist'."
-      puts "To list all of the songs of a particular genre, enter 'list genre'."
-      puts "To play a song, enter 'play song'."
-      puts "To quit, type 'exit'."
-      puts "What would you like to do?"
-      input = gets
-      if input == "list songs"
-        list_songs
-      elsif input == "list artists"
-        list_artists
-      elsif input == "list genres"
-        list_genres
-      elsif input == "list artist"
-        list_songs_by_artist
-      elsif input == "list genre"
-        list_songs_by_genre
-      elsif input == "play song"
-        play_song
-      else if input != "exit"
-        call
-      end
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    input = gets
+    if input == "list songs"
+      list_songs
+    elsif input == "list artists"
+      list_artists
+    elsif input == "list genres"
+      list_genres
+    elsif input == "list artist"
+      list_songs_by_artist
+    elsif input == "list genre"
+      list_songs_by_genre
+    elsif input == "play song"
+      play_song
+    elsif input != "exit"
+      call
     end
   end
 
@@ -80,7 +77,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     song_choice = gets.to_i
-    if (song_choice > 0) && (song_choice <= Song.all.count)
+    if song_choice > 0 && song_choice <= Song.all.count
       list = Song.all.sort{|a, b| a.name <=> b.name}.collect do |song, index|
         "#{song.name} by #{song.artist.name}"
        end
