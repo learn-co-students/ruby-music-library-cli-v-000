3e5271f1d3b7bd1a0f3709ff991e67b56bac373d
Shangni Hu <shangnihu@gmail.com>
2018-01-10 20:16:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
 
+  extend Concerns::Findable
   attr_accessor :name, :artist, :genre
 
   @@all = []
@@ -40,16 +41,30 @@ class Song
     end
   end
 
-  def self.find_by_name(name)
-    this = nil
-    @@all.each do |song|
-      this = song unless song.name != name
-    end
-    this
+  def self.new_from_filename(filename)
+    artist, name, genre = filename.gsub(".mp3", "").split(" - ")
+    song = self.new(name)
+    song.artist = Artist.find_or_create_by_name(artist)
+    song.genre = Genre.find_or_create_by_name(genre)
+    song
   end
 
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+  def self.create_from_filename(filename)
+    song = new_from_filename(filename)
+    song.save
+    song
   end
+
+  # def self.find_by_name(name)
+  #   this = nil
+  #   @@all.each do |song|
+  #     this = song unless song.name != name
+  #   end
+  #   this
+  # end
+  #
+  # def self.find_or_create_by_name(name)
+  #   self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+  # end
 
 end
