1764d1e5dfb0cc1f8a58842c7d2002c8aa7a6058
Andy Maskiell <maskiella@gmail.com>
2018-01-11 01:19:56 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -43,6 +43,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create(name)
+    else
+      self.find_by_name(name)
+    end
   end
 
   def self.destroy_all
