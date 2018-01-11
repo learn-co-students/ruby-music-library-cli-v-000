1a9c5b2848af2da3c2cfca836a48ca8ae4cc8333
Andy Maskiell <maskiella@gmail.com>
2018-01-11 03:30:32 +0000

    Done.

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,2 +1,13 @@
 class MusicImporter
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.glob("#{path}/*.mp3").collect do |item|
+      item.gsub("#{path}/", "")
+    end
+  end
 end
