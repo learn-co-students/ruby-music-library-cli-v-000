f74bdf4b18b26bd60e9ef0c2c1eab471a5f616de
Brad Hambrice <hambrice@uab.edu>
2018-01-11 22:37:18 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -8,6 +8,7 @@ class MusicLibraryController
   end
   
   def call
+    binding.pry
   puts "Welcome to your music library!"
   puts "To list all of your songs, enter 'list songs'."
   puts "To list all of the artists in your library, enter 'list artists'."
