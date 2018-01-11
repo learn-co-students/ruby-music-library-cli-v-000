13d8fca62df68a962a047188f9efa412f8da67fc
Andy Maskiell <maskiella@gmail.com>
2018-01-11 20:45:53 +0000

    Done.

---
+++ lib/musiclibrarycontroller.rb
@@ -0,0 +1,57 @@
+class MusicLibraryController
+  attr_accessor :path
+
+  def initialize(path = "./db/mp3s")
+    @path = path
+    MusicImporter.new(path).import
+  end
+
+
+  # `each_with_index` and `sort`
+  def list_songs
+  end
+
+  def list_artists
+  end
+
+  def list_genres
+  end
+
+  def list_songs_by_artist
+  end
+
+  def list_songs_by_genre
+  end
+
+  def play_song
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    user_input = gets.chomp
+    if user_input == 'list songs'
+      self.list_songs
+    elsif user_input == 'list artists'
+      self.list_artists
+    elsif user_input == 'list genres'
+      self.list_genres
+    elsif user_input == 'list artist'
+      self.list_songs_by_artist
+    elsif user_input == 'list genre'
+      self.list_songs_by_genre
+    elsif user_input == 'play song'
+      self.play_song
+    elsif user_input == 'exit'
+    else
+      call
+    end
+  end
+end
