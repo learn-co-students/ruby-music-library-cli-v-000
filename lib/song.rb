9e77ca126ea42762c9df36f6d06f3681b734d64c
funemployeddad <funemployeddad@gmail.com>
2018-01-12 23:03:34 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -37,4 +37,28 @@ class Song
       genre.add_song(self)
     end
 
+    def self.new_from_filename(filename)
+      file_data = filename.split(" - ")
+      song = Song.new(file_data[1])
+      song.artist_name = file_data[0]
+      song.genre_name = file_data[2].gsub(".mp3","")
+      song
+    end
+
+    def artist_name=(name)
+      artist = Artist.find_or_create_by_name(name)
+      self.artist = artist
+      artist.add_song(self)
+    end
+
+    def genre_name=(name)
+      genre = Genre.find_or_create_by_name(name)
+      self.genre = genre
+    end
+
+    def self.create_from_filename(filename)
+      song = self.new_from_filename(filename)
+      song.save
+      song
+    end
 end
