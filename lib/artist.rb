47c5b5f8a23090bbbc7d85c67193c0627f74e93d
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-13 16:18:32 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Artist
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+
+end
