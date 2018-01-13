1fba7e9d6bc3fc905a1d1854ba6572a16c4b1c27
github username <github email address>
2018-01-13 09:13:35 -0700

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class MusicLibraryController
   attr_accessor :path
 
@@ -7,25 +8,34 @@ class MusicLibraryController
   end
 
   def call
-    puts "To list all of your songs, enter 'list songs'."
+    # until user enters "exit" do this code
+        puts "Welcome to your music library!"
 
-    puts "To list all of the artists in your library, enter 'list artists'."
+        puts "To list all of your songs, enter 'list songs'."
 
-    puts "To list all of the genres in your library, enter 'list genres'."
+        puts "To list all of the artists in your library, enter 'list artists'."
 
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
+        puts "To list all of the genres in your library, enter 'list genres'."
 
-    puts "Welcome to your music library!"
+        puts "To list all of the songs by a particular artist, enter 'list artist'."
 
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
+        puts "Welcome to your music library!"
 
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
+        puts "To list all of the songs by a particular artist, enter 'list artist'."
 
-    puts "To play a song, enter 'play song'."
+        puts "To list all of the songs of a particular genre, enter 'list genre'."
 
-    puts "To quit, type 'exit'."
+        puts "To play a song, enter 'play song'."
 
-    puts "What would you like to do?"
+        puts "To quit, type 'exit'."
+
+        puts "What would you like to do?"
+
+
+        input = gets.strip
+        until input == "exit" do
+
+        end
 
   end
 
