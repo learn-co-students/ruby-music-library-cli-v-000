59c875ae02f4d84c7d8882c8e780f852d5b7d56e
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-13 16:37:25 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Genre
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+    genre
+  end
+
+end
