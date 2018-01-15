fc90ed979b826f2414d2ffff960ea10f16a6718a
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-15 20:24:32 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -42,14 +42,40 @@ class MusicLibraryController
   end
 
   def list_songs
-     list = Song.all.sort {|a, b| a.name <=> b.name}
-     list.map.with_index do |song, index|
-       puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-     end
+    songs_alpha = Song.all.sort {|a, b| a.name <=> b.name}
+    songs_alpha.map.with_index do |song, index|
+      puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
   end
 
   def list_artists
+    artists_alpha = Artist.all.sort {|a, b| a.name <=> b.name}
+    artists_alpha.map.with_index do |artist, index|
+       puts "#{index + 1}. #{artist.name}"
+    end
+  end
 
+  def list_genres
+    genre_alpha = Genre.all.sort {|a, b| a.name <=> b.name}
+    genre_alpha.map.with_index do |genre, index|
+      puts "#{index + 1}. #{genre.name}"
+    end
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+    Artist.all.each do |artist|
+      if artist.name == input
+        songs_alpha = artist.songs.sort {|a, b| a.name <=> b.name}
+        songs_alpha.map.with_index {|song, index| puts "#{index + 1}. #{song.name} - #{song.genre.name}"}
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
   end
 
 end
