04e295deff92b2061c947c2f328542a4a358c192
Allyson Wesman <allysonwesman@gmail.com>
2018-01-16 21:33:41 -0800

    create files to pass all tests

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,27 @@ require 'bundler'
 Bundler.require
 
 module Concerns
-end
+  # Class Methods
+  module Findable
+    def find_by_name(name)
+      self.all.detect {|item| item.name == name}
+    end
+
+    def find_or_create_by_name(name)
+      result = self.find_by_name(name)
+      result != nil ? result : self.create(name)
+    end
+  end
+
+  module Printable
+    def print_alphabetized_by_name
+      sorted = self.all.sort_by! {|item| item.name}
+      sorted.each do |item|
+        puts "#{sorted.index(item)+1}. #{item.name}"
+      end
+    end
+  end #end Printable
+
+end #end Concerns
 
 require_all 'lib'
