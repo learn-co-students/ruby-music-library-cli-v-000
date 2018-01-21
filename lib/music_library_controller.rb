0770dc1b36c28944f5f4031b41897a59a596b0b3
Ariana Berger <ariana.berger@gmail.com>
2018-01-21 00:39:18 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,7 +9,7 @@ class MusicLibraryController
   attr_reader :path
 
   def initialize (path = "./db/mp3s")
-    @path = path #do I need this here?
+    @path = path
     object = MusicImporter.new(path).import
   end
 
@@ -28,8 +28,34 @@ class MusicLibraryController
       puts "What would you like to do?"
 
       user_input = gets.strip
-      
+
     end
   end
 
+  def list_songs
+    self.path
+  end
+
+
 end
+
+#   /////FIRST ATTEMPT
+  # def call
+  #   puts "Welcome to your music library!"
+  #   puts "To list all of your songs, enter 'list songs'."
+  #   puts "To list all of the artists in your library, enter 'list artists'."
+  #   puts "To list all of the genres in your library, enter 'list genres'."
+  #   puts "To list all of the songs by a particular artist, enter 'list artist'."
+  #   puts "To list all of the songs of a particular genre, enter 'list genre'."
+  #   puts "To play a song, enter 'play song'."
+  #   puts "To quit, type 'exit'."
+  #   puts "What would you like to do?"
+  #
+  #
+  #   user_input = gets.chomp
+  #
+  #     until user_input == "exit"
+  #       puts "What would you like to do?"
+  #       user_input
+  #     end
+  #  end
