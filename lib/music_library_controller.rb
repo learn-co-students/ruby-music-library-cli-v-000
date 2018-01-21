ad0a4df2ee4e15a9fc0314e9e4dad38c9160543d
Ariana Berger <ariana.berger@gmail.com>
2018-01-21 00:29:16 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,29 +9,27 @@ class MusicLibraryController
   attr_reader :path
 
   def initialize (path = "./db/mp3s")
-    @path = path
-    object = MusicImporter.new(path)
-    object.import
+    @path = path #do I need this here?
+    object = MusicImporter.new(path).import
   end
 
   def call
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
+    user_input = ""
 
+    while user_input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
 
-    user_input = gets.chomp
-binding.pry
-      until user_input == "exit"
-        user_input
-        break
-      end
+      user_input = gets.strip
+      
+    end
   end
 
 end
