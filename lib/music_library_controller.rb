797a07aa49bc6e509f2b8ac8f8658a35aa317434
Ariana Berger <ariana.berger@gmail.com>
2018-01-24 17:45:51 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,8 +9,8 @@ class MusicLibraryController
   attr_reader :path
 
   def initialize (path = "./db/mp3s")
-    @path = path
-    object = MusicImporter.new(path).import
+    object = MusicImporter.new(path)
+    object.import
   end
 
   def call
@@ -33,29 +33,10 @@ class MusicLibraryController
   end
 
   def list_songs
-    self.path
+    Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |s, i|
+      puts "#{i} - #{s.artist.name} - #{s.artist.song} - #{s.artist.genre}"
+    binding.pry
+  end
   end
 
-
 end
-
-#   /////FIRST ATTEMPT
-  # def call
-  #   puts "Welcome to your music library!"
-  #   puts "To list all of your songs, enter 'list songs'."
-  #   puts "To list all of the artists in your library, enter 'list artists'."
-  #   puts "To list all of the genres in your library, enter 'list genres'."
-  #   puts "To list all of the songs by a particular artist, enter 'list artist'."
-  #   puts "To list all of the songs of a particular genre, enter 'list genre'."
-  #   puts "To play a song, enter 'play song'."
-  #   puts "To quit, type 'exit'."
-  #   puts "What would you like to do?"
-  #
-  #
-  #   user_input = gets.chomp
-  #
-  #     until user_input == "exit"
-  #       puts "What would you like to do?"
-  #       user_input
-  #     end
-  #  end
