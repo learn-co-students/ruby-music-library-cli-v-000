c8e9571d3aa5474cb54cfd41e5cf31fb6382d629
Ariana Berger <ariana.berger@gmail.com>
2018-01-25 01:59:16 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,12 +1,10 @@
 module Concerns::Findable
 
   def find_by_name(name)
-    binding.pry
     all.detect {|song| song.name == name}
   end
 
   def find_or_create_by_name(name)
-    binding.pry
     find_by_name(name) || create(name)
   end
 
--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,4 +41,54 @@ class MusicLibraryController
     end
   end
 
+  def list_artists
+    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
+      puts "#{i}. #{a.name}"
+    end
+    # need artists + unique
+    # then alphabetize and puts with number
+  end
+
+  def list_genres
+    Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
+      puts "#{i}. #{g.name}"
+    end
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    user_input = gets.strip
+
+    if artist = Artist.find_by_name(user_input) #why does there need to be artist = here? How does this if statement work?
+      artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.name} - #{s.genre.name}"
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    user_input = gets.strip
+
+    if genre = Genre.find_by_name(user_input)
+      genre.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.artist.name} - #{s.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    user_input = gets.strip.to_i
+    # if Song.all.count includes the user_input number then...
+      a = Song.all[user_input -1].artist.name
+      s = Song.all[user_input -1].name
+      puts "Playing #{s} by #{a}"
+      binding.pry
+    list_songs[user_input]
+
+    # check what song matches the input from list_songs method based on what number it is
+    # Puts playing [song] by [artist]
+  end
+
 end
