eb52cdc272fe5c51632149ec7ee1a0cde5e94e30
Ryne Stajcar <rynostajcar@gmail.com>
2018-02-03 02:07:58 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -26,11 +26,47 @@ class MusicLibraryController
   end
 
   def list_songs
-    sorted_songs = []
     Song.all.sort_by! {|song| song.name}.each_with_index do |song, position|
         puts "#{position+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
       end
     end
 
+  def list_artists
+    Artist.all.sort_by! {|artist| artist.name}.each_with_index do |artist, position|
+      puts "#{position+1}. #{artist.name}"
+    end
+  end
+
+  def list_genres
+    Genre.all.sort_by! {|genre| genre.name}.each_with_index do |genre, position|
+      puts "#{position+1}. #{genre.name}"
+    end
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+      input = gets.strip
+      if artist = Artist.find_by_name(input)
+        artist.songs.sort_by! {|song| song.name}.each_with_index do |s, position|
+          puts "#{position+1}. #{s.name} - #{s.genre.name}"
+        end
+      end
+    end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort_by! {|song| song.name}.each_with_index do |s, position|
+        puts "#{position+1}. #{s.artist.name} - #{s.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip_to_i
+
+  end
 
 end
