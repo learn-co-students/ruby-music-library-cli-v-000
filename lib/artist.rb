4930fb67bbd665804816eeb6c95643330f9e89e0
Katrina Brinson <kbrinso@gmail.com>
2018-02-06 00:07:45 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,30 @@ class Artist
   extend MusicModule::ClassMethods
   include MusicModule::InstanceMethods
   
+  attr_accessor :destroy_all, :create, :name
+  
   @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+      @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
 
 end
--- lib/genre.rb
+++ lib/genre.rb
@@ -3,5 +3,31 @@ require 'music_module.rb'
 class Genre
   extend MusicModule::ClassMethods
   include MusicModule::InstanceMethods
+  
+   attr_accessor :destroy_all, :create, :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  @@all = []
+
+  def self.all
+      @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+    genre
+  end
 
 end
