36838eb3538e18a461f064c04439c7ed71e49816
Katrina Brinson <kbrinso@gmail.com>
2018-02-06 18:51:53 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -21,8 +21,10 @@ class MusicLibraryController
   end
   
   def list_songs
+    Song.all.sort_by{|song| song.name}
     Song.all.each_with_index{|song,i| 
-      puts "#{i + 1}. #{song.name} - #{song.artist.name} - #{song.genre.name}"
-    }
+        puts "#{i + 1}. #{song.name} - #{song.artist.name} - #{song.genre.name}"
+      }
+    #need to sort by the song name
   end
 end
