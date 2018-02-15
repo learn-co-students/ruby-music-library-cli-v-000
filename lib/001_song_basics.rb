844d4645d6dd304c356faf809db278f3a05e731e
Barbara Choy <barbchoy@gmail.com>
2018-02-15 19:42:11 +0000

    Done.

---
+++ lib/001_song_basics.rb
@@ -0,0 +1,28 @@
+class Song
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = self.new(name)
+    song.save
+  end
+
+end
