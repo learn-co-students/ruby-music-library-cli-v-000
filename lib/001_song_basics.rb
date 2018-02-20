5924831b8f3fd7120fc14bb55dcf9f20236fa8a2
Barbara Choy <barbchoy@gmail.com>
2018-02-20 16:42:35 +0000

    Done.

--- lib/001_song_basics.rb
+++ lib/001_song_basics.rb
@@ -29,6 +29,10 @@ class Song
     @@all.find { |song| song.name == name }
   end
 
+  def self.find_or_create_by_name(name)
+     self.find_by_name(name)? self.find_by_name(name): self.create(name)
+  end
+
   def artist=(artist)
     @artist = artist
     artist.add_song(self)
