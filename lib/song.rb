8f597d141c72adbac2ce565259767ae99afad60d
github username <github email address>
2018-02-20 14:39:57 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,6 @@
 class Song
+  extend Concerns::Findable
+
   attr_accessor :name
   attr_reader :artist, :genre
 
@@ -27,13 +29,29 @@ class Song
   end
 
   def artist=(artist)
+    #binding.pry
     @artist = artist
-    artist.add_song(self)
+    self.artist.add_song(self)
   end
 
   def genre=(genre)
     @genre = genre
     genre.songs << self if !genre.songs.include?(self)
+  end
+
+  def self.new_from_filename(file_name)
+    artistName = file_name.split(" - ")[0]
+    songName = file_name.split(" - ")[1]
+    genreName = file_name.split(" - ")[2].chomp(".mp3")
+    artist = Artist.find_or_create_by_name(artistName)
+    genre = Genre.find_or_create_by_name(genreName)
+    song = self.new(songName, artist, genre)
+  end
+
+  def self.create_from_filename(file_name)
+    song = self.new_from_filename(file_name)
+    song.save
+    song
   end
 
 end
