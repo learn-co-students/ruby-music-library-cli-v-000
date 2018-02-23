85284bc8b85dccdbd2c53a335de0b6454c550fcb
Barbara Choy <barbchoy@gmail.com>
2018-02-23 22:16:32 +0000

    Done.

--- lib/005_music_library_controller.rb
+++ lib/005_music_library_controller.rb
@@ -1,8 +1,10 @@
 class MusicLibraryController
 
+  attr_reader :songs_names
+
   def initialize(path = "./db/mp3s")
     new_music_importer = MusicImporter.new(path)
-    new_music_importer.import
+    @songs_names = new_music_importer.import
   end
 
   def call
@@ -23,4 +25,29 @@ class MusicLibraryController
     end
   end
 
+  def list_songs
+    name_order_hash = {}
+    idx = 1
+    @songs_names.each do |song_name|
+      name_order_hash[song_name.split(" - ")[1]] = song_name.split(".")[0]
+    end
+    songs_sorted = name_order_hash.sort_by { |song_title, _full_title| song_title }
+    songs_sorted.to_h.each_value do |each_song|
+      puts "#{idx}. #{each_song}"
+      idx += 1
+    end
+  end
+
+  def list_artists
+    artists_list = []
+    idx = 1
+    @songs_names.each do |song_name|
+      artists_list << song_name.split(" - ")[0]
+    end
+    artists_list.uniq!.sort!
+    artists_list.each do |each_artist|
+      puts "#{idx}. #{each_artist}"
+      idx += 1
+    end
+  end
 end
