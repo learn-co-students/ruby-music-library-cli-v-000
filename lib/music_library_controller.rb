b6e3df5baa2a25738d6c53c38154464cc96e6fba
Jay Bednar <ironviralmedia@gmail.com>
2018-02-24 01:44:10 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,7 +7,11 @@ class MusicLibraryController
 
 	def call
 
-	  puts "Welcome to your music library!"
+    input = ""
+
+    while input != 'exit'
+
+	    puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of the genres in your library, enter 'list genres'."
@@ -18,27 +22,25 @@ class MusicLibraryController
       puts "What would you like to do?"
 
 
-      input = gets.chomp
+      input = gets.strip
 
-      if input == 'list songs'
-      	self.list_songs
-      elsif input == 'list artists'
-      	list_artists
-      elsif input == 'list genres'
-      	list_genres
-      elsif input == 'list artist'
-      	list_songs_by_artist
-      elsif input == 'list genre'
-      	list_songs_by_genre
-      elsif input == 'play song'
-      	play_song
-      elsif input == 'exit'
-      	return "Goodbye!"
-      else
-      	self.call
+      case input
+        when 'list songs'
+      	  list_songs
+        when 'list artists'
+      	  list_artists
+        when 'list genres'
+      	  list_genres
+        when 'list artist'
+      	  list_songs_by_artist
+        when 'list genre'
+      	  list_songs_by_genre
+        when 'play song'
+      	  play_song
       end
+   end
 
-    end
+  end
 
 
     def list_songs
@@ -62,7 +64,7 @@ class MusicLibraryController
     def list_songs_by_artist
     	puts "Please enter the name of an artist:"
 
-    	artist_name = gets.chomp
+    	artist_name = gets.strip
     	if Artist.find_by_name(artist_name)
     		Artist.find_by_name(artist_name).songs.sort{|song_x, song_y| song_x.name <=> song_y.name}.each_with_index do |song, i|
     			puts "#{i + 1}. #{song.name} - #{song.genre.name}"
@@ -86,7 +88,7 @@ class MusicLibraryController
 	def play_song
 		puts "Which song number would you like to play?"
 
-		song_num = gets.chomp.to_i
+		song_num = gets.strip.to_i
 
 		if song_num > 0 && song_num <= Song.all.length
 			song = Song.all.sort{|song_x, song_y| song_x.name <=> song_y.name}[song_num - 1]
