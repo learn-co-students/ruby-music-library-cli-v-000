eb54e8ef7e2a415bec6891f7f81090623045207e
Neil Seejoor <nvseejoor@gmail.com>
2018-02-28 23:40:15 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,4 +7,22 @@ class Artist
     @name = name
   end
 
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+      song = new(name)
+      song.save
+      song
+  end
+
 end
--- lib/genre.rb
+++ lib/genre.rb
@@ -7,4 +7,22 @@ class Genre
     @name = name
   end
 
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+      song = new(name)
+      song.save
+      song
+  end
+
 end
