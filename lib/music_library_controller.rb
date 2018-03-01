53f524613747e22340b71edc3670db65734062e8
Monica Tamaru Jones <mntamaru@gmail.com>
2018-03-01 18:35:39 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -25,6 +25,21 @@ class MusicLibraryController
     input = gets.strip
     
     while input != "exit"
+      case input 
+        when 'list songs'
+          list_songs
+        when 'list artists'
+          list_artists
+        when 'list genres'
+          list_genres
+        when 'list artist'
+          list_songs_by_artist
+        when 'list genre'
+          list_songs_by_genre
+        when 'play song'
+          list_songs
+          play_song
+      end
       input = gets.strip
     end
   end
