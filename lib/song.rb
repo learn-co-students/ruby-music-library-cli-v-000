d1c3bf0c8da3cc725eb602e4b4a8fd96a7758f05
Ron Sala <ron.sala@gmail.com>
2018-03-02 07:26:41 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,27 @@
 class Song
 
+  attr_accessor :name
+
+  @@all = []
+
   def initialize(name)
     @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    @@all << self
   end
 
 end
