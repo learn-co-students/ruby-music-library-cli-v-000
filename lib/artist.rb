40bf752ff9040b1af9608f5459bbb6440864e1f3
Ron Sala <ron.sala@gmail.com>
2018-03-04 09:52:42 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new(name).tap{|a| a.save}
+  end
+
+end
