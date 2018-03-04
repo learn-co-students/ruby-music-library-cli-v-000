77d44a294164e7fb7e43247c9fcece239f43e0a6
Ron Sala <ron.sala@gmail.com>
2018-03-04 12:47:14 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Artist
-  # extend Concerns::Findable
+  extend Concerns::Findable
   attr_accessor :name, :songs
   @@all = []
 
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Genre
-  # extend Concerns::Findable
+  extend Concerns::Findable
   attr_accessor :name, :songs
 
   @@all = []
