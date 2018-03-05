c88172cd071867ed23d5616f6b4489e455d64847
Zac Willmington <zac.willmington@gmail.com>
2018-03-05 23:33:29 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,11 +1,17 @@
 require 'pry'
+require 'pathname'
 
 class MusicImporter
 
-    attr_accessor
+    attr_accessor :path
 
     def initialize(file_path)
+        @path = file_path
+    end
 
+    def files
+         dir_var = Dir.entries(@path).select {|f| !File.directory? f}
+         #binding.pry
     end
 
 end
