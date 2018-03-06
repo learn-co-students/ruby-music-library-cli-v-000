a98f572d9df89ddbbb22986328ec96e5ae84a03c
Monica Tamaru Jones <mntamaru@gmail.com>
2018-03-06 19:10:32 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,6 +41,22 @@ class MusicLibraryController
           play_song
       end
       input = gets.strip
+      
+      case input 
+        when 'list songs'
+          list_songs
+        when 'list artists'
+          list_artists
+        when 'list genres'
+          list_genres
+        when 'list artist'
+          list_songs_by_artist
+        when 'list genre'
+          list_songs_by_genre
+        when 'play song'
+          list_songs
+          play_song
+      end
     end
   end
   
