1444819428a8e3bd7b44a20724c1d5f0cb87d969
Nathan  Visak <nathan.visak@gmail.com>
2018-03-07 23:30:45 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,30 @@
+require 'pry'
+
 class Artist
     attr_accessor :name
     @@all = []
 
     def initialize(name)
       @name = name
+      @songs = []
     end
+
+    def songs
+      @songs
+    end
+
+    def add_song(song)
+      if song.artist != self
+        song.artist = self
+      end
+      if @songs.include?(song) == false
+        @songs << song
+      end
+      # song.artist = self unless song.artist == self
+      # @songs << song unless @songs.include?(self)
+    end
+
+
 
     def self.all
       @@all
--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 class Song
-  attr_accessor :name
+  attr_accessor :name, :artist
   @@all = []
 
-  def initialize(name)
+  def initialize(name, artist = nil)
     @name = name
+    @artist = artist
   end
 
   def self.all
