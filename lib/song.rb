f9306ff6d8aff638a753ff4e918e1ffcfe49092d
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-21 03:34:38 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
 
   def initialize(name, artist=nil)
     @name = name
-    self.artist = artist
+    @artist = artist
     self.save
   end
 
--- notes.md
+++ notes.md
@@ -1,1 +1,3 @@
 rspec spec/004_songs_and_artists_spec.rb --f-f
+
+ruby lib/song.rb
