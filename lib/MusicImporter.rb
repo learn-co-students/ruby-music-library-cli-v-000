c6ed8f35102ae24db71788e4038fd2e002bd9dae
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-23 17:28:59 +0000

    Done.

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -3,8 +3,15 @@ class MusicImporter
 
   def initialize(path)
     @path = path
-
   end
+
+  def files
+    Dir["#{path}/*.mp3"].collect{|path| path.split('/').last}
+  end
+
+  # def import
+  #   files.each{ |filename| Song.new_by_filename(filename) }
+  # end
 
 
 end
