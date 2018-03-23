cf42160596339687ca9902b8a4b6f634ad792c37
Dee Race <hey@deeraceart.com>
2018-03-23 19:39:58 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,34 @@
+class Genre
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = new(name)
+    genre.save
+    genre
+  end
+
+  def artists
+    songs.map(&:artist).uniq
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include? self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_by_name(name)
+    all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+  end
+
+
+end
