69c6160f0da5d8eb09ccb5678be29421fa499d47
Maverick Tesini <mavericktesini@gmail.com>
2018-03-28 21:15:33 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -21,21 +21,21 @@ class MusicLibraryController
     while input != "exit"
       input = gets.strip
 
-    case input
-    when "list songs"
-      list_songs
-    when "list artists"
-      list_artists
-    when "list genres"
-      list_genres
-    when "list artist"
-      list_songs_by_artist
-    when "list genre"
-      list_songs_by_genre
-    when "play song"
-      play_song
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
     end
-  end
   end
 
   def list_songs #why the error?
