385ed746aeea82fe71596378d78660ae9339bbab
Daiane Bushey <daianebushey@gmail.com>
2018-04-04 15:47:32 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,20 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self unless song.artist
+  end
+
+  def songs
+    @songs
   end
 
   def save
