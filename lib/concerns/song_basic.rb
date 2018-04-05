3df3c6b65d9ed8e9382e33d95e7759a19d7bae29
Vanessa Toro <vanetoro84@gmail.com>
2018-04-05 19:36:11 +0000

    Done.

---
+++ lib/concerns/song_basic.rb
@@ -0,0 +1,28 @@
+class Song
+  attr_accessor :name
+ 
+  @@all= []
+  
+  def initialize(name)
+    @name = name
+    @@all = []
+  end  
+  
+  def self.all
+    @@all
+  end  
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end  
+  
+  def self.create
+    song = self.new
+  
+  end
+  
+end
