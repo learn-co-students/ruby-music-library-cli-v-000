845219a2f0d7b02706a2dc17397343ff33321cd9
Vanessa Toro <vanetoro84@gmail.com>
2018-04-07 16:43:21 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,6 +7,9 @@ class MusicLibraryController
   end
   
   def call
+    answer = nil
+    while answer != 'exit'
+      answer = gets
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -16,10 +19,23 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    answer = nil
-    while answer != 'exit'
-      answer = gets
+    
+    case answer
+       when 'list songs'
+         self.list_songs
+       when 'list artists'
+         self.list_artists
+       when 'list genres'   
+         self.list_genres
+       when 'list artist'
+         self.list_songs_by_artist
+       when 'list genre'
+         self.list_songs_by_genre
+      when 'play song'
+          self.play_song
     end  
+     answer = gets 
+    end   
   end
   
   def list_songs
@@ -66,15 +82,14 @@ class MusicLibraryController
     
     def play_song
       puts "Which song number would you like to play?"
-      self.list_songs
       answer = gets
-      # binding.pry
-      if self.list_songs.find{|song| song == answer} != nil
-        binding.pry
-       puts answer
-     else
-       answer = gets
-      end
+      song_array = Song.all.sort_by { |song| song.name }
+      if  answer.to_i - 1  > -1 && song_array[answer.to_i - 1] != nil
+        song = song_array[answer.to_i - 1]
+        puts "Playing #{song.name} by #{song.artist.name}"
+      end  
     end
+    
+  
 end
 
