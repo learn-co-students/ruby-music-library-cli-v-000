7a61548d0ee63e1de1d6eaa7283e0507b4a52bd6
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-09 18:16:43 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,3 +1,4 @@
+require "pry"
 class MusicLibraryController
   
   attr_accessor :path
@@ -27,6 +28,70 @@ def call
    puts "To quit, type 'exit'."
     puts "What would you like to do?"
 end
+end
+
+def list_songs
+    array = []
+    array_two = []
+    Song.all.map do |song|
+    array << song.name
+    end
+    sorted = array.sort do |a, b|
+      a <=> b 
+    end 
+    sorted.map do |name|
+     array_two << Song.find_by_name(name)
+  end
+  counter = 1
+  array_two.map do |song_list|
+   puts "#{counter}. #{song_list.artist.name} - #{song_list.name} - #{song_list.genre.name}"
+   counter += 1
+  end
+end
+ 
+def list_artists
+ array = []
+    array_two = []
+    Artist.all.map do |artist|
+    array << artist.name
+    end
+    sorted = array.sort do |a, b|
+      a <=> b 
+    end 
+    sorted.map do |name|
+     array_two << Artist.find_by_name(name)
+  end
+  counter = 1
+  array_two.map do |artist_list|
+   puts "#{counter}. #{artist_list.name}"
+   counter += 1
+  end
+end
+  
+  def list_genres
+     array = []
+    array_two = []
+    Genre.all.map do |genre|
+    array << genre.name
+    end
+    sorted = array.sort do |a, b|
+      a <=> b 
+    end 
+    sorted.map do |name|
+     array_two << Genre.find_by_name(name)
+  end
+  counter = 1
+  array_two.map do |genre_list|
+   puts "#{counter}. #{genre_list.name}"
+   counter += 1
+  end
+end
+
+def list_songs_by_artist
+puts "Please enter the name of an artist:"
+retrieve = gets.chomp
+
+
 end
 
 end
