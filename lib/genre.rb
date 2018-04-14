345d16356de625435234d872a7244603f5966d1e
Eric Johnson <ericjohnson0216@icloud.com>
2018-04-14 15:50:26 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,37 @@
+class Genre
+  extend Concerns::Findable
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    genre = new(name)
+    genre.save
+    genre
+  end
+
+  def artists
+    songs.collect{ |s| s.artist }.uniq
+  end
+
+end
