bb4a256fabf9e5dfc872e8c0742a0a7e88f2e184
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-04-15 00:36:48 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -50,4 +50,14 @@ class Song
     self.find_by_name(name) || self.create(name)
   end
 
+  def self.new_from_filename(filename)
+    split_name = filename.split(" - ")
+    song = self.new(split_name[1])
+    artist = Artist.find_or_create_by_name(split_name[0])
+    song.artist = artist
+    artist.add_song(song)
+    song
+
+  end
+
 end
