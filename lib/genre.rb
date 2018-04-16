93f98cb14c59f59fdc7af2c4475254cc9cdb044a
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-04-16 02:16:17 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,34 @@
+class Genre
+  extend Concerns::Findable
+  attr_accessor :name
+
+
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,21 @@
 class Song
-  attr_accrssor :name
+  attr_accessor :name
+  attr_reader :artist, :genre
+
   @@all = []
-  def initialize (name)
+
+  def initialize(name)
     @name = name
-    save
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
   end
 
   def self.all
@@ -11,10 +23,24 @@ class Song
   end
 
   def self.destroy_all
-    self.class.all.clear
+    all.clear
   end
 
-  def self.save
-    self.class.all <<
+  def save
+    self.class.all << self
+  end
+
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+
+    new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{ |s| s.save }
   end
 end
