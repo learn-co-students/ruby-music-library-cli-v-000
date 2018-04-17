c9fc2876b34cb8983dd8a0e63b9313c75bfafafa
Katie Cassell <kcassell944@gmail.com>
2018-04-17 21:29:38 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,6 +18,42 @@ def initialize(path = './db/mp3s')
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets.strip
+      case input
+      when 'list songs'
+        list_songs
+      when 'list artists'
+        list_artists
+      when 'list genres'
+        list_genres
+      when 'list artist'
+        list_songs_by_artist
+      when 'list genre'
+        list_songs_by_genre
+      when 'play song'
+        play_song
+        end
+      end
     end
+
+
+  def list_songs
+    Song.all.sort do |a|
+      a.name
+    end
+  end
+
+  def list_artists
+  end
+
+  def list_genres
+  end
+
+  def list_songs_by_artist
+  end
+
+  def list_songs_by_genre
+  end
+
+  def play_song
   end
 end
