dc3af8d0a9f2eb132774978dc60d308af43274bf
Chris Dundon <chris.dundon@gmail.com>
2018-04-18 18:56:31 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -4,4 +4,10 @@ class MusicImporter
   def initialize(path)
     self.path = path
   end
+
+  def files
+    Dir.glob(self.path.strip + '/*.mp3') do |rb_file|
+      # do work on files ending in .rb in the desired directory
+    end
+  end
 end
