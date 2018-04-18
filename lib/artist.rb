3491de721fbfcceb47e627235d82e2b298654ca8
Chris Dundon <chris.dundon@gmail.com>
2018-04-18 16:20:00 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,31 +3,31 @@ require_relative "./music_module.rb"
 class Artist
   extend Music::C_Methods
   include Music::I_Methods
-  
+
   attr_reader :songs
-  
+
   @@all = []
-  
+
   def self.all
     @@all
   end
-  
+
   def self.destroy_all
       self.all.clear
   end
-  
-  def initialize(name)
-    super(name)
-    @songs = []
-  end
-  
-  def add_song(song)
+
+  def self.add_song(song)
     @songs << song
-    
+
     if songs.include?(song)
       return nil
     end
-    
+
     song.artist = self if !!!song.artist
+  end
+
+  def initialize(name)
+    super(name)
+    @songs = []
   end
 end
