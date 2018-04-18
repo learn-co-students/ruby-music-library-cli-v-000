4baf28cd83a9bbbbc19497aaf879d13253edb64b
cam <camsay@gmail.com>
2018-04-18 02:05:14 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,73 @@
+class Song
+
+  extend Concerns::Findable
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|n| n.name == name}
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+
+
+  def artist=(artist)
+   @artist = artist
+   artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self)
+  end
+
+  def self.new_from_filename(filename)
+      filename_arr = filename.split(" - ")
+      artist_name = filename_arr[0]
+      song_name = filename_arr[1]
+      genre_name = filename_arr[2].split(".")[0]
+
+      song = self.new(song_name)
+      artist = Artist.find_or_create_by_name(artist_name)
+      artist.add_song(song)
+      genre = Genre.find_or_create_by_name(genre_name)
+      genre.add_song(song)
+      song
+    end
+
+    def self.create_from_filename(filename)
+      song = self.new_from_filename(filename)
+      song.save
+      song
+  end
+
+
+
+end
