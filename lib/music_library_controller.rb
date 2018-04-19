389c86f64ac7edfc3d8590837515a46a4baefccc
Chris Dundon <chris.dundon@gmail.com>
2018-04-19 21:37:47 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,7 +9,6 @@ class MusicLibraryController
 
   def call
     puts "Welcome to your music library!"
-    puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
     puts "To list all of the genres in your library, enter 'list genres'."
