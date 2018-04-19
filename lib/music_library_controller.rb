02d41609bfff261afca0bdb2d93bf992dbdac2ae
Chris Dundon <chris.dundon@gmail.com>
2018-04-19 21:07:38 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,6 +1,16 @@
 
-class MusicLibraryController < MusicImporter
+class MusicLibraryController
+  #attr_reader :music_importer
+
   def initialize(path_ = "./db/mp3s") # = "./db/mp3s")
-    super(path_)
+    self.music_importer = MusicImporter.new(path_)
   end
+
+  def call
+    #Do something
+  end
+
+  private
+  attr_accessor :music_importer
+
 end
