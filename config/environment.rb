4e75d5406969f06316c48e3111b7cfd5091faff3
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-20 01:44:15 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,89 @@ module Concerns
 end
 
 require_all 'lib'
+
+
+class Song
+attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+ def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    name = self.new(name)
+    name.save
+    name
+  end
+
+end
+
+class Artist
+attr_accessor :name
+
+
+  def initialize(name)
+    @name = name
+    @@all = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+     @@all = []
+   end
+
+   def save
+     @@all << self
+   end
+
+   def self.create(name)
+     name = self.new(name)
+     name.save
+     name
+   end
+
+end
+
+class Genre
+attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+     @@all = []
+   end
+
+   def save
+     @@all << self
+   end
+
+   def self.create(name)
+     name = self.new(name)
+     name.save
+     name
+   end
+
+end
