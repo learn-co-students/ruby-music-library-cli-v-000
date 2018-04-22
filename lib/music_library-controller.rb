a711140f0cfde721c475859516a759c78f4283c6
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-04-22 04:37:46 +0000

    Done.

--- lib/music_library-controller.rb
+++ lib/music_library-controller.rb
@@ -7,17 +7,19 @@ class MusicLibraryController
   end
 
   def call
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    input = gets.strip.downcase
-    call unless input == "exit"
+    input = ""
+
+    while input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      input = gets.strip.downcase
 
       case input
       when "list songs"
@@ -33,6 +35,7 @@ class MusicLibraryController
       when "play song"
         play_song
       end
+    end
   end
 
    def list_songs
