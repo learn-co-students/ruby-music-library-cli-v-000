01cf7fd7ced01a121f77c3a352f30ba70bc7ba34
Diego Yanez <diegosyanez92@gmail.com>
2018-04-24 00:55:17 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,6 +18,21 @@ class MusicLibraryController
     puts "What would you like to do?"
 
     input = gets.strip
+
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
     end
   end
 
@@ -60,4 +75,14 @@ class MusicLibraryController
       end
     end
   end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip.to_i
+    if (1..Song.all.length).include?(input)
+      song = Song.all.sort_by(&:name)[input - 1]
+    puts "Playing #{song.name} by #{song.artist.name}"
+  end
+  end
+
 end
