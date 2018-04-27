8c7d59da4d710f938028e89faa6baea48fce47e5
David McGrann <dpmcgrann@gmail.com>
2018-04-27 19:52:10 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,60 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+  @@all = []
+
+  def initialize(name, artist=nil, genre=nil)
+    @name = name
+    self.artist = artist if artist != nil
+    self.genre = genre if genre != nil
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self) unless genre.songs.include?(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name, artist=nil, genre=nil)
+    self.new(name, artist, genre).tap {|s| s.save}
+  end
+
+  def self.find_by_name(name)
+    @@all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+
+  def self.new_from_filename(name)
+    data = name.split(" - ")
+    artist_name, song_name, genre_name = data[0], data[1], data[2].gsub(".mp3", "")
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.create(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(data)
+    self.new_from_filename(data)
+  end
+
+
+end
