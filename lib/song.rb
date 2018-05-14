e42ca579862f141544141332cbe2d09c0f84a117
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-14 22:44:58 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,84 @@
+require "pry"
+class Song
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+end
+
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
+end
+
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    genre
+  end
+end
