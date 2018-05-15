8751fd5d2e8ae477de46003b43b299facade0646
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-15 17:48:18 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -39,4 +39,11 @@ class Song
     self.all.clear
   end
 
+  def self.find_by_name(name)
+    @@all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+  end
 end
