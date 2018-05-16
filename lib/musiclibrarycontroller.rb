2895a7a893c5e42e6d47147695e35b9ae83a3369
Eric DeSimone <eric.desimone@cortland.edu>
2018-05-16 20:15:02 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -8,8 +8,6 @@ class MusicLibraryController
   end
 
   def call
-
-    while answer != 'exit'
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -30,8 +28,10 @@ class MusicLibraryController
         list_songs_by_artist
       elsif answer == 'list genre'
         list_songs_by_genre
-      else answer == 'play song'
+      elsif answer == 'play song'
         play_song
+      else
+
     end
   end
 
