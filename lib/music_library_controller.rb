1ad54ab383d2e9d6c39c247879bbd2338c49aee6
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-16 15:12:55 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,16 +1,32 @@
 require "pry"
-require_relative './music_importer.rb'
+
 
 class MusicLibraryController
   attr_accessor :path
 
   def initialize(path='./db/mp3s')
-    MusicImporter.new('./db/mp3s')
+    MusicImporter.new(path).import
     @path = path
   end
 
   def call
-    puts "Welcome to your music library!"
+    welcome
+    input = gets.chomp
+    while input != "exit"
+      welcome
+      input = gets.chomp
+    end
+  end
 
+  def welcome
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
   end
 end
