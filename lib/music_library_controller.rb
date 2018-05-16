44c723c7c86998eb3ee4d48567a0491a193ba429
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-16 02:21:24 +0000

    Done.

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,23 @@
+require "pry"
+class MusicLibraryController < MusicImporter
+  attr_accessor :path, :MusicImporter
+
+  def initialize(path = "./db/mp3s")
+    @path = path
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    input = gets.chomp
+    binding.pry
+
+  end
+
+end
