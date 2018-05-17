fa6278dbe4d34291f27f36aeb6eab5f11e34fbbd
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-17 20:05:39 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -45,7 +45,40 @@ class MusicLibraryController
       a = obj.name
       puts "#{count}. #{a}"
       count += 1}
+  end
+  
+  def list_genres
+    count = 1
+    az = Genre.all.sort_by {|obj| obj.name}
+    az.each {|obj| 
+      g = obj.name
+      puts "#{count}. #{g}"
+      count += 1}
   end 
+  
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets
+    if Artist.find_by_name(input) 
+      a = Artist.find_by_name(input)
+      count = 1
+      az = a.songs.sort_by {|obj| obj.name}
+      az.each {|obj| puts "#{count}. #{obj.name} - #{obj.genre.name}"
+        count += 1}
+    end 
+  end 
+  
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets
+    if Genre.find_by_name(input) 
+      g = Genre.find_by_name(input)
+      count = 1
+      az = g.songs.sort_by {|obj| obj.name}
+      az.each {|obj| puts "#{count}. #{obj.artist.name} - #{obj.name}"
+        count += 1}
+    end 
+  end
 end
 
 #  learn --fail-fast
