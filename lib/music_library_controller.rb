41b83d76fea09ef10aff8e7dcf496ae10fe4f339
Shirley Collins <shirley@m104.us>
2018-05-25 23:11:24 -0700

    Added list_artists & list_genres methods in MusicLibraryController.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -26,12 +26,11 @@ class MusicLibraryController
 
   # CLI Methods
   def list_songs
-    songs_obj = (Song.all).sort_by {|obj| obj.name}
+    all_songs = (Song.all).sort_by {|obj| obj.name}
     songs_array = Array.new
     obj_count = 1
 
-    songs_obj.each do |song|
-      # binding.pry
+    all_songs.each do |song|
       artist = song.artist.name
       genre = song.genre.name
       song_name = song.name
@@ -41,6 +40,38 @@ class MusicLibraryController
 
     songs_array.each do |song|
       puts song
+    end
+  end
+
+  def list_artists
+    all_artists = (Artist.all).sort_by {|obj| obj.name}
+    artist_array = Array.new
+    obj_count = 1
+
+    all_artists.each do |artist|
+      artist = artist.name
+      artist_array << "#{obj_count}. #{artist}"
+      obj_count += 1
+    end
+
+    artist_array.each do |artist|
+      puts artist
+    end
+  end
+
+  def list_genres
+    all_genres = (Genre.all).sort_by {|obj| obj.name}
+    genre_array = Array.new
+    obj_count = 1
+
+    all_genres.each do |genre|
+      genre = genre.name
+      genre_array << "#{obj_count}. #{genre}"
+      obj_count += 1
+    end
+
+    genre_array.each do |genre|
+      puts genre
     end
   end
 end
