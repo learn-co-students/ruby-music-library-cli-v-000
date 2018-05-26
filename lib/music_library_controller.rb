21d16e22f401ee00fc1297a528a0349446177850
Shirley Collins <shirley@m104.us>
2018-05-26 11:21:48 -0700

    Added play method to MusicLibraryController class.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -27,51 +27,103 @@ class MusicLibraryController
   # CLI Methods
   def list_songs
     all_songs = (Song.all).sort_by {|obj| obj.name}
-    songs_array = Array.new
     obj_count = 1
+    songs_array = Array.new
 
     all_songs.each do |song|
       artist = song.artist.name
       genre = song.genre.name
       song_name = song.name
-      songs_array << "#{obj_count}. #{artist} - #{song_name} - #{genre}"
+      str = "#{obj_count}. #{artist} - #{song_name} - #{genre}"
+      songs_array << str
+      puts str
       obj_count += 1
     end
-
-    songs_array.each do |song|
-      puts song
-    end
+    return songs_array
   end
 
   def list_artists
     all_artists = (Artist.all).sort_by {|obj| obj.name}
-    artist_array = Array.new
     obj_count = 1
+    artists_array = Array.new
 
     all_artists.each do |artist|
       artist = artist.name
-      artist_array << "#{obj_count}. #{artist}"
+      str = "#{obj_count}. #{artist}"
+      artists_array << str
+      puts str
       obj_count += 1
     end
 
-    artist_array.each do |artist|
-      puts artist
-    end
+    return artists_array
   end
 
   def list_genres
     all_genres = (Genre.all).sort_by {|obj| obj.name}
-    genre_array = Array.new
     obj_count = 1
+    genres_array = Array.new
 
     all_genres.each do |genre|
       genre = genre.name
-      genre_array << "#{obj_count}. #{genre}"
+      str = "#{obj_count}. #{genre}"
+      genres_array << str
+      puts str
       obj_count += 1
     end
+  end
 
-    genre_array.each do |genre|
-      puts genre
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    artist_name = gets.strip
+    artist = Artist.find_by_name(artist_name)
+    songs_by_artist = Array.new
+
+    if !artist.nil?
+      artist_songs_sorted = (artist.songs).sort_by {|x| x.name}
+      count = 1
+
+      artist_songs_sorted.each do |song|
+        str = "#{count}. #{song.name} - #{song.genre.name}"
+        songs_by_artist << str
+        puts str
+        count +=1
+      end
+    end
+    return songs_by_artist
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    genre_name = gets.strip
+    genre = Genre.find_by_name(genre_name)
+    songs_by_genre = Array.new
+
+    if !genre.nil?
+      genre_songs_sorted = (genre.songs).sort_by {|x| x.name}
+      count = 1
+
+      genre_songs_sorted.each do |song|
+        str = "#{count}. #{song.artist.name} - #{song.name}"
+        songs_by_genre << str
+        puts str
+        count += 1
+      end
+    end
+    return songs_by_genre
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input =  gets.strip
+
+    index = input.to_i - 1
+    all_songs = (Song.all).sort_by {|obj| obj.name}
+
+    if index >= 0 && index <= all_songs.length
+      song = all_songs[index]
+      unless song.nil?
+        puts "Playing #{song.name} by #{song.artist.name}"
+      end
     end
   end
 end
