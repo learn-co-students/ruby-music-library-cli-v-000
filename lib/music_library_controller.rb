0e32f0b60990b5959d0b202fb6619f34bdbaef8a
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-05-29 21:46:30 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,31 +1,48 @@
 class MusicLibraryController
 
   def initialize(path = "./db/mp3s")
-    @path = path
     MusicImporter.new(path).import
   end
-  
+
   def call
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    @path = gets.strip
-    until @path == "exit"
-      call
+    user_input = ""
+    until user_input == "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      user_input = gets.chomp
+
+      case user_input
+      when "list_songs"
+        list songs
+
+      when "list artists"
+        list_artists
+
+      when "list genres"
+        list_genres
+
+      when "list artist"
+        list_songs_by_artist
+
+      when "list genre"
+        list_songs_by_genre
+
+      when "play song"
+        play_song
+
+      end
     end
   end
-  
+
   def list_songs
-    number = array.each do |num|
-      array[index].to_i + 1
-    end
-    puts "#{number}. #{Song.all}.sort"
+
   end
-  
+
 end
