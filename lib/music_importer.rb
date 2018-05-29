62c029252a130dcc4024c7ba684ddc51d6f20772
Dale Terrell <tragicid@gmail.com>
2018-05-29 21:44:52 +0000

    Done.

---
+++ lib/music_importer.rb
@@ -0,0 +1,17 @@
+class MusicImporter
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir[File.join(@path, '*.mp3')].map do |file|
+      File.basename file
+    end
+  end
+
+  def import
+    files.each { |file| Song.create_from_filename(file) }
+  end
+end
