18fa38f06f16bb632a9d113f0680d2f785775c4c
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-29 01:10:17 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -41,4 +41,12 @@ class MusicLibraryController
     artist = Artist.find_or_create_by_name(input)
     artist.songs.sort_by {|s| s.name}.each_with_index {|s, i| puts "#{i+1}. #{s.name} - #{s.genre.name}"}
   end
+  
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+    artist = Genre.find_or_create_by_name(input)
+    genre.songs.sort_by {|s| s.name}.each_with_index {|s, i| puts "#{i+1}. #{s.artist.name} - #{s.name}"}
+  end
+  
 end
