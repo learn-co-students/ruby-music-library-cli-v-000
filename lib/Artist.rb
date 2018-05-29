7af363c022c6217411fe90a3362feada8a757c36
Dale Terrell <tragicid@gmail.com>
2018-05-29 21:08:57 +0000

    Done.

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -1,44 +1,35 @@
 class Artist
-  attr_accessor :songs, :name, :genre
-  @@all = []
-  
+  attr_accessor :name, :genre, :artist
+
   def initialize(name)
     @name = name
-    @songs = []
-    @@all << self
+       
+   
+  @@all << self
   end
   
-  def new_song(name, genre)
-    Song.new(name, self, genre)
-  end
+  @@all = []
   
-  def songs 
-    Song.all.select{|something|something.artist==self}
-  end
-
-  def add_Artist()
+  def add_Song()
     self.Artist << Artist
-    song.artist = self
+    artist.song = self
   end
   
   def self.all 
-    @@all
+   @@all
   end
   
-  def self.reset 
-    @@all.clear
+  def self.destroy_all
+    @@all = []
   end
   
-  def save 
+  def save
     @@all << self
   end
   
-  def self.count
-    @@all.size 
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
   end
-  
-  def self.find_by_name(name)
-    @@all.detect{|artist| artist.name = name}
-  end
-
 end
--- lib/Genre.rb
+++ lib/Genre.rb
@@ -6,16 +6,4 @@ class Genre
     @songs = []
     @@all = []
   end
-
-  def add_song(song)
-    self.songs << songs
-  end
-
-  def artists
-    songs.map { |s| s.artist }
-  end
-  
-  def songs 
-    Song.all.select{|something|something.genre==self}
-  end
 end
