9fde5183f8305ebf45a8b3dda35104c97b98b9f1
Dale Terrell <tragicid@gmail.com>
2018-05-29 21:09:46 +0000

    Done.

--- lib/Genre.rb
+++ lib/Genre.rb
@@ -6,4 +6,16 @@ class Genre
     @songs = []
     @@all = []
   end
+
+  def add_song(song)
+    self.songs << songs
+  end
+
+  def artists
+    songs.map { |s| s.artist }
+  end
+  
+  def songs 
+    Song.all.select{|something|something.genre==self}
+  end
 end
