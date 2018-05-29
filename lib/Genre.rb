6cb0df0b67a864621416bca18bbc2662a3d7c6f5
Dale Terrell <tragicid@gmail.com>
2018-05-29 21:19:19 +0000

    Done.

--- lib/Genre.rb
+++ lib/Genre.rb
@@ -1,21 +1,36 @@
 class Genre
-  attr_reader :name, :songs
+#extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :songs
+  @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
-    @@all = []
   end
 
-  def add_song(song)
-    self.songs << songs
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+    self
+  end
+
+  def self.create(name)
+    Genre.new(name).save
   end
 
   def artists
-    songs.map { |s| s.artist }
+    @songs.map(&:artist).uniq
   end
-  
-  def songs 
-    Song.all.select{|something|something.genre==self}
+
+  def to_s
+    name
   end
 end
