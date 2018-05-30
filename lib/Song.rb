cac994ace837cfb28438e8d743442aef2c4915ba
Dale Terrell <tragicid@gmail.com>
2018-05-30 14:04:16 +0000

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -3,7 +3,7 @@ class Song
   attr_accessor :name, :genre, :artist
   @@all = []
   
-  def initialize(name, artist=nil, genre)
+  def initialize(name, artist=nil)
     @name = name
     self.artist = artist if artist
   end
