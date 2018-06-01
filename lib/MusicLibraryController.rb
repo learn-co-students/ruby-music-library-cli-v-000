14c07e8102772ca80f0511cdab6992661f5ae5ff
Dale Terrell <tragicid@gmail.com>
2018-06-01 20:39:01 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,6 +1,9 @@
+require "pry"
+
 class MusicLibraryController
-  def initialize(library_path)
-    MusicImporter.new(library_path)
+  def initialize(library_path = './db/mp3s')
+    music_importer_object = MusicImporter.new(library_path)
+    #how use objet to kall import method from MusicImporter Klass?
   end
   
   def MusicImporter
