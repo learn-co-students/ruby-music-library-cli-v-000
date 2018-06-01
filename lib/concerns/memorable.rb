0c1529653fb935e6c3d94d7780ab4a74e3d3121c
tsyaeger <tsyaeger@gmail.com>
2018-05-31 22:05:16 -0700

    draft

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,21 +2,20 @@
 
 
 
-module Concerns::Memorable
+module Memorable
 
 	module ClassMethods
 
-
-		def self.destroy_all
-    		all.clear
+		def destroy_all
+    		self.all.clear
 		end
 
-
-		def create(name)
-			object = self.new(name).tap do |object|
-				object.save
-			end
-		end
+		# def create(name)
+		# 	object = self.new(name).tap do |object|
+		# 		object.save
+		# 	end
+		# end
+		
 	end
 
 
