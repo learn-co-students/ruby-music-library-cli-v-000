f91d2b2649696ebe933ec8c8d357aa9f76481c1a
Dale Terrell <tragicid@gmail.com>
2018-06-02 18:01:26 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -7,8 +7,9 @@ class MusicLibraryController
   end
   
   def call 
-    input = gets.strip
+    input = " "
     while input != "exit"
+    input = gets.strip
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
