df2c2954f6ba12fb08b30cea9bb0f005264fe7ce
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-02 01:23:45 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -23,6 +23,7 @@ class MusicLibraryController
   
   def list_songs
     alphabetized = Song.all.sort_by { |song| song.name }
+    
     alphabetized.each_with_index { |song, index| 
       puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     }
@@ -30,6 +31,7 @@ class MusicLibraryController
   
   def list_artists
     sorted = Artist.all.sort_by { |band| band.name }.uniq
+    
     sorted.each_with_index { |musician, index|
       puts "#{index + 1}. #{musician.name}"
     }
@@ -37,9 +39,24 @@ class MusicLibraryController
   
   def list_genres
     organized = Genre.all.sort_by { |type| type.name }.uniq
+    
     organized.each_with_index { |style, index| 
       puts "#{index + 1}. #{style.name}"
     }
+  end
+  
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    answer = gets.strip
+    music_group = Artist.find_by_name(answer)
+    
+    if music_group != nil
+      song_list = music_group.songs.sort_by { |song| song.name }
+      
+      song_list.each_with_index { |song, index|
+        puts "#{index + 1}. #{song.name} - #{song.genre.name}"
+      }
+    end
   end
 end
 
