b3ec06b7cc26fc9c130970d80c4fa1a50c94b3e5
Amar Chatman <amarjerome@gmail.com>
2018-06-14 21:19:35 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,40 @@
+class Artist
+
+  attr_accessor :name
+
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(name)
+    @songs << name 
+  end
+
+
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,34 @@
+require 'pry'
+
 class Song
 
 
-attr_accessor :name
+attr_accessor :name, :artist
+
 
 @@all = []
 
-def initialize(name)
+def initialize(name, artist = nil)
   @name = name
+  @artist = artist 
 end
 
 def self.all
   @@all
-end 
+end
+
+def self.destroy_all
+  @@all.clear
+end
+
+def save
+  @@all << self
+end
+
+def self.create(name)
+  song = Song.new(name)
+  song.save
+  song
+end
 
 end
