e0a2eb505a9281259aa3114c582a3be133936c57
Amar Chatman <amarjerome@gmail.com>
2018-06-15 23:30:20 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,18 @@
 module Concerns::Findable
 
-  def self.find_by_name(song)
-    @@all.detect do |x|
+  def find_by_name(song)
+    self.all.detect do |x|
       x.name == song
     end
   end
+
+  def find_or_create_by_name(song)
+    if !self.find_by_name(song)
+      self.create(song)
+    else
+      self.find_by_name(song)
+    end
+  end
+
 
 end
