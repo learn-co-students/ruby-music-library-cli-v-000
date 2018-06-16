4070c01ed57d157fe397be290f2c7301ab9a3fd9
Amar Chatman <amarjerome@gmail.com>
2018-06-16 17:54:05 +0000

    Done.

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -10,8 +10,13 @@ end
 def files
   Dir.entries(path).select do |files|
     files.include?("mp3")
-  end 
+  end
 end
 
+def import
+  files.collect do |songs|
+    Song.create_from_filename(songs)
+  end 
+end
 
 end
