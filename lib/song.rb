84e253fcc9354ea8579fdae2e03e03713acec6ec
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-19 16:08:54 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist, :genre
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end
+
+  def artist=(artist)
+    @artist = artist
+    self.artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    @genre.songs << self unless @genre.songs.include?(self)
+  end
+
+  def self.find_by_name(name)
+    @@all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+end
