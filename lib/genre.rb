b4d040c5544f9752048a0180a097c285248f81a4
Gabriel Valle <gvalle91@gmail.com>
2018-06-21 00:08:54 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,32 +1,38 @@
 class Genre
   @@all = []
-  
+
   attr_accessor :name
 
   def initialize(name)
     @name = name
     @songs = []
   end
-  
-  def create(name)
+
+  def self.create(name)
     new_genre = Genre.new(name)
-    self.save
+    new_genre.save
   end
-  
+
   def artists
     @songs.collect do |song_name|
       song_name.artist
     end
   end
-  
+
+  def self.find_by_name(genre_name)
+      self.all.find do |genre_object|
+        genre_object.name == genre_name
+      end
+  end
+
   def save
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.destroy_all
     @@all.clear
   end
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
   @@all = []
-  
+
   attr_accessor :name, :artist, :genre
 
   def initialize(name:, artist: = nil, genre: = nil)
@@ -8,7 +8,7 @@ class Song
     self.artist=(artist) if artist
     @genre = genre
   end
-  
+
   def artist=(artist_name)
     if Artist.find_by_name(artist_name)
       Artist.find_by_name(artist_name).songs << self
@@ -19,20 +19,42 @@ class Song
       @artist = new_artist
     end
   end
-  
-  def create(name)
-    new_song = Song.new(name)
-    self.save
+
+  def genre=(genre_name)
+    if Genre.find_by_name(genre_name)
+      Genre.find_by_name(genre_name).songs << self
+      @genre = Genre.find_by_name(genre_name)
+    else
+      new_genre = Genre.create(genre_name)
+      new_genre.songs << self
+      @genre = new_genre
+    end
   end
-  
+
+  def self.find_by_name(name)
+    self.all.find do |song_name|
+      song_name.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) if self.find_by_name(name)
+    self.create(name) if !self.find_by_name(name)
+  end
+
+  def self.create(name)
+    new_song = Song.new(name)
+    new_song.save
+  end
+
   def save
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.destroy_all
     @@all.clear
   end
