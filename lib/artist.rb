11cde93fa986b5992891cc2c935ecd2de0e4e127
Kazumi Karbowski <kazkazinus@gmail.com>
2018-06-22 04:23:58 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,49 +1,51 @@
 require 'pry'
 
 class Artist
+  # extend Findable
 
   attr_accessor :name, :song
 
   @@all = []
 
   def initialize(name)
-    @name = name
-    @@all << self
-    @songs = []
+      @name = name
+      @@all << self
+      @songs = []
   end
 
   def self.all
-    @@all
+      @@all
   end
 
   def self.destroy_all
-    @@all.clear
+      @@all.clear
   end
 
   def save
-    @@all << self
+      @@all << self
   end
 
   def self.create(name)
-    self.new(name)
+      self.new(name)
   end
 
   def songs
-    @songs
+      @songs
   end
 
   def add_song(song)
-      if song.artist == nil
-        song.artist = self
-      end
+        if song.artist == nil
+          song.artist = self
+        end
 
-      if @songs.include?(song) == false
-        @songs << song
-      end
+        if @songs.include?(song) == false
+          @songs << song
+        end
   end
 
   def genres
-    songs.map {|song|song.genre}.uniq
+      songs.map {|song|song.genre}.uniq
   end
+
 
 end
