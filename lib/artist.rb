39e7bed5cf5bf574bf9415cf47bea1b751eea641
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-06-22 19:22:11 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+class Artist
+  extend Concerns::Findable
+
+  attr_accessor :name, :songs, :genre
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new(name).tap{ |a| a.save }
+  end
+
+  def add_song(song)
+    song.artist = self unless song.artist == self
+    @songs << song unless @songs.include?(song)
+  end
+
+  def genres
+    songs.collect{ |s| s.genre }.uniq
+  end
+end
---
+++ lib/genre.rb
@@ -0,0 +1,32 @@
+class Genre
+  extend Concerns::Findable
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    new(name).tap { |genre| genre.save }
+  end
+
+  def artists
+    songs.collect{ |s| s.artist }.uniq
+  end
+end
