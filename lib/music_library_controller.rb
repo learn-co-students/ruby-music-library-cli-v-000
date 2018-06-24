325f3eb2570b77bcb87ea412a496c9ec8a6f0688
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-06-24 01:09:30 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -11,7 +11,7 @@ class MusicLibraryController
     puts "To list all of the artists in your library, enter 'list artists'."
     puts  "To list all of the genres in your library, enter 'list genres'."
     puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genres'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
@@ -26,8 +26,11 @@ class MusicLibraryController
       when input == 'list artists'
         list_artists
         'list artists'
-      when input == 'list genre'
+      when input == 'list genres'
         list_genres
+        'list genres'
+      when input == 'list genre'
+        list_genre
         'list genre'
       when input == 'play song'
         play_song
