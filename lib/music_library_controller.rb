4fcdd88743fd175a4a68bec1db4ae7bc1d9cd49f
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-06-24 00:51:47 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -11,7 +11,7 @@ class MusicLibraryController
     puts "To list all of the artists in your library, enter 'list artists'."
     puts  "To list all of the genres in your library, enter 'list genres'."
     puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To list all of the songs of a particular genre, enter 'list genres'."
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
@@ -21,12 +21,16 @@ class MusicLibraryController
     #this is a switch statment
     case input
       when input == 'list songs'
+        list_songs
         'list songs'
       when input == 'list artists'
+        list_artists
         'list artists'
       when input == 'list genre'
+        list_genres
         'list genre'
       when input == 'play song'
+        play_song
         'play song'
       end
     end
