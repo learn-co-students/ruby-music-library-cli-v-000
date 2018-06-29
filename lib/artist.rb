804989ab682335b207aa713989a73ca9603edbcf
Hayden Betts <haydenbetts@gmail.com>
2018-06-29 18:16:02 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,28 @@
 class Artist
   attr_accessor :name, :songs
+
+  @@all = []
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    self.name = name
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  # MEMORABLE
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    return artist
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 class Song
+  
   attr_accessor :name, :artist, :genre
 
   @@all = []
