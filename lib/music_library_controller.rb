2139eb35deb14b3b77657a4339d91d63d6f5ab8c
Hayden Betts <haydenbetts@gmail.com>
2018-06-29 20:19:58 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,6 +1,11 @@
 class MusicLibraryController
 
-  def initialize(path = "./db/mp3s/")
+  INPUT_OPTIONS = ['list songs', 'list artists', 'list genres',
+                  'list artist', 'list genre', 'play song', 'exit']
+
+  def initialize(path = "./db/mp3s")
+    music_importer = MusicImporter.new(path)
+    music_importer.import
   end
   # Upon initialization, accepts an optional path to the
   # library of MP3 files, defaulting to ./db/mp3s/.
@@ -8,8 +13,22 @@ class MusicLibraryController
   # which it will use to import songs from the specified library.
 
   def call
-  # Has a #call method that starts the CLI and prompts the user
-  # for input. Read the tests carefully for specifics.
+
+    input = ""
+
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+
+    input = gets.strip
+    
+    call unless INPUT_OPTIONS.include?(input)
   end
 
 end
