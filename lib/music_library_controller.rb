65e26f196940c90f900de2557e77ba66d56bcbe7
Hayden Betts <haydenbetts@gmail.com>
2018-06-29 20:53:59 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -37,6 +37,10 @@ class MusicLibraryController
       list_genres
     when 'list artist'
       list_songs_by_artist
+    when 'list genre'
+      list_songs_by_genre
+    when 'play song'
+      play_song
     end
 
     call unless input == 'exit'
