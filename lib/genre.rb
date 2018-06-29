0d43b154d565f42463200bb7a9f11df168047796
Hayden Betts <haydenbetts@gmail.com>
2018-06-29 18:19:29 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,8 +1,31 @@
 class Genre
   attr_accessor :name, :songs
-  
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
   def initialize(name)
+    self.name = name
     self.songs = []
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  # MEMORABLE
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    return genre
   end
 
 end
