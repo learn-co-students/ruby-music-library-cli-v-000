c3171bab41a4acf409e3abc1634db3c17509b6a3
Michael Blandford <mdblandford@gmail.com>
2018-07-01 19:30:13 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -34,21 +34,8 @@ class MusicLibraryController
     else
       call
     end
-  end#input = ''
-    #until input == 'exit'
-    #  puts "Welcome to your music library!"
-    #  puts "To list all of your songs, enter 'list songs'."
-    #  puts "To list all of the artists in your library, enter 'list artists'."
-    #  puts "To list all of the genres in your library, enter 'list genres'."
-    #  puts "To list all of the songs by a particular artist, enter 'list artist'."
-    #  puts "To list all of the songs of a particular genre, enter 'list genre'."
-    #  puts "To play a song, enter 'play song'."
-    #  puts "To quit, type 'exit'."
-    #  puts "What would you like to do?"
-    #input = gets.chomp
-    #  end
-  #end
-
+  end
+  
   def list_songs
     Song.all.sort_by(&:name).each.with_index(1) do |song, index|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
