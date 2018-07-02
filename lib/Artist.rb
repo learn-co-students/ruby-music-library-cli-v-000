69c0739c3ee6d363bc470c9369542af46c6da8fe
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-02 23:24:38 +0000

    Done.

---
+++ lib/Artist.rb
@@ -0,0 +1,48 @@
+class Artist
+  extend Concerns::Findable
+attr_accessor :name, :songs
+
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @songs = []
+end
+
+def self.all
+  @@all
+end
+
+def save
+  @@all << self
+end
+
+def self.destroy_all
+  @@all.clear
+end
+
+def self.create(name)
+  artist = self.new(name)
+  artist.save
+  artist
+end
+
+def add_song(song)
+song.artist = self if !song.artist
+songs << song if !songs.include?(song)
+end
+
+def genres
+  songs.map do |x|
+    x.genre
+  end.uniq
+end
+
+
+
+
+
+
+
+end
---
+++ lib/Song.rb
@@ -0,0 +1,69 @@
+require 'pry'
+class Song
+attr_accessor :name
+attr_reader :artist, :genre
+@@all = []
+
+def initialize(name, artist = nil, genre = nil)
+  self.artist = artist if artist
+  self.genre = genre if genre
+  @name = name
+end
+
+def self.all
+  @@all
+end
+
+def save
+  @@all << self
+end
+
+def self.destroy_all
+  @@all.clear
+end
+
+def self.create(song)
+  new_song = self.new(song)
+  new_song.save
+  new_song
+end
+
+def artist=(artist)
+  @artist = artist
+  artist.add_song(self)
+end
+
+def genre=(genre)
+  @genre = genre
+  genre.add_song(self)
+end
+
+def self.find_by_name(name)
+  @@all.find {|x| x.name == name}
+end
+
+def self.find_or_create_by_name(name)
+  if self.find_by_name(name)
+    self.find_by_name(name)
+  else
+    self.create(name)
+   end
+end
+
+def self.new_from_filename(filename)
+   song_name = filename.split(" - ")[1]
+   artist_name = filename.split(" - ")[0]
+   genre_name = filename.split(" - ")[2].gsub(".mp3","")
+   new_song = self.new(song_name)
+   new_song.artist = artist_name
+   new_song.genre = genre_name
+end
+
+
+
+
+
+
+
+
+end
