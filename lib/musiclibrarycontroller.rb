8729bc0a08b41b9a0758ae82ce0b1801cff234e7
Kazumi Karbowski <kazkazinus@gmail.com>
2018-07-03 04:32:25 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -11,6 +11,8 @@ class MusicLibraryController
   end
 
   def call
+    input = ""
+    while input != "exit"
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -20,11 +22,23 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    gets
-    "exit"
-    3.times do
-      gets
+    input = gets.strip
+    case input
+    when "list songs"
+      list_songs
+    when "list artists"
+      list_artists
+    when "list genres"
+      list_genres
+    when "list artist"
+      list_songs_by_artist
+    when "list genre"
+      list_songs_by_genre
+    when "play song"
+      play_song
     end
+  end
+
   end
 
   def list_songs
@@ -58,9 +72,33 @@ class MusicLibraryController
       sorted_songs.each.with_index(1) do |song, index|
         puts "#{index}. #{song.name} - #{song.genre.name}"
       end
+    end
+  end
 
-end
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
 
+    genre = Genre.find_by_name(input)
+    if genre
+      sorted_songs = genre.songs.sort do |a, b|
+        a.name <=> b.name
+      end
+
+      sorted_songs.each.with_index(1) do |song, index|
+        puts "#{index}. #{song.artist.name} - #{song.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.to_i
+    sorted_songs = Song.all.sort {|song1, song2|song1.name <=> song2.name}
+    song = sorted_songs[input - 1]
+    if input >= 1 && input <= sorted_songs.count
+    puts "Playing #{song.name} by #{song.artist.name}"
+    end
   end
 
 end
