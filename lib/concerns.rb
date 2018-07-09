7d4f3b06fab434e80e0d62e3a72c28ab7e929d7b
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-09 15:43:03 +0000

    Done.

---
+++ lib/concerns.rb
@@ -0,0 +1,19 @@
+module 
+  Concerns::Findable
+  
+  def find_by_name(name)
+    self.all.find do |thing|
+      thing.name == name 
+    end 
+  end 
+  
+   def find_or_create_by_name(name)
+    return_value = self.find_by_name(name)
+    if return_value
+      return_value
+    else 
+      self.create(name)
+    end
+  end
+  
+end 
