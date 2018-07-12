eb4e325f27e5e44eb5154955e9b27703671d300d
Leslie Stevens <flylady@mail.utexas.edu>
2018-07-12 19:43:31 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -41,4 +41,17 @@ class Song
     new_song
   end
 
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create(name)
+    end
+  end
+
 end
