e49f7ad5e67c366db65ce87b0807838bece8d192
Leslie Stevens <flylady@mail.utexas.edu>
2018-07-13 22:06:28 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 class MusicLibraryController
   attr_accessor :path
 
@@ -8,8 +8,32 @@ class MusicLibraryController
   end
 
   def call
+
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+
+
+  end
+
+  def list_songs
+    sorted_songs = Song.all.sort_by {|song|
+      song.name}
+    sorted_songs.each_with_index {|song, index|
+      puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+  end
+
+  def list_artists
+    sorted_artists = Artist.all.sort_by {|artist|
+      artist.name}
+    sorted_artists.each_with_index{|artist, index|
+      puts "#{index + 1}. #{artist.name}"}
   end
 
 
