40bb308db1ad1e0ce13930f77d3b514c40f1563f
Leticia Molina <letimolina@gmail.com>
2018-07-14 22:59:04 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,14 +15,17 @@ class Artist
   end
   
   def self.destroy_all
- 
+    all.clear
   end
   
   def save
-
+    self.class.all << self
   end
   
   def self.create(name)
+    artist = new(name)
+    artist.save
+    artist
 
   end
 
