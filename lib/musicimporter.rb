7ace16a03308dda8ce1abc6920c60e8625c74709
valeria campilongo <cvaleriac@hotmail.it>
2018-07-17 00:25:56 +0000

    Done.

---
+++ lib/musicimporter.rb
@@ -0,0 +1,19 @@
+class MusicImporter
+
+def initialize(path)
+  @path = path
+end
+
+def path
+  @path
+end
+
+def files
+  Dir.entries(path).select{ |file| file.end_with?(".mp3") }
+end
+
+def import
+  files.each { |file| Song.create_from_filename(file) }
+end
+
+end
