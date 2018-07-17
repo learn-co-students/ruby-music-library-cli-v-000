26e770e7b491559f21cb4f35af087edd9961ab4a
Trisha Garrett <trisha.garrett@gmail.com>
2018-07-17 00:16:22 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,6 +5,7 @@ class Artist
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def self.all
@@ -23,5 +24,19 @@ class Artist
     artist = Artist.new(name)
       artist.save
       artist
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    if song != song.artist
+      song.artist = self
+    #binding.pry
+    else
+      song
+    end
+
   end
 end
