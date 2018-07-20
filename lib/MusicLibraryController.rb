13bfa0c65e68489d77aaa440275da89477b960ce
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-20 21:52:16 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -4,7 +4,6 @@ class MusicLibraryController
   def initialize(path="./db/mp3s")
     @path = path
     @music_importer = MusicImporter.new(path)
-    #binding.pry
     @music_importer.import
   end
 
@@ -33,10 +32,48 @@ class MusicLibraryController
     end
 
     def list_artists
-      list = Song.all.map {|song| song.artist.name}
-      binding.pry
-      list.uniq!.sort_by!{|a| a.name}
-      list.each_with_index {|val, index| puts "#{index+1}. #{val.name}"}
+      list = Artist.all.map { |artist| artist.name}
+      #binding.pry
+      list.sort!
+      list.each_with_index {|val, index| puts "#{index+1}. #{val}"}
     end
 
+    def list_genres
+      list = Genre.all.map {|genre| genre.name}
+      #binding.pry
+      list.sort!
+      list.each_with_index {|val, index| puts "#{index+1}. #{val}"}
+    end
+
+    def list_songs_by_artist
+      puts "Please enter the name of an artist:"
+      artist_name = gets
+      artist = Artist.find_by_name(artist_name)
+      if artist != nil
+        list = artist.songs.sort_by{|song| song.name}
+        list.map.each_with_index {|song, index| puts "#{index+1}. #{song.name} - #{song.genre.name}"}
+     end
+    end
+
+    def list_songs_by_genre
+      puts "Please enter the name of a genre:"
+      genre_name = gets
+      genre = Genre.find_by_name(genre_name)
+      if genre != nil
+        list = genre.songs.sort_by{|song| song.name}
+        list.map.each_with_index {|song, index| puts "#{index+1}. #{song.artist.name} - #{song.name}"}
+     end
+    end
+
+    def play_song
+      #self.list_songs
+      puts "Which song number would you like to play?"
+      files = @music_importer.files.sort_by {|song| song.split(" - ")[1]}
+      song_number = gets.chomp
+      index = song_number.to_i-1
+      #binding.pry
+      if index < files.size && index > 0
+        puts "Playing #{files[index].split(" - ")[1]} by #{files[index].split(" - ")[0]}"
+      end
+    end
 end
