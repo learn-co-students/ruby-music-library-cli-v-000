9f5d64f06c04bb002d1183b7e2bc681acad363b3
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-20 00:06:32 +0000

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -1,3 +1,4 @@
+require pry
 class Song
   attr_accessor :name
   attr_reader :artist, :genre
@@ -45,4 +46,19 @@ class Song
       genre.songs << self
     end
   end
-end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    binding.pryq
+
+    if self.find_by_name(name) == nil
+      new_song = Song.create(name)
+      new_song
+    end
+    self.find_by_name(name)
+  end
+
+end 
