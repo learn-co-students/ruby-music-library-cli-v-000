497d486ce3dffb2454cee3cba79c62830e362c5e
Trisha Garrett <trisha.garrett@gmail.com>
2018-07-22 20:49:05 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -16,6 +16,6 @@ class MusicImporter
   end
 
   def import
-    files.each {|filename| Song.new_by_filename (filename)}
-  end  
+    files.each {|filename| Song.create_from_filename (filename)}
+  end
 end
