4bcb58d018a1acb22a0023e009dc7da7e7f96f19
Wendy Calderon <wendycalderon@gmail.com>
2018-07-26 02:52:21 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,44 @@
+require "pry"
+class Artist
+  attr_accessor :name, :song
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.destroy_all 
+    @@all.clear 
+  end
+  
+  def save 
+    @@all << self
+  end
+  
+  def self.create(name) 
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+  
+  def songs 
+    @songs 
+  end
+  
+  def add_song(song)
+    if song.artist == nil
+      song.artist = self
+      @songs << song unless @songs.include?(song)
+    end
+  end
+  
+  def genres 
+    songs.collect {|song| song.genre}.uniq
+    #binding.pry
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+require "pry"
+class Song 
+  attr_accessor :name 
+  attr_reader :artist, :genre 
+  @@all = []
+  
+  def initialize(name, artist = nil, genre = nil)
+    @name = name 
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.destroy_all 
+    @@all.clear 
+  end
+  
+  def save 
+    @@all << self
+  end
+  
+  def self.create(name) 
+    song = self.new(name)
+    song.save
+    song
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+  
+  def genre=(genre) 
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+end
