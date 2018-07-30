4d5af08ac7a002c421d180648cccfaad21944da0
Kitty Davies <kittyfdavies@gmail.com>
2018-07-30 00:41:52 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,6 +20,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    input = gets.strip
   end
 
 end
