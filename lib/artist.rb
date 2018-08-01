b1e4da57e043d40acced03a1438994350c059ca5
Reno Sorensen <renojames19@gmail.com>
2018-08-01 01:51:27 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+
+
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.create(name)
+    v = self.new(name)
+    v.save
+    v
+  end
+
+end
