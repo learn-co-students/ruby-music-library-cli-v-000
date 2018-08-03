0738558ac6fc63a98da20238998f7675e2e6f86b
Reno Sorensen <renojames19@gmail.com>
2018-08-03 00:56:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -52,4 +52,16 @@ class Song
     @genre
   end
 
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create(name)
+    end
+  end
+
 end
