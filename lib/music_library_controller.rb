d147e92c5bbaa2e5397d0ef2bcb014f2c5599861
Reno Sorensen <renojames19@gmail.com>
2018-08-04 22:02:43 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -15,8 +15,13 @@ class MusicLibraryController
     puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    gets
+    loop do
+      puts "What would you like to do?"
+      i = gets
+      if i == 'exit'
+        break
+      end
+    end
 
 
 
