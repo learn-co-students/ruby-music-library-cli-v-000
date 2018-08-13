b969491ce5329dd21829986bb872f0ae2e4c4b5d
Aziz Kosber <a.kosber@gmail.com>
2018-08-13 02:07:04 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,30 @@
+require 'pry'
 
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(genre_name)
+    song = Genre.new(genre_name)
+    song.save
+    song
+  end
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+  end
+
+  def save
+    @@all << self
+  end
+
+end
