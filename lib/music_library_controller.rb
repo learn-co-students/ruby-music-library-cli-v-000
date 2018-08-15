e49d20d2a22eeea8bb3be13450cf406638ae6282
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-15 03:39:14 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,22 +20,22 @@ class MusicLibraryController
     # until @input == "exit"
     #   @input = gets.strip
     # end
-
-    case @input
-    when 'list songs'
-      list_songs
-    when 'list genres'
-      list_genres
-    when 'list artist'
-      list_songs_by_artist
-    when 'list genre'
-      list_songs_by_genre
-    when 'play_song'
-      play_song
-    else until @input == "exit"
+    until @input == "exit"
       @input = gets.strip
-    end
-
+      case @input
+      when 'list songs'
+        list_songs
+      when 'list genres'
+        list_genres
+      when 'list artist'
+        list_songs_by_artist
+      when 'list genre'
+        list_songs_by_genre
+      when 'play song'
+        play_song
+      when 'list artists'
+        list_artists
+      end
     end
   end
 
