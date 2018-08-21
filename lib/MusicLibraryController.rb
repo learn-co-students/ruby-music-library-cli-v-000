fa05f6d7cf86aacd642504766046dc37ef35f433
Azhar Shah <azharshahstl@yahoo.com>
2018-08-21 20:27:18 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class MusicLibraryController 
   
   def initialize (path = "./db/mp3s")
@@ -15,5 +17,15 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    
+    input = gets.strip
+    
+  end
+  
+  def list_songs
+  Song.all.sort_by{|name| @name}.each_with_index do {|song,index|}
+    puts "#{index + 1} - #{song.artist.name} - #{song.genre.name}"
+    #binding.pry
+  end 
   
 end
