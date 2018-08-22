d66711158ad9eeb215d211c0a5b9a8e61d7ee412
Azhar Shah <azharshahstl@yahoo.com>
2018-08-22 17:40:03 +0000

    Done.

--- lib/Music_Library_Controller.rb
+++ lib/Music_Library_Controller.rb
@@ -7,6 +7,8 @@ class MusicLibraryController
   end
   
   def call 
+    input = ''
+    while input != "exit"
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -18,25 +20,24 @@ class MusicLibraryController
     puts "What would you like to do?"
     
     input = gets.strip
-    until input == "exit"
+      # case input
+      #   when "list songs"
+      #     self.list_songs
+      #   when "list artists"
+      #     self.list_artists
+      # end
       if input == "list artists"
         self.list_artists
-        input = gets.strip
       elsif input == "list songs" 
         self.list_songs 
-        input = gets.strip
       elsif input == "list genres" 
         self.list_genres 
-        input = gets.strip
       elsif input == "list genre"
         self.list_songs_by_genre
-        input = gets.strip
       elsif input == "list artist" 
         self.list_songs_by_artist
-        input = gets.strip
       elsif input == "play song" 
         self.play_song
-        input = gets.strip
       end
     end
   end
@@ -83,13 +84,8 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
     if (1...Song.all.length).include?(input)
-      song = 
+      song = Song.all.sort_by{|song| song.name}[input - 1]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
    end 
-    
-    
-    
-    
-    
 end
