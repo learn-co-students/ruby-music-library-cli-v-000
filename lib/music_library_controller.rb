8a20f2fef73d6f8ec72b6f72032bfd33a168a19d
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-26 17:24:08 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,16 +7,16 @@ class MusicLibraryController
   
   def call
     call_input = ""
-    until call_input == "exit"
-      # puts "Welcome to your music library!"
-      # puts "To list all of your songs, enter 'list songs'."
-      # puts "To list all of the artists in your library, enter 'list artists'."
-      # puts "To list all of the genres in your library, enter 'list genres'."
-      # puts "To list all of the songs by a particular artist, enter 'list artist'."
-      # puts "To list all of the songs of a particular genre, enter 'list genre'."
-      # puts "To play a song, enter 'play song'."
-      # puts "To quit, type 'exit'."     
-      # puts "What would you like to do?"
+    while call_input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."     
+      puts "What would you like to do?"
       call_input = gets.chomp
       case call_input 
       when "list songs"
