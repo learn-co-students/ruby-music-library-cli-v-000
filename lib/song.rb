8f1ed9e54f12714ced21f8f7367432f506444d03
Austin Powell <austinpowell58@gmail.com>
2018-08-28 02:42:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,18 @@ class Song
   def artist=(artist)
     @artist = artist
     artist.add_song(self)
+  end
+  
+  def self.find_by_name(name)
+    self.all.detect{|a| a.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create(name)
+    end 
   end 
     
   
