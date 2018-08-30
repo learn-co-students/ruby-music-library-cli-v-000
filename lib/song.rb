4d88eda120e9fb44b36c491c20ffe26e6477c3cc
Kari O'Neal <triadfirewood@gmail.com>
2018-08-30 14:53:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -40,6 +40,18 @@ class Song
     genre.songs << self unless genre.songs.include?(self)
   end
   
+  def self.find_by_name(name)
+      self.all.detect {|song| song.name == name}
+    end
+  
+  def self.find_or_create_by_name(name)
+      if self.find_by_name(name) == nil
+         self.create(name)
+      else 
+        self.find_by_name(name)
+    end
+  end
+  
   def self.new_from_filename(filename)
     filename_array = filename.split(" - ")
     song_artist = Artist.find_or_create_by_name(filename_array[0])
