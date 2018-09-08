80362f82742bee77cc65c964c720be8772b1df77
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-09-08 16:11:40 -0400

    wip

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   attr_accessor :name
   attr_reader :artist, :genre
+  extend Concerns::Findable
 
   @@all = []
 
@@ -42,23 +43,15 @@ class Song
     end
   end
 
-  def self.create_by_name(name)
-    create(name)
-  end
-
-  def self.find_by_name(name)
-    all.detect { |song| song.name == name }
-  end
-
-  def self.find_or_create_by_name(name)
-    find_by_name(name) || create_by_name(name)
-  end
-
   def self.new_from_filename(filename)
     artist_name, song_name, genre_name = filename.chomp('.mp3').split(" - ")
     song = find_or_create_by_name(song_name)
     song.artist = Artist.find_or_create_by_name(artist_name)
     song.genre = Genre.find_or_create_by_name(genre_name)
     song
+  end
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename)
   end
 end
