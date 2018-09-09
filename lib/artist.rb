eb8e878e5f0d795f7143101cf8e5a11d8b5a2fae
Angie Kassab <anasab12@gmail.com>
2018-09-09 22:41:15 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,20 +4,22 @@ attr_accessor :name
 
 @@all = []
 
-def initialize(artist_name)
-  @name = artist_name
-end 
+  def initialize(artist_name)
+    @name = artist_name
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.destroy_all
+    self.all.clear
+  end 
+  
+  def save
+    self.class.all << self
+  end 
+  
 
-def self.all
-  @@all
-end 
-
-def.self.destroy_all
-  self.all.clear
-end 
-
-def save
-  self.class.all << self
-end 
 
 end 
--- lib/song.rb
+++ lib/song.rb
@@ -4,20 +4,26 @@ attr_accessor :name
 
 @@all = []
 
-def initialize(title)
-  @name= title
-end 
-
-def self.all
-  @@all
-end 
-
-def.self.destroy_all
-  self.all.clear
-end 
-
-def save
-  self.class.all << self
-end 
+  def initialize(title)
+    @name= title
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.destroy_all
+    self.all.clear
+  end 
+  
+  def save
+    self.class.all << self
+  end 
+  
+  def self.create(title)
+    song = Song.new(title)
+    song.save
+    song
+  end 
 
 end 
