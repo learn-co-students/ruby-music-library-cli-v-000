90c43e5f82ad415e3457f436a0f8d0f0773dbb96
Angie Kassab <anasab12@gmail.com>
2018-09-09 23:51:55 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -32,9 +32,13 @@ attr_accessor :name
   end
   
   def add_song(song)
-    @songs << song
-    song.artist =self unless song.artist.include?(self)
+    @songs << song unless @songs.include?(song) # does not add the song to the current artist's collection of songs if it already exists therein
+    if song.artist == nil # does not assign the artist if the song already has an artist
+    song.artist = self # assigns the current artist (self) to the song's 'artist' property (song belongs to artist)
+    end
   end
+  
+  #  Alternate code below:
   
   #  def add_song(song)
   #    if songs.include?(song) == false
