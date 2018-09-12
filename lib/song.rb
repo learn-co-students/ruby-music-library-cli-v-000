d1bebe0dcddd6becc3f337ab119d3f34e712aa6c
Angie Kassab <anasab12@gmail.com>
2018-09-12 21:08:30 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,32 +5,32 @@ attr_reader :artist, :genre
 
 @@all = []
 
-  def initialize(title, artist = artist)
+  def initialize(title, artist = nil)  # Tip: A commonly used default value for optional parameters is nil
     @name= title
-    @artist= artist
-  end 
-  
+    @artist = artist
+  end
+
   def self.all
     @@all
-  end 
-  
+  end
+
   def self.destroy_all
     self.all.clear
-  end 
-  
+  end
+
   def save
     self.class.all << self
-  end 
-  
+  end
+
   def self.create(title)
     song = Song.new(title)
     song.save
     song
-  end 
-  
+  end
+
   def artist=(artist) #setter for artist
     @artist = artist
     artist.add_song(self) #assigns an artist to the song (song belongs to artist)
   end
 
-end 
+end
