e2da2c1704b8a8e4af157aef95cae8fc2f4c7465
Rafael Lopez <lpz.raf@gmail.com>
2018-09-12 01:58:17 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -52,6 +52,17 @@ class Song
     self.find_by_name(name) || self.create(name)
   end
 
+  def self.new_from_filename(filename)
+    split_file = filename.gsub(".mp3", "").split(" - ")
+    artist = Artist.find_or_create_by_name(split_file[0])
+    genre = Genre.find_or_create_by_name(split_file[2])
+    self.new(split_file[1], artist, genre)
+  end
+  
+   def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+  end
 
 end
 
