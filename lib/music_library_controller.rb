d5d0f2c9957e4dc7b5d01567641ec6f00a6e4b30
Rafael Lopez <lpz.raf@gmail.com>
2018-09-14 01:41:56 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,6 +6,7 @@ class MusicLibraryController
     new_importer_object.import
   end
 
+  
   def list_songs
     songs_sorted_by_name = Song.all.sort_by do |song|
       song.name
@@ -16,18 +17,68 @@ class MusicLibraryController
     end
   end
   
- 
- 
+  
   def list_artists
     sorted_by_artist = Artist.all.uniq.sort_by do |artist| 
       artist.name
     end
-    # binding.pry
-    sorted_by_artist2 = sorted_by_artist.each.with_index(1) do |artist,index|
-      puts "#{index}. #{artist.name}"
   
+    sorted_by_artist.each.with_index(1) do |artist,index|
+      puts "#{index}. #{artist.name}"
     end
   end
+  
+   def list_genres
+    sorted_by_genre = Genre.all.uniq.sort_by do |genre| 
+      genre.name
+    end
+  
+    sorted_by_genre.each.with_index(1) do |genre,index|
+      puts "#{index}. #{genre.name}"
+    end
+  end
+  
+    def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.chomp
+    if artist = Artist.find_by_name(input) 
+      songs_sorted_by_name = artist.songs.sort_by do |song|
+        song.name
+      end
+      songs_sorted_by_name.each.with_index(1) do |song,index|
+        puts "#{index}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+  
+   def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.chomp
+    if genre = Genre.find_by_name(input) 
+      songs_sorted_by_name = genre.songs.sort_by do |song|
+        song.name
+      end
+
+      songs_sorted_by_name.each.with_index(1) do |song,index|
+        puts "#{index}. #{song.artist.name} - #{song.name}"
+      end
+    end
+  end
+  
+  
+ def play_song
+    puts "Which song number would you like to play?"
+    list_of_songs =  Song.all.sort{ |a, b| a.name <=> b.name }
+
+    input = gets.strip.to_i
+  
+    if (1..Song.all.length).include?(input)
+      song = list_of_songs[input+2]
+      puts "Playing #{song.name} by #{song.artist.name}"
+    end
+  end
+  
+  
   def call
     input = ""
     while input != "exit"
