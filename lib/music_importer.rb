28185d3b297fbb5aa6ad0a0a568b36f51aaa860f
Angie Kassab <anasab12@gmail.com>
2018-09-15 18:41:59 +0000

    Done.

---
+++ lib/music_importer.rb
@@ -0,0 +1,20 @@
+class MusicImporter
+
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+      #entries method: Returns an array containing all of the filenames in the given directory.
+    @files ||= Dir.entries(@path).select {|filename| !File.directory?(filename) && filename.end_with?(".mp3")}
+      #directory is another name for a folder, and we're not looking for folders, only file names, and specifically file names that end with .mp3
+  end
+
+  def import
+    filecollection.each {|song| Song.new_by_filename(song)}    #We're creating new instances of the Song class
+      # files.each {|song| Song.new_by_filename(song)}
+  end
+
+end
