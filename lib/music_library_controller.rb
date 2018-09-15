773838d5854c59b4c18ebdc420e661a60347caff
Angie Kassab <anasab12@gmail.com>
2018-09-15 20:16:39 +0000

    Done.

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,7 @@
+class MusicLibraryController
+
+  def initialize(path = "./db/mp3s" ) #it "the 'path' argument defaults to './db/mp3s'" do
+    MusicImporter.new(path).import #"invokes the #import method on the created MusicImporter object" do
+  end
+
+end
