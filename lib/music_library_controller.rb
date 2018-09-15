c272df58d32b7dd9fa6f2e1f868272a0c51fb58c
Angie Kassab <anasab12@gmail.com>
2018-09-15 23:40:03 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,9 +17,28 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
 
-    input = gets.strip
-    end
-  end
+      # all of the code below is needed to trigger the methods,
+      # otherwise errors will pop up like:
+      # MusicLibraryController - CLI Commands 'list songs' triggers #list_songs
+
+      input = gets.strip
+         case input
+
+           when "list songs"
+             list_songs
+           when "list artists"
+             list_artists
+           when "list genres"
+             list_genres
+           when "list artist"
+             list_songs_by_artist
+           when "list genre"
+             list_songs_by_genre
+           when "play song"
+             play_song
+         end
+     end
+   end
 
   def list_songs
       sorted_songs = Song.all.sort_by(&:name)
@@ -35,9 +54,6 @@ class MusicLibraryController
 
     def list_artists
         sorted_artists = Artist.all.sort_by(&:name)
-        # either sort_by or a block with <=> can be used to sort an array
-        # of objects, such as an array of strings.
-        # See http://www.rubyguides.com/2017/07/ruby-sort/
 
         sorted_artists.each_with_index do |artist, index|
           puts "#{index+1}. #{artist.name}"
@@ -46,5 +62,59 @@ class MusicLibraryController
           #    got: ("1. #<Artist:0x00000003576270>")
         end
     end
+
+    def list_genres
+        sorted_genres = Genre.all.sort_by(&:name)
+
+        sorted_genres.each_with_index do |genre, index|
+          puts "#{index+1}. #{genre.name}"
+        end
+    end
+
+    def list_songs_by_artist
+      puts "Please enter the name of an artist:"
+      input = gets.strip
+
+      artist = Artist.find_by_name(input)
+
+      if artist != nil # meaning, there is a matching artist found
+         songs_of_artist = artist.songs.sort_by(&:name)
+
+         songs_of_artist.each_with_index do |song, index|
+             puts "#{index+1}. #{song.name} - #{song.genre.name}"
+         end
+      end
+    end
+
+    def list_songs_by_genre
+      puts "Please enter the name of a genre:"
+      input = gets.strip
+
+      genre = Genre.find_by_name(input)
+
+      if genre != nil # meaning, there is a matching genre found
+         songs_of_genre = genre.songs.sort_by(&:name)
+
+         songs_of_genre.each_with_index do |song, index|
+             puts "#{index+1}. #{song.artist.name} - #{song.name}"
+         end
+      end
+    end
+
+    def play_song
+      puts "Which song number would you like to play?"
+      input = gets.strip.to_i # the number that the user inputs will be a string
+
+      # list_songs method inputted below
+
+      sorted_songs = Song.all.sort_by(&:name)
+      sorted_songs.each_with_index do |song, index| # not |index,song|
+        if input == index + 1
+          puts "Playing #{song.name} by #{song.artist.name}"
+        end
+      end
+    end
+
+
 
 end
