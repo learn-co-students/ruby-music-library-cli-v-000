cd2445890d63dfcbf60ce38f149d9dac38971da7
Angie Kassab <anasab12@gmail.com>
2018-09-15 18:44:40 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -9,7 +9,8 @@ class MusicImporter
   def files
       #entries method: Returns an array containing all of the filenames in the given directory.
     @files ||= Dir.entries(@path).select {|filename| !File.directory?(filename) && filename.end_with?(".mp3")}
-      #directory is another name for a folder, and we're not looking for folders, only file names, and specifically file names that end with .mp3
+      # using song instead of (filename) and filename.end_with... like in collaborating objects lab did not work
+      # directory is another name for a folder, and we're not looking for folders, only file names, and specifically file names that end with .mp3
   end
 
   def import
