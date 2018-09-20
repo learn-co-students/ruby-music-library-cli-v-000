0944a5fbc86429940b7fa8e1f7476f55af04df3c
Harley Rayner <harleymteden@gmail.com>
2018-09-20 19:55:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,14 +10,19 @@ class Song
     @@all
   end
 
-  def self.find_by_name(name)
-    # binding.pry. this is where im stuck
-    self.all.select do |song|
-      song.name == name
+  def self.find_by_name(song_name)
+    # detect stops on the first thing it finds that matches the condition and returns that value
+    self.all.detect do |song|
+      song.name == song_name
     end
   end
 
-  def find_or_create_by_name
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name) == nil #if the song isn't in there
+        self.create(song_name)              #make a new song
+    else self.find_by_name(song_name) != nil #if the song is in there
+      self.find_by_name(song_name)            # find it and return it
+    end
   end
 
   def initialize(name, artist = nil, genre = nil)
@@ -51,9 +56,6 @@ class Song
   def genre=(genre)
     @genre = genre
     @genre.songs << self unless @genre.songs.include?(self)
-    # genre.add_song(self)
-    # self.genre = genre
-    # genre.add_song(self)
   end
 
 end
