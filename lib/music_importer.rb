ba55e773e000150df6521f62932b0eeb4b53accf
Sherry Martin <sm674@cornell.edu>
2018-10-02 23:28:21 +0000

    Done.

---
+++ lib/music_importer.rb
@@ -0,0 +1,12 @@
+class MusicImporter
+  attr_reader :path 
+  
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+    Dir.glob("#{path}/*").map{ |file| file.gsub("#{path}/", '') }
+  end
+end
