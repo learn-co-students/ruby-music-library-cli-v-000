b5f35cc11fbe997176b2114647d85e65056ebd8f
Sherry Martin <sm674@cornell.edu>
2018-10-03 13:30:30 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,6 +18,21 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets.strip
+        
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
     end
   end
   
@@ -41,5 +56,32 @@ class MusicLibraryController
   
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
+    input = gets.strip
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort_by(&:name).each.with_index(1) do |song, index|
+        puts "#{index}. #{song.name} - #{song.genre.name}"
+     end
+    end
+  end
+  
+  def list_songs_by_genre
+   puts "Please enter the name of a genre:"
+    input = gets.strip
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort_by(&:name).each.with_index(1) do |song, index|
+        puts "#{index}. #{song.artist.name} - #{song.name}"
+     end
+    end
+  end
+  
+  def play_song
+    puts "Which song number would you like to play?"
+    
+    input = gets.strip.to_i
+    if (1..Song.all.length).include?(input)
+      song = Song.all.sort{ |x,y| x.name <=> y.name }[input - 1]
+    end
+    
+    puts "Playing #{song.name} by #{song.artist.name}" if song
   end
 end
