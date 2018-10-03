d06bbad0b7f3f483266f9880b601bbeaf8384b2e
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-10-03 20:54:56 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -17,39 +17,36 @@ class MusicLibraryController
     
     action = gets.chomp
     
-      if action != "exit"
-        call 
-      elsif action == "list songs"
+    while action != "exit"
+      if action == "list songs"
         list_songs
       elsif action == "list artists"
-        list_artists
+       list_artists
       elsif action == "list genres"
         list_genres
       elsif action == "list artist"
         list_songs_by_artist
-      end 
-    end 
-  
-  def list_songs
-    alphabetize(Song).each_with_index do |song, index|
-      puts "#{index + 1}. #{song_formatter(song)}"
-    end 
+      elsif action == "list genre"
+        list_songs_by_genre
+      elsif action == "play song"
+        play_song
+     end 
+    
+    action = gets.chomp
+    end
   end 
   
+  def list_songs
+    formatter(Song)
+  end 
+
   def list_artists
-    
-     alphabetize(Artist).each_with_index do |artist, index|  
-      puts "#{index + 1}. #{artist.name}"
-    end 
+      formatter(Artist)
   end 
   
   def list_genres
-    alphabetize(Genre).each_with_index do |genre, index|
-      "#{index + 1}. #{genre.name}"
-      puts "#{index + 1}. #{genre.name}"
-    end 
+    formatter(Genre)
   end 
-   
  
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
@@ -77,7 +74,7 @@ class MusicLibraryController
     end
   end
     
-    
+
   def play_song 
     puts "Which song number would you like to play?"
     song_choice = gets.chomp
@@ -96,16 +93,19 @@ class MusicLibraryController
     input.all.sort_by{|sorted|sorted.name}
   end 
   
+  def formatter(class_name)
+    alphabetize(class_name).each_with_index do |title, index|
+      if class_name != Song
+        puts "#{index +1}. #{title.name}"
+      else
+        puts "#{index + 1}. #{title.artist.name} - #{title.name} - #{title.genre.name}"
+      end 
+    end 
+ end 
  
-  def song_formatter(song)
-    "#{song.artist.name} - #{song.name} - #{song.genre.name}" 
-  end 
-  
-  def artist_formatter(artist)
-    "#{song.name} - #{song.genre.name}" 
-  end 
-  
+ 
 end 
+
   
 
 
