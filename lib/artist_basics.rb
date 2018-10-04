ddbc7d92ddf57dd1b941361a9d0efd4cbce5f311
Samantha brake <sammy.brake@gmail.com>
2018-10-04 00:40:43 +0000

    Done.

--- lib/artist_basics.rb
+++ lib/artist_basics.rb
@@ -2,12 +2,13 @@ require 'pry'
 
 class Artist  
   
-attr_accessor :name 
+attr_accessor :name, :songs  
 
 @@all = []
 
 def initialize(name)
   @name = name 
+  @songs = []
 end 
 
 def self.all 
--- lib/song_basics.rb
+++ lib/song_basics.rb
@@ -2,13 +2,16 @@ require 'pry'
 
 class Song 
   
-attr_accessor :name 
+attr_accessor :name, :artist 
 
 @@all = []
 
-def initialize(name)
+def initialize(name, artist=nil)
   @name = name 
+  @artist = artist 
+ 
 end 
+ 
 
 def self.all 
   @@all 
