9936ab9d02d943d422a21a5264d92e226584bcd9
Samantha brake <sammy.brake@gmail.com>
2018-10-08 19:50:23 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -11,4 +11,8 @@ class MusicImporter
   def files 
     Dir.glob("#{path}/*").map {|file| file.gsub("./spec/fixtures/mp3s/", "")}
   end 
+  
+  def import 
+    files.each {|file| Song.create_from_filename(file)}
+  end 
 end 
