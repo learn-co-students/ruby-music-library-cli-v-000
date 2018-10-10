4d772f2c1d531a58eb2b6345756a8ad724579314
taisaoko <ttd3calvin@gmail.com>
2018-10-10 21:54:38 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,11 @@
 module Concerns::Findable
-  module ClassMethods
-    def self.find_by_name(name)
-      self.all.detect {|x| x.name == name }
-    end
-
-    def self.find_or_create_by_name(name)
-      self.find_by_name(name) || self.create(name)
-    end
+  
+  def find_by_name(name)
+    all.detect {|x| x.name == name }
   end
+
+  def find_or_create_by_name(name)
+    find_by_name(name) || self.create(name)
+  end
+  
 end
