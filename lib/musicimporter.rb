fbeec3607a48c7fcc4a49e8a479081f421f2632a
Nick Romans <nicholas.romans@gmail.com>
2018-10-10 15:33:45 +0000

    Done.

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,3 +1,17 @@
 
 class MusicImporter
+  
+  attr_reader :path
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+    files = []
+    Dir.glob("#{path}/*.mp3").each do |file_name|
+      files << file_name
+    end
+  end
+  
 end
