49ac793468cf74ba94601ae65f1521d727bccf8d
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-11 09:08:41 +0000

    Done.

--- lib/musicimportercontroller.rb
+++ lib/musicimportercontroller.rb
@@ -25,6 +25,22 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       user_input = gets.strip 
+    
+    if user_input == 'list songs'
+      list_songs
+    elsif user_input == 'list artists'
+      list_artists
+    elsif user_input == 'list genres'
+      list_genres
+    elsif user_input == 'list artist'
+      list_songs_by_artist
+    elsif  user_input == 'list genre'
+      list_songs_by_genre
+    elsif user_input == 'play song'
+      play_song
+    end 
+    
+    
     end 
   end 
   
@@ -52,6 +68,7 @@ class MusicLibraryController
 
  def list_songs_by_artist
     puts "Please enter the name of an artist:"
+
     input = gets.strip
 
     if artist = Artist.find_by_name(input)
@@ -62,10 +79,10 @@ class MusicLibraryController
  end
 
 
-def list_songs_by_genre 
+def list_songs_by_genre
+  
     puts "Please enter the name of a genre:"
     input = gets.strip
-
     if genre = Genre.find_by_name(input)
       genre.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
         puts "#{i}. #{s.artist.name} - #{s.name}"
@@ -73,4 +90,16 @@ def list_songs_by_genre
     end
  end
  
+ def play_song
+    puts "Which song number would you like to play?"
+
+    input = gets.strip.to_i
+    if (1..Song.all.length).include?(input)
+      list = Song.all.uniq.sort {|a, b| a.name <=> b.name}
+      song = list[input - 1]
+    end
+
+    puts "Playing #{song.name} by #{song.artist.name}" if song 
+  end
+
 end 
