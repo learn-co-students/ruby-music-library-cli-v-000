6bda5d84136479a4cbea7d67b0002eefe180a267
taisaoko <ttd3calvin@gmail.com>
2018-10-12 22:38:05 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -11,6 +11,14 @@ class MusicLibraryController
     input = ""
     while input != "exit"
       puts "Welcome to your music library!"
+      "To list all of your songs, enter 'list songs'."
+      "To list all of the artists in your library, enter 'list artists'."
+      "To list all of the genres in your library, enter 'list genres'."
+      "To list all of the songs by a particular artist, enter 'list artist'."
+      "To list all of the songs of a particular genre, enter 'list genre'."
+      "To play a song, enter 'play song'."
+      "To quit, type 'exit'."
+      "What would you like to do?"
       input = gets.chomp
       case input
       when "list songs"
