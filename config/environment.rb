2010a62faba4d5b3da8cc438017df95e0c8bcc5f
Ashley Wong <ashwong04@gmail.com>
2018-10-12 01:36:02 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,34 @@ module Concerns
 end
 
 require_all 'lib'
+
+
+
+class Song
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name= name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(song_name)
+    song_name = Song.new(song_name)
+    song_name.save
+    song_name
+  end
+end
