d64d705da1309da78e0b965dcdbaa8ba1aae388b
Ashley Wong <ashwong04@gmail.com>
2018-10-13 02:39:17 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -36,3 +36,63 @@ class Song
     song_name
   end
 end
+
+
+class Artist
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name= name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(art_name)
+    art_name = Artist.new(art_name)
+    art_name.save
+    art_name
+  end
+end
+
+
+class Genre
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name= name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(genre_name)
+    genre_name = Genre.new(genre_name)
+    genre_name.save
+    genre_name
+  end
+end
