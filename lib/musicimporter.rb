6e197bf4a01f822af96918305fb90597f50abca7
Rachel Wyatt <rw126@evansville.edu>
2018-10-14 14:16:14 +0000

    Done.

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -5,10 +5,12 @@ attr_reader :path
   end
 
   def files
-    Dir.glob("#{path}").map { |file| file.gsub("#{path}")}
+    Dir.glob("#{path}/*").map { |file| file.gsub("#{path}/", '')}
+  end
+
+  def import
+    files.each {|file| Song.create_from_filename(file)}
   end
 
 
 end
-
-#video 13:11
--- lib/song.rb
+++ lib/song.rb
@@ -52,4 +52,16 @@ class Song
     find_by_name(song) || Song.create(song)
   end
 
+  def self.new_from_filename(name)
+    artist, song, genre = name.split(" - ")
+    fixed = genre.gsub('.mp3', '')
+    artist = Artist.find_or_create_by_name(artist)
+    genre = Genre.find_or_create_by_name(fixed)
+    new(song, artist, genre)
+  end
+
+  def self.create_from_filename(name)
+    new_from_filename(name).save
+  end
+
 end
