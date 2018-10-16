2e3c6810111697cde753f65ae5f1591c0cb44611
Nick Romans <nicholas.romans@gmail.com>
2018-10-16 15:19:06 +0000

    Done.

--- lib/musiclibrarcontroller.rb
+++ lib/musiclibrarcontroller.rb
@@ -1,32 +1,42 @@
-
+require 'pry'
 
 class MusicLibraryController
   
-   def initialize(path = "./db/mp3s")
+  def initialize(path = "./db/mp3s")
     MusicImporter.new(path).import
    end
    
-   def call
+  def call
+    input = ""
+    while input != "exit"
+  
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
     
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    
-    input = gets.chomp
-    
-      until input == "exit"
-        input = gets.chomp
-      end 
-    end    
-    
-    def list_songs
-    end 
-      
+      input = gets.strip 
+    end
   end
+  
+  def list_songs
+    Song.all.sort_by(&:name).each.with_index(1) do |song, index|
+      #binding.pry
+      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+  end
+end
+
+  def list_artists
+    Artist.all.sort_by(&:name).each.with_index(1) do |artist, index|
+      #binding.pry
+      puts "#{index}. #{artist.name}"
+    end
+  end
+  
+end    
     
