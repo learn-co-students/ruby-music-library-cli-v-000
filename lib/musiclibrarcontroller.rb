44792a93e9df73d024d66f653ee0e3bc5273adc6
Nick Romans <nicholas.romans@gmail.com>
2018-10-16 17:57:08 +0000

    Done.

--- lib/musiclibrarcontroller.rb
+++ lib/musiclibrarcontroller.rb
@@ -22,6 +22,20 @@ class MusicLibraryController
     
       input = gets.strip 
       
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
       
     end
   end
