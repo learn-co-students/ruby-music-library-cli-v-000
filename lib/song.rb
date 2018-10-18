ddf7e39e1af7655636ca83c99e2fee8c9e86bb19
Cheryl Kelson <cherylkelson@icloud.com>
2018-10-18 21:59:25 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song 
+  attr_accessor :artist, :genre, :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    self.artist = artist if artist 
+    self.genre = genre if genre
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def self.create(name)
+    new(name).tap {|s| s.save} 
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def save 
+    @@all << self
+    
+  def self.create(name) 
+    new(name,)
+  end 
+  end 
+  
+end 
