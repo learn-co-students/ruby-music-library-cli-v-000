0020ae3e2c34c9fe974193a356c00e0a23fe88b4
Aderianna Williams <aderiannaw@yahoo.com>
2018-10-19 03:26:39 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor  :name, :artist
    @@all = []
    
-def initialize(name, artist = nil)
+def initialize(name, artist = nil, genre = nil)
   @name = name
   @artist = artist
 end
