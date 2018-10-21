55e9e690fa350986e58f46cb968979cf9f0bfaf0
Ashley Wong <ashwong04@gmail.com>
2018-10-21 03:40:14 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -29,19 +29,12 @@ class MusicLibraryController
   end
 
   def list_songs
-    sorted_songs = @importer.sort do |x,y|
-      x= x.split(' - ')[1]
-      y= y.split(' - ')[1]
-    # binding.pry
-      x <=> y
-    end
-    
+    songs = Song.all.sort {|x,y| x.name <=> y.name}
       x = 1
-    @importer.each do |file|
-      file = file.delete('.mp3')
-      puts "#{x}. #{file}"
-      x += 1
-    end
-  end
+    songs.each do |song|
+  #     puts "#{x}. #{song.name}"
+  #     x += 1
+  #   end
+  # end
 
 end
