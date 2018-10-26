fdc0c3c733c4ef915c9cc10d8fa043de4b14b44b
Andrew Bernier <a6bernier@gmail.com>
2018-10-26 16:07:50 +0000

    Done.

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -20,6 +20,7 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets.strip
+
       case input
         when "list songs"
           list_songs
