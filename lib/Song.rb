3a4812365460e3fa7d9f8805996a954440fc8408
Stef Claus <stef.claus@gmail.com>
2018-10-28 22:48:27 +0000

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -47,6 +47,19 @@ class Song
     new_song 
   end 
 
+  def self.find_by_name(name) 
+    if self.all.detect { |artist| artist.name == name } 
+       self.all.detect { |artist| artist.name == name } 
+    end 
+  end 
   
-end 
+  def self.find_or_create_by_name(name)
+     if self.find_by_name(name) != nil 
+       self.find_by_name(name)
+    else
+      self.create(name)
+    end   
+  end 
+
+end #ends class 
 
