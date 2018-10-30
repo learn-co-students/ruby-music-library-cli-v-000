902d0aa3da67055aee270595cfba54be2286d76f
Vince Andrew Pimentel <contact@vincep.me>
2018-10-30 22:12:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
 
   def self.find_by_name(name)
-    self.all.detect { |song| song.name == name }
+    self.all.find { |song| song.name == name }
   end
 
   def self.find_or_create_by_name(name)
