432a5fb12cf12f98c33d5958f2e36ac16ea0a909
Vince Andrew Pimentel <contact@vincep.me>
2018-10-31 04:56:45 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,12 +1,12 @@
 class MusicImporter
-  attr_accessor :path, :files
+  attr_accessor :path
 
   def initialize(path)
     @path = path
   end
 
   def files
-    @files = Dir.glob("#{@path}/*.mp3").collect { |file| file.gsub("#{@path}/", "") }
+    Dir.glob("#{@path}/*.mp3").collect { |file| file.gsub("#{@path}/", "") }
   end
 
   def import
--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -72,8 +72,8 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
+    song = Song.all.sort_by { |song| song.name }[input - 1]
     if input.between?(1, Song.all.size)
-      song = Song.all.sort_by { |song| song.name }[input - 1]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
