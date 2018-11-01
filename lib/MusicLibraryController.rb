ca502e843fc6c1b648dddf071c7f21c4667dd1df
Stef Claus <stef.claus@gmail.com>
2018-11-01 00:26:23 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,14 +1,32 @@
 class MusicLibraryController
   
+   extend Concerns::Findable
+
+  
   attr_accessor :path 
   
-  def initialize(path = nil)
-    default_path = ".../db/mp3s/"
-    self.path= default_path  if path != nil
+  def initialize(path = "./db/mp3s")
+    #default_path = "./db/mp3s/"
+    #self.path = default_path  if path != nil
+    MusicImporter.new(path).import 
   end 
 
   def call 
-  end 
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts  "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    4.times do
+      list_songs = gets 
   
+    end 
+    
+  end
+
 
 end 
