1e8c739b3e4c8ff2f77887ee8895352b80cd126f
Stef Claus <stef.claus@gmail.com>
2018-11-04 01:59:37 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -11,21 +11,39 @@ class MusicLibraryController
   
 
 
-  def call 
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts  "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    4.times do
-      stef = gets 
-    end 
-  end
+   def call
+     input = gets.strip
 
+     if input != exit
+
+     puts "Welcome to your music library!"
+     puts "To list all of your songs, enter 'list songs'."
+     puts "To list all of the artists in your library, enter 'list artists'."
+     puts "To list all of the genres in your library, enter 'list genres'."
+     puts "To list all of the songs by a particular artist, enter 'list artist'."
+     puts "To list all of the songs of a particular genre, enter 'list genre'."
+     puts "To play a song, enter 'play song'."
+     puts "To quit, type 'exit'."
+     puts "What would you like to do?"
+
+     case input
+         when "list songs"
+           list_songs
+         when "list artists"
+           list_artists
+         when "list genres"
+           list_genres
+         when "list artist"
+           list_songs_by_artist
+         when "list genre"
+           list_songs_by_genre
+         when "play song"
+           play_song
+         end
+     end
+   end
+   
+   
   def list_songs
     Song.all.sort {|x,y| x.name <=> y.name }.each.with_index(1) do |item, index|
     puts "#{index}. #{item.artist.name} - #{item.name} - #{item.genre.name}"
@@ -73,17 +91,6 @@ end
    
 end 
 
-#def play_song 
-  
-  #puts "Which song number would you like to play?"
-  
-  #user_input = gets.chomp.to_i
-
-  #song_list =  Song.all.sort {|x,y| x.name <=> y.name }
-  #song = song_list[user_input+1]
- # puts "Playing #{song.name} by #{song.artist.name}"
-  
-#end 
 
   def play_song
     puts "Which song number would you like to play?"
@@ -94,6 +101,7 @@ end
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
-
-
+  
+  
+  
 end #ends class 
