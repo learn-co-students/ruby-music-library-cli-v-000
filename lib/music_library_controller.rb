b48f8554b363b9063584472e8dee8e2bc189ecb9
Susan Meyers <smmeyers24@gmail.com>
2018-11-12 16:46:09 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -28,5 +28,21 @@ class MusicLibraryController
         input = gets.strip 
       end
   end
+
   
 end
+
+# class CLIMethods
+  
+#   def list_songs
+#     prints all songs in the music library in a numbered list (alphabetized by song name)"
+#           1. Thundercat - For Love I Come 
+#           2. Real Estate - Green Aisles 
+#           3. Real Estate - It's Real 
+#           4. Action Bronson 
+#           5. What's Golden 
+
+#       music_library_controller.list_songs
+    
+#   end
+  
