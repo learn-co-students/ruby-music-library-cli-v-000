50befe9febadc174d43a381ca7bdfa79f353abf9
Malcome Fein <mfein90@gmail.com>
2018-11-12 19:54:49 -0500

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,24 +1,39 @@
-require 'pry'
+# require 'pry'
+# class MusicImporter
+#
+# attr_accessor :path
+#
+# @@all = []
+#   def initialize(path)
+#     @path = path
+#     # "./spec/fixtures/mp3s"
+#     #binding.pry
+#   end
+#
+#   def files
+#     #binding.pry
+#     Dir.entries("./spec/fixtures/mp3s").select {|f| !File.directory? f}
+#
+#   end
+#
+#   def import
+#     #binding.pry
+#     files.each{|file_name| Song.create_from_filename(file_name)}
+#     # Song.create_from_filename(file_name)
+#   end
+# end
 class MusicImporter
+  attr_reader :path
 
-attr_accessor :path
-
-@@all = []
   def initialize(path)
     @path = path
-    # "./spec/fixtures/mp3s"
-    #binding.pry
   end
 
   def files
-    #binding.pry
-    Dir.entries("./spec/fixtures/mp3s").select {|f| !File.directory? f}
-
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 
   def import
-    #binding.pry
-    files.each{|file_name| Song.create_from_filename(file_name)}
-    # Song.create_from_filename(file_name)
+    files.each{ |f| Song.create_from_filename(f) }
   end
 end
