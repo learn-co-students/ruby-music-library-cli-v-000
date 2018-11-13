a3275ac33a0104e6e0caa5b14349203df2b382fd
Malcome Fein <mfein90@gmail.com>
2018-11-12 19:47:33 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
+# the file that loaded all of my libraries
+# all of my different files
 require 'bundler'
 Bundler.require
+# read gem file and require in
+# the proper gems for our project
 
 module Concerns
 end
