322e8b4efa5ee867fea095fedf7b6ea29e6a76e5
Greg Morgan <gregm9999@gmail.com>
2018-11-15 23:18:09 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,35 @@
+class Genre
+  extend Concerns::Findable
+  attr_accessor :name, :songs
+  @@all=[]
+
+  def initialize(name)
+    @name=name
+    @songs=[]
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all<<self
+    self
+  end
+
+  def self.create(name)
+    genre=self.new(name)
+    genre.save
+    genre
+  end
+
+  def add_song(song)
+   song.genre = self unless song.genre == self
+   @songs << song unless @songs.include?(song)
+  end
+
+end
