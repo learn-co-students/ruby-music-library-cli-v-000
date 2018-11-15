a7c8e5fc5b288efa797a3afa45dd07d5832d49ff
Rose Brennan <brennan.m.rose@gmail.com>
2018-11-15 13:29:55 +0000

    Done.

--- lib/music_library_controler.rb
+++ lib/music_library_controler.rb
@@ -5,44 +5,35 @@ class MusicLibraryController
   end
   
   def call 
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    exit_selected = false 
-    until exit_selected do
-      input = gets.strip
-      if input == 'exit'
-        exit_selected = true
-      else 
-        handle_inputs(input)
-      end
-    end
-  end 
-  
-  def handle_inputs(input)
-    if input == "list songs"
-      list_songs
-    elsif 
-      input == "list artists"
-      list_artists
-    elsif 
-      input == "list genres"
-      list_genres
-    elsif
-      input == "list artist" 
-      list_songs_by_artist 
-    elsif 
-      input == "list genre"
-      list_songs_by_genre 
-    elsif 
-      input == "play song"
-      play_song 
+    input = "" 
+    
+    while input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      
+      input = gets.strip 
+      
+      case input
+        when "list songs"
+          list_songs
+        when "list artists"
+          list_artists
+        when "list genres"
+          list_genres
+        when "list artist" 
+          list_songs_by_artist 
+        when "list genre"
+          list_songs_by_genre 
+        when "play song"
+          play_song 
+        end
     end
   end
   
