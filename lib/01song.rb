acee9e03a9290e447021f536d6a289591ee3262d
Amanda Murphy <helloamandamurphy@gmail.com>
2018-11-16 23:23:10 +0000

    Done.

--- lib/01song.rb
+++ lib/01song.rb
@@ -41,4 +41,20 @@ attr_reader :artist, :genre
     instance.save
     instance
   end
+
+  def self.new_from_filename(filename)
+    file = filename.split(" - ")
+    artist_name = file[0].gsub(".mp3", "")
+    song_name = file[1].gsub(".mp3", "")
+    genre_name = file[2].gsub(".mp3", "")
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+
+    new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{|song| song.save}
+  end
 end
---
+++ lib/musicimporter.rb
@@ -0,0 +1,18 @@
+class MusicImporter
+
+attr_reader :path
+
+  def intialize(path)
+    @path = path
+  end
+
+  def files
+    @files ||= Dir.glob("#{@path}/*.mp3").collect do |file|
+      file.gsub("#{@path}/","")
+    end
+  end
+
+  def import
+    files.each {|file| Song.create_from_filename(file)}
+  end
+end
