9741ac740202e5fc8cbb65ed1fdf183f18ee2298
Lois Lee <lois.lee@me.com>
2018-11-30 04:55:31 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,48 @@
+require 'pry'
+class Artist
+
+  extend Concerns::Findable
+  
+  attr_accessor :name
+  attr_reader :songs
+
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    songs.collect{ |s| s.genre }.uniq
+  end
+
+  def add_song(song)
+    song.artist = self unless song.artist
+    @songs << song unless songs.include?(song)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,39 @@
+class Genre
+
+  extend Concerns::Findable
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    songs.map {|s| s.artist}.uniq
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    genre = Genre.new(name)
+    genre.save
+    genre
+  end
+
+end
