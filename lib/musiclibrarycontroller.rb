83d18228e280374fdf6a7732eaa44911f1a750f3
jontenthoff <jontenthoff@hotmail.com>
2018-12-20 15:46:25 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -19,6 +19,7 @@ class MusicLibraryController
     puts "What would you like to do?"
     input = ""
     while input != "exit"
+    puts "Welcome to your music library!"
     puts "What would you like to do?"
     input = gets.strip
   end
