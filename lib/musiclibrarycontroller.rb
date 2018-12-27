b7e5e9ad5ef91eef084b5765c34d63d60351053a
jontenthoff <jontenthoff@hotmail.com>
2018-12-27 15:49:33 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -21,7 +21,19 @@ class MusicLibraryController
     while input != "exit"
     puts "What would you like to do?"
     input = gets.strip
-
+    if input == "list songs"
+      list_songs
+    elsif input == "list artists"
+      list_artists
+      elsif input == "list genres"
+      list_genres
+      elsif input == "list artist"
+      list_songs_by_artist
+      elsif input == "list genre"
+      list_songs_by_genre
+      elsif input == "play song"
+      play_song
+    end
       end 
     end
      
