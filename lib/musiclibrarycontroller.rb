583c7cf2b14da774115924dff7815ff5bf7fce4e
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-01-15 21:35:59 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -18,7 +18,42 @@ class MusicLibraryController
     puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'." 
-    puts "What would you like to do?"
+      counter = 0
+      loop do
+        puts "What would you like to do?"
+        counter += 1 
+        answer = gets.chomp
+        if answer == "exit" || counter == 4 
+          break
+        end
+      end
+  end
+  
+  def list_songs
+    # binding.pry
+    counter = 1
+    Song.all.sort_by(&:name).map { |song|
+      puts "#{counter}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      counter+=1  
+    }
+  end
+  
+  def list_artists
+    # binding.pry
+    counter = 1
+    Artist.all.sort_by(&:name).map { |artist|
+      puts "#{counter}. #{artist.name}"
+      counter+=1  
+    }
+  end
+  
+  def list_genres
+    # binding.pry
+    counter = 1
+    Genre.all.sort_by(&:name).map { |genre|
+      puts "#{counter}. #{genre.name}"
+      counter+=1  
+    }
   end
   
 end
