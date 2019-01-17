8dfea76de8549bbb2dd5fba52bb95edf58549efe
Paula McKinley <pmknyc@optimum.net>
2019-01-17 16:59:24 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,12 +1,16 @@
-# 1/8/19  MUST UPDATE from artist-song modules lab
-
+require 'pry'
 module Concerns::Findable
 
     def find_by_name(name)
-      self.all.find {|x| x if x.name == name}
+    #  self.all.find {|x| x if x.name == name}
+    binding.pry
+      self.all.find {|x| self if x.name == name}
     end
 
-    def find_or_create_by_name(name)
-      find_by_name(name) || create(name)
-    end
+  #  def find_or_create_by_name(name)
+  #    find_by_name(name) || create(name)
+  #  #  find_by_name(name) || self.new(name).save
+  #  #  self.all.find {|x| x if x.name == name} || self.new(name).save
+  #    self
+  #  end
 end #module end
