1d1339d3c78e9c5ce2133bb91157e1f4a848425e
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-01-18 04:35:04 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -26,6 +26,20 @@ class MusicLibraryController
         if answer == "exit" || counter == 4 
           break
         end
+        case answer
+        when 'list songs'
+          list_songs
+        when 'list artists'
+          list_artists
+        when 'list genres'
+          list_genres
+        when 'list artist'
+          list_songs_by_artist
+        when 'list genre'
+          list_songs_by_genre
+        when 'play song'
+          play_song
+        end
       end
   end
   
