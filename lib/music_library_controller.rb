20003e806a574add6e2e5483a53ce8344054e910
Paula McKinley <pmknyc@optimum.net>
2019-02-12 20:35:53 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -4,15 +4,16 @@ class MusicLibraryController
   # accepts an optional path to the library of MP3 files, defaulting to ./db/mp3s/
   # instantiate a MusicImporter object
 
-  attr_accessor :path, :library
+  attr_accessor :path
 
   def initialize(path="./db/mp3s")
     @path = path
     MusicImporter.new(path).import
-    @library = []
   end
 
     def call
+      input = ""
+      while input != "exit"
         puts "Welcome to your music library!"
         puts "To list all of your songs, enter 'list songs'."
         puts "To list all of the artists in your library, enter 'list artists'."
@@ -24,49 +25,42 @@ class MusicLibraryController
         puts "What would you like to do?"
 
         input = gets.strip.downcase
-        until input == "exit"
-          self.call
-          case input
+
+        case input
           when "list songs"
             list_songs
           when "list artists"
             list_artists
           when "list genres"
             list_genres
-          when "list_artist"
+          when "list artist"
             list_songs_by_artist
           when "list genre"
             list_songs_by_genre
-          end
+          when "play song"
+            play_song
         end
       end
+    end
 
   def list_songs
     Song.all.sort_by(&:name).each.with_index(1) do |song, index|
-      #self.library << [index, song]
-        #  end
-        #  self.library.sort!{|a,b| a[1] <=> b[1]}.each.with_index(1) do |song_parts, index|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
 
+#  note & compare equivalency in the use of
+#  #sort_by in list_artists
+#  #sort in list_genres
   def list_artists
-   alpha = []
-     Artist.all.each do |artist|
-       alpha << artist.name
-     end
-     alpha.sort!.each.with_index(1) do |name, index|
-       puts "#{index}. #{name}"
-     end
+    Artist.all.sort_by(&:name).each.with_index(1) do |artist, index|
+     puts "#{index}. #{artist.name}"
+    end
   end
 
   def list_genres
-    alpha = []
-    Genre.all.each do |genre|
-      alpha << genre.name
-    end
-    alpha.sort!.each.with_index(1) do |name, index|
-      puts "#{index}. #{name}"
+    Genre.all.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |genre, index|
+      puts "#{index}. #{genre.name}"
     end
   end
 
@@ -77,14 +71,8 @@ class MusicLibraryController
       artist.songs.sort_by(&:name).each.with_index(1) do |song, index|
         puts "#{index}. #{song.name} - #{song.genre.name}"
       end
-    else
-        nil
     end
   end
-        #  alpha.sort!{|a,b| a[0] <=> b[0]}.each.with_index(1) do |song_parts, index|
-        #    list << "#{index}. #{song_parts.join(" - ")}"
-        #    puts "#{index}. #{song_parts.join(" - ")}"
-        #  end
 
   def list_songs_by_genre
     puts "Please enter the name of a genre:"
@@ -92,33 +80,17 @@ class MusicLibraryController
     if genre = Genre.find_by_name(input)
       genre.songs.sort_by(&:name).each.with_index(1) do |song, index|
         puts "#{index}. #{song.artist.name} - #{song.name}"
-      end    #input = gets.strip.downcase.split(/ |\_/).map(&:capitalize).join(" ")
-    else
-      nil
+      end
     end
   end
-          #alpha.sort!{|a,b| a[1] <=> b[1]}.each.with_index(1) do |song_parts, index|
-          #  list << "#{index}. #{song_parts.join(" - ")}"
-          #  puts "#{index}. #{song_parts.join(" - ")}"
-          #end
 
   def play_song
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
-    binding.pry
     if (1..Song.all.length).include?(input)
-      song = Song.all[input]
-      puts "Playing #{song.name} by #{song.artist.name}"
+      selection = Song.all.sort_by(&:name)[input-1] #convert input to array index num
+      puts "Playing #{selection.name} by #{selection.artist.name}"
     end
   end
 
-      # =>   def play_song
-      # =>     puts "Which song number would you like to play?"
-      # =>     input = gets.strip.to_i #gets input, converts to the string to an integer
-      # =>     if (1..Song.all.length).include?(input)
-      # =>       song = Song.all[input] #matches input by index
-      # =>     end
-      # =>     puts "Playing #{song.name} by #{song.artist.name}" if song #prints the matching song
-      # =>     #checks for 1 - all songs
-      # =>   end
 end # class end
