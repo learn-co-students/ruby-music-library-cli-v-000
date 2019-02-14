91ad0c90e9dc6e3960089043f4563434dc516d96
J Fern <jfern20@gmail.com>
2019-02-14 06:47:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,27 +2,31 @@ class Song
   
   attr_accessor :name, :artist, :genre 
   
-  @@all =[]
+  @@all =[]             #is initialized as an empty array
   
   def initialize(name)
     @name= name 
-  
+   
   end 
   
-  
-  def self.all 
+  def self.all          #returns the class variable @@all
     @@all 
   end 
   
   
-  def self.destroy_all
+  def self.destroy_all  #resets the @@all class variable to an empty array
    @@all.clear
   end 
   
+  def  save            #adds the Song instance to the @@all class variable
+  @@all << self  
+  self 
+  end 
+  
 
-  
-  
-  
+  def self.create
+    
+  end 
   
   
   
