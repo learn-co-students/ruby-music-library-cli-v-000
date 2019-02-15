08a6d436758d235514e5953431e60e46fe14270d
maxwellbenton <maxwellbenton@gmail.com>
2019-02-15 09:31:11 -0500

    fixes missing environment.rb content

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,19 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+  module Findable
+    def find_by_name(name)
+      self.all.detect {|object| object.name == name}
+    end
+
+    def find_or_create_by_name(name)
+      if self.find_by_name(name).nil?
+        self.create(name)
+      else
+        self.find_by_name(name)
+      end
+    end
+  end
 end
 
 require_all 'lib'
