9d27c83b3301d1f6308a6d46237528d03c266747
jamieberrier <berrier.jamie@gmail.com>
2019-03-20 18:01:27 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,5 +9,27 @@ class MusicLibraryController
     MusicImporter.new(@path).import
   end
 
+  #welcomes the user
+  #asks the user for input
+  #loops and asks for user input until they type in exit
+  def call
+    input = nil
+    until input == 'exit'
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      input = gets.strip
+    end
+  end
 
+  #prints all songs in the music library in a numbered list (alphabetized by song name)
+  def list_songs
+    Song.all.collect {|song| puts song.artist, song.name, song.genre}
+  end
 end
