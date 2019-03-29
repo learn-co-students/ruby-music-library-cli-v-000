07ef194cba895e050b65adfc104fd253f77a0ba3
Kimberly Aviles <01010110.v@gmail.com>
2019-03-29 22:38:48 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,38 @@
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :name
+  attr_reader :artist, :genre
   @@all = []
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
-    @artist = artist
+
+    # if artist
+    #   self.artist = artist
+    # end
+    ####BETTER WRITTEN AS \/
+    self.artist = artist unless artist.nil?
+
+    self.genre = genre unless genre.nil?
 
   end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+
+    # if !genre.songs.include?(self)
+    #   genre.songs << self
+    # end
+    ###BETTER WRITTEN AS \/
+
+    genre.songs << self unless genre.songs.include?(self)
+
+  end
+
 
   def self.all
     @@all
@@ -21,9 +47,11 @@ class Song
   end
 
   def self.create(name)
-    created_song = Song.new(name)
-    created_song.save
-    created_song
+
+    created_song = Song.new(name).tap do |c_s| #<---using the TAP method removes the need to end this method with repetitive returns
+      c_s.save
+    end
+
   end
 
 
