b80273b9b16667979e3c79f28028f379941a972b
Kimberly Aviles <01010110.v@gmail.com>
2019-04-01 03:34:06 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,11 @@
 module Concerns::Findable
 
-  def self.find_by_name(name_string)
+  def find_by_name(name_string)
     self.all.detect { |each_object| each_object.name == name_string }
     # DETECTmethod returns NIL if no matching data is found - eliminating the need for an explicit flag
   end
 
-  def self.find_or_create_by_name(name_string)
+  def find_or_create_by_name(name_string)
     found = self.find_by_name(name_string)
     found != nil ? found : self.create(name_string)
     # if the FOUND placeholder does not have NIL as its value, return the FOUND song, otherwise call the CREATE method
