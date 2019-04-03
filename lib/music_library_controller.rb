2a2a46e1426178d8eb7eb893c6d04ff9d8ee090a
Kimberly Aviles <01010110.v@gmail.com>
2019-04-03 03:45:45 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,3 +1,5 @@
+
+
 class MusicLibraryController
 
   def initialize(path = nil)
@@ -21,7 +23,32 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    4.times { user_response = gets.strip unless user_response == "exit" }
+   4.times { user_response = gets.strip unless user_response == "exit" }
+
   end
+
+  def list_songs
+    all_songs = Song.all
+    all_songs.sort! { |a, b| a.name <=> b.name }
+
+    counter = 0
+    all_songs.each do |each_song|
+      counter += 1
+      puts "#{counter}. #{each_song.artist.name} - #{each_song.name} - #{each_song.genre.name}"
+    end
+  end
+
+  def list_artists
+    all_artists = Artist.all
+    all_artists.sort! { |a, b| a.name <=> b.name }
+
+    counter = 0
+    all_artists.each do |each_artist|
+      counter += 1
+      puts "#{counter}. #{each_artist.name}"
+    end
+  end
+
+
 
 end
