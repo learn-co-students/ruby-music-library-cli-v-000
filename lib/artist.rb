5a6ed4a303448996be4762ae02a5b8f064774d77
Niusha H <niusha.hadziomerovic@gmail.com>
2019-04-11 16:18:07 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+class Artist
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+
+  end 
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.destroy_all
+    # self.@@all.clear
+    @@all = []
+  end 
+  
+  def self.create(new_song) 
+    new_song = Song.new(new_song)
+    # new_song.initialize
+    new_song.save
+    new_song
+  end 
+  
+  # def new 
+    
+  # end 
+  
+end   
+  
