0d3adc1f5e2448024553a06efd61b69499dac320
Toni-Ty <antonique2479@yahoo.com>
2019-04-24 22:06:59 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,54 @@
+class Genre
+  extend Concerns::Findable
+
+  @@all = []
+  attr_accessor :name
+  attr_accessor :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = new(name)
+    genre.save
+    genre
+  end
+
+def artists
+  @songs.collect { |song| song.artist }.uniq
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
