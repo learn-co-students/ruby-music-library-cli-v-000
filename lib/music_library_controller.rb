9c460fe07c7428be0352379f5d6a33c70e865140
Toni-Ty <antonique2479@yahoo.com>
2019-04-25 05:16:26 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class MusicLibraryController
 
 def initialize(path = "./db/mp3s")
@@ -37,29 +38,54 @@ while input != "exit"
    end
  end
 
-def list_songs
+ def list_songs
+   Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, index|
+     puts "#{index}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+   end
+ end
 
+def list_artists
+  Artist.all.sort { |a, b| a.name <=> b.name}.each.with_index(1) do |a, index|
+    puts "#{index}. #{a.name}"
+  end
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def list_genres
+  Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |genre, index|
+    puts "#{index}. #{genre.name}"
+  end
 end
+
+def list_songs_by_artist
+  puts "Please enter the name of an artist:"
+  input = gets.strip
+
+  if artist = Artist.find_by_name(input)
+    artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, index|
+      puts "#{index}. #{s.name} - #{s.genre.name}"
+    end
+  end
+end
+
+def list_songs_by_genre
+  puts "Please enter the name of a genre:"
+  input = gets.strip
+
+  if genre = Genre.find_by_name(input)
+    genre.songs.sort{ |a, b| a.name <=> b.name}.each.with_index(1) do |s, index|
+    puts "#{index}. #{s.artist.name} - #{s.name}"
+    end
+  end
+end
+
+def play_song
+  puts "Which song number would you like to play?"
+
+  input = gets.strip.to_i
+  if (1..Song.all.length).include?(input)
+    song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+  end
+
+    puts "Playing #{song.name} by #{song.artist.name}" if song
+  end
+ end
