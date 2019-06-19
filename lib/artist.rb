7b09fefc59d477176e37ddc323daf8e7e7135e3e
roy renno <royrenno@yahoo.com>
2019-06-19 05:05:52 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs =[]
+  end
+
+def self.create(name)
+  new(name).tap{|a| a.save}
+end
+
+def self.all
+  @@all
+end
+def self.destroy_all
+  @@all clear
+end
+def add_song(song)
+  song.artist = self unless song.artist == self
+  @songs << song unless @songs.include?(song)
+def save
+  @@all << self unless @@all.include?(self)
+end
+
