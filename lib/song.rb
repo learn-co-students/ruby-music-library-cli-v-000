94cdc15f6153b356ad1dbcccc4fa6e75643c96f2
Dan Suarez <suarez.daniel101@gmail.com>
2019-07-06 22:34:55 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+end
