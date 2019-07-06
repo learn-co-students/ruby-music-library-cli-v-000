671055a382aab5d8b8942977c333f7780ed41a27
Lauren Days <lmchambers0420@gmail.com>
2019-07-06 13:27:42 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song 
+  attr_accessor :name
+
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.destroy.all 
+    
+  end 
+  
+end 
