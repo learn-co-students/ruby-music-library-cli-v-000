ff6d8cc7b1bcfde8c3b6905994b63536a37d1b87
Dan Suarez <suarez.daniel101@gmail.com>
2019-07-22 03:43:42 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,10 +1,12 @@
 class MusicImporter
 
+  attr_reader :path
+
   def initialize(path)
     @path = path
   end
 
-  def path
-
+  def files
+    Dir.entries(path).select {|f| !File.directory? f}
   end
 end
