e40b0fb60feba71b5e752a3b1a44973f99603358
Dan Suarez <suarez.daniel101@gmail.com>
2019-07-22 03:11:59 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -11,10 +11,7 @@ module Concerns
       else self.create(name)
       end
     end
-  end
-end
 
-# Implement a generic #find_by_name method that uses the .all method defined by the class to find an
-# instance of the class by name.
-# Implement a generic #find_or_create_by_name method that uses the .all method defined by the
-# class to find an instance of the class by name and to create a new instance if a match is not found.
+  end
+  
+end
