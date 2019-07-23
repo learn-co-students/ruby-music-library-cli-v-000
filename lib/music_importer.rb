53720d353366da6be0f5b034d7422feaca2e4c11
Dan Suarez <suarez.daniel101@gmail.com>
2019-07-23 03:11:49 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,6 +1,6 @@
 class MusicImporter
 
-  attr_reader :path
+  attr_reader :path, :filename
 
   def initialize(path)
     @path = path
@@ -9,4 +9,15 @@ class MusicImporter
   def files
     Dir.entries(path).select {|f| !File.directory? f}
   end
+
+  def import
+    files.each {|filename| Song.create_from_filename(filename)}
+  end
 end
+
+
+
+  # def import
+  #   #  4.times {Song.new_by_filename(file_name)} "this works but hardcoded to 4 times"
+  #   files.each {|filename| Song.new_by_filename(filename)}
+  # end
