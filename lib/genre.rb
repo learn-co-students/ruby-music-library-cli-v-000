82c84bcb6561bf5f0f73c5e2c2e2fa5c23c3191c
Lauren Days <lmchambers0420@gmail.com>
2019-08-12 20:26:10 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -25,4 +25,8 @@ class Genre
       song.save
     end    
   end 
+  
+  def artists
+    songs.map { | song| song.artist }.uniq
+  end 
 end 
--- lib/song.rb
+++ lib/song.rb
@@ -44,5 +44,19 @@ class Song
     @genre = genre
     genre.songs << self unless genre.songs.include?(self)
   end 
+  
+  def self.find_by_name(name)
+    @@all.find {|song| name == song.name }
+  end 
+  
+  def self.find_or_create_by_name(name)
+    if 
+     self.find_by_name(name)
+     return self.find_by_name(name)
+   else
+      self.create(name)
+    end 
+    
+  end 
 
 end
