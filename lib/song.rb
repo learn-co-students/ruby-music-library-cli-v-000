afa1381b2759cf0dc0441c9cb40a023be7610aeb
Lauren Days <lmchambers0420@gmail.com>
2019-09-05 01:59:22 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -61,7 +61,13 @@ class Song
   end
     
     def self.new_from_filename(name)
-     # binding.pry
+      artist, song, genre = name.split('-')
+      
+      
+      
+      artist = Artist.find_or_create_by_name(artist)
+      genre = Genre.find_or_create_by_name(genre)
+      new(song, artist, genre)
     end 
 
 end
