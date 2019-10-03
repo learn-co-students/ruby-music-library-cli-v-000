566696aa050a60ba327525b95c8a59e468d5044b
Olivia Kesler <oakesler@protonmail.ch>
2019-10-03 18:35:42 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,9 +1,10 @@
 class MusicLibraryController
+  attr_accessor :path, :music
   
   def initialize(path = "./db/mp3s", import = true)
     @path = path
     @import = import
-    music = MusicImporter.new(path)
+    @music = MusicImporter.new(path)
     music.import
   end
   
@@ -22,4 +23,38 @@ class MusicLibraryController
         user_input_1 = gets.strip
       end
     end
+    
+  def list_songs
+    song_array = []
+    alpha_file_list = []
+    final_file_list = []
+    music.files.each do |song|
+      song_array << song.scan(/(?<=-\s).*?(?=\s-)/)[0]
+    end
+    song_array.sort.each do |song|
+      music.files.each do |file|
+        if file.include? "#{song}"
+          alpha_file_list << file
+        end
+      end
+    end 
+    x = 1
+    alpha_file_list.each do |file|
+      final_file_list << "#{x}. #{file.gsub(".mp3", "")}"
+      x += 1
+    end
+    z = 0 
+    while z <= final_file_list.length - 1
+      puts final_file_list[z]
+      z += 1
+    end
   end
+end
+          
+        #while x <= alpha_file_list.length
+        #puts "#{x}. #{alpha_list[y]}"
+        #x += 1 
+        #y += 1
+      #end
+    #end
+  #end
