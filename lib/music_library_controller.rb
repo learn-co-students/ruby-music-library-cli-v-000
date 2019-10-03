afd2294eb3af390e6aabd36bcd58bc1b1f199d79
Olivia Kesler <oakesler@protonmail.ch>
2019-10-03 17:18:02 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -3,10 +3,23 @@ class MusicLibraryController
   def initialize(path = "./db/mp3s", import = true)
     @path = path
     @import = import
-    MusicImporter.new(path)
+    music = MusicImporter.new(path)
+    music.import
   end
   
-  def call(input)
-    input_1 = input.strip
+  def call
+    user_input_1 = " "
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      while user_input_1 != "exit"
+        puts "What would you like to do?"
+        user_input_1 = gets.strip
+      end
+    end
   end
-end
