baf4940c96911c28e1ebcfa8ba9291a2f0bf9aef
Olivia Kesler <oakesler@protonmail.ch>
2019-10-04 20:48:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -91,14 +91,12 @@ end
     name_final = name.scan(/(?<=-\s).*?(?=\s-)/)[0]
     #format_genre_1 = name.split.last
     #genre_temp = format_genre_1.gsub(".mp3", "")
-   
-    genre_redo_1 = name.scan(/(?<= - ).*?(?=.mp3)/)
+    genre_redo_1 = name.scan(/(?<= - ).*?(?=.mp3)/)[0]
     genre_redo_2 = genre_redo_1.split("-")
-    
-    
+    genre_redo_final = genre_redo_2[1].strip
     artist_temp = name.scan(/(?<=\A).*?(?= -)/)[0]
     artist_final = Artist.find_or_create_by_name(artist_temp)
-    genre_final = Genre.find_or_create_by_name(genre_temp)
+    genre_final = Genre.find_or_create_by_name(genre_redo_final)
     self.new(name_final, artist_final, genre_final)
   end
   
