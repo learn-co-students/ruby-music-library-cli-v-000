578fabeb3a2c6225b3dc10650d92d9447298dea7
Paris Dolfman <parisdolfman@gmail.com>
2019-10-14 13:42:35 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+end
