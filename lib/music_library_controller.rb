08ab09eddaefd1fcbf487afc935ca917293af23e
Paris Dolfman <parisdolfman@gmail.com>
2019-10-16 22:08:18 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -37,7 +37,11 @@ class MusicLibraryController
       end
     end
 
-
+  def list_songs
+     Song.all.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |song, index|
+       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+     end
+  end
 
 
   end
