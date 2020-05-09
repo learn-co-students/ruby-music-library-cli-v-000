643bb5d7befeb35b4617b91a1feaf29ca7a56801
rosa lee <rosalee0116@gmail.com>
2020-05-09 21:35:13 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,20 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+  module Findable
+
+    def find_by_name(name)
+      all.detect {|song| song.name == name}
+    end
+
+    def find_or_create_by_name(name)
+      if find_or_create_by_name(name).nil?
+        create (name)
+      else
+        find_by_name(name)
+      end
+    end
+  end
 end
 
 require_all 'lib'
