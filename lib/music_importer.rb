5aaf18e49a17e678853113131a3a1c5f0965be32
Jaime Johnsen <drivetraindiva@gmail.com>
2020-12-01 17:53:28 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -5,12 +5,13 @@ class MusicImporter
   end
 
   def path#path retrieves the path provided to the MusicImporter object
-    dir  = File.dirname(pn)     # "/usr/bin"
-  #./spec/fixtures/mp3s
+    #dir  = File.dirname(pn)     # "/usr/bin"
+   MusicImporter.path = ("./spec/fixtures/mp3s")
   end
 
   def files
-
+    #@files || = Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+    #removes the actual path that is attached to filename and collects the filename only
   end
 
   def import
