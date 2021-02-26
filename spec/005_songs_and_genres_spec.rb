c0f8d6b66fd6ffe3370ba2c596a91fb951d46f8a
max b <maxwellbenton@gmail.com>
2021-02-26 11:46:09 -0500

    add tests from single_source_of_truth_solution

--- spec/005_songs_and_genres_spec.rb
+++ spec/005_songs_and_genres_spec.rb
@@ -1,22 +1,15 @@
 require "spec_helper"
 
 describe "Associations — Song and Genre:" do
-  let(:song) { Song.new("In the Aeroplane Over the Sea") }
+  let(:song) { Song.create("In the Aeroplane Over the Sea") }
   let(:genre) { Genre.new("indie rock") }
 
   context "Genre" do
-    describe "#initialize" do
-      it "creates a 'songs' property set to an empty array (genre has many songs)" do
-        expect(genre.instance_variable_defined?(:@songs)).to be(true)
-        expect(genre.instance_variable_get(:@songs)).to eq([])
-      end
-    end
-
     describe "#songs" do
       it "returns the genre's 'songs' collection (genre has many songs)" do
         expect(genre.songs).to eq([])
 
-        genre.songs << song
+        song.instance_variable_set(:@genre, genre)
 
         expect(genre.songs).to include(song)
       end
